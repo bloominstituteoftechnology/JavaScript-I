723fdd8525570b891cf477b565e02c9033622e9f
mister-corn <code@ronaldlibago.com>
2018-04-16 16:44:04 -0700

    4:43 PM PDT Added simple test to callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,14 +1,20 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const log = (result) => console.log(result);
 
 const firstItem = (arr, cb) => cb(arr[0]);
+firstItem(items, log);
 
 const getLength = (arr, cb) => cb(arr.length);
+getLength(items, log);
 
 const last = (arr, cb) => cb(arr[arr.length -1]);
+last(items, log);
 
 const sumNums = (x, y, cb) => cb(x + y);
+sumNums(35, 34, log);
 
 const multiplyNums = (x, y, cb) => cb(x * y);
+multiplyNums(35, 34, log);
 
 const contains = (item, list, cb) => {
   if (list.includes(item)) {
@@ -17,6 +23,7 @@ const contains = (item, list, cb) => {
     cb(false);
   }
 };
+contains('Backpack', items, log);
 
 /* STRETCH PROBLEM */
 
@@ -35,6 +42,6 @@ const removeDuplicates = (array, cb) => {
 }
 
 array = [1, 2, 3, 4, 5, 6, 6, 7, 8];
-const log = (array) => console.log(array);
+
 
 removeDuplicates(array, log);
