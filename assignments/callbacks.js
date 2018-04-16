f429ebcf17e7bedbc8cff755da01a748f0200f17
kelly <kelfro@fastmail.com>
2018-04-16 16:05:15 -0700

    callbacks - missing parenthesis

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,30 +2,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
-    return cb(arr[0]);
+    return cb(arr[0])
     
 }
 
 function getLength(arr, cb) {
     // getLength passes the length of the array into the callback.
-    return cb(arr.length);
+    return cb(arr.length)
     
 }
 
 function last(arr, cb) {
     // last passes the last item of the array into the callback.
-    return cb(arr[arr.length-1]);
+    return cb(arr[arr.length-1])
     
 }
 
 function sumNums(x, y, cb) {
     // sumNums adds two numbers (x, y) and passes the result to the callback.
-    return cb(x+y);
+    return cb(x+y)
 }
 
 function multiplyNums(x, y, cb) {
     // multiplyNums multiplies two numbers and passes the result to the callback.
-    return(cb(x*y);
+    return(cb(x*y))
 	
 }
 
