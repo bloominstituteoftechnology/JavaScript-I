c4695fa03d18736466343cc0e8dfcc402985e2d9
William VanDolah <william.vandolah@gmail.com>
2018-04-16 14:14:48 -0700

    array Challenge 3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -78,7 +78,11 @@ console.log(`Challenge 2: ${inventory[lastCar].car_make} ${inventory[lastCar].ca
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+for(i = 0; i < inventory.length; i++) {
+    carModels.push(inventory[i].car_model);
+}
+// console.log(carModels);
+console.log(`Challenge 3: ${carModels.sort()}`);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
