abc35f1004ac53b18d4444703a9186aa8376cc2e
jesuarva <jean.ariza@jesuarva.com>
2018-04-16 21:49:36 +0200

    END Challenge 3: Object Methods && Stretch Challenge

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,3 +1,5 @@
+import { POINT_CONVERSION_COMPRESSED } from "constants";
+
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
 // ==== Challenge 1: Writing Objects ==== 
@@ -74,29 +76,57 @@ console.log(interns.Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+interns.Kennan.speak = () => ("Hello, my name is " + Object.keys(interns)[1]);
+console.log(interns.Kennan.speak());
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+interns.Antonietta.multiplyNums = ( a , b ) => a * b ;
+
+console.log( interns.Antonietta.multiplyNums(3,4) );
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
-// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
-// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
-// 4. Give each of the objects the ability to speak their names using the this keyword.
+const Person = function (name, age ) {
+  this.name = name;
+  this.age = age;
+}
 
-let parent = {}
+
+const parent = new Person("Susan", 70);
+console.log(parent); //?
+
+// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
+parent.child = new Person("George", 50);
+console.log(parent.child);
+// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
+parent.child.grandchild = new Person("Sam", 30);
+console.log(parent.child.grandchild);
+// 4. Give each of the objects the ability to speak their names using the this keyword.
+Person.prototype.speak = function () {
+  return `My name is ${this.name}`;
+}
+
+
 
 // Log the parent object's name
+console.log(parent.speak());
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log(`Grandchild is ${parent.child.grandchild.name}, and is ${parent.child.grandchild.age} years old`);
 
 // Have the parent speak
+console.log(parent.speak());
 
 // Have the child speak
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
