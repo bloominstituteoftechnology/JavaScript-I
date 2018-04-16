0c8550abdb38c3fd559579e651e223719ef7bc93
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-17 02:29:50 +0800

    Answered questions on callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,23 +2,48 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 }
+
+firstItem(items, function() {
+  console.log(items[0]);
+});
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, function() {
+  console.log(items.length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
+
+last(items, function() {
+  console.log(items[items.length - 1]);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+sumNums(3, 3, function() {
+  console.log(3 + 3);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(3, 3, (product) => {
+  console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
