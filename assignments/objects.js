b7f593197751c5403238cc0a5c1dfdb1ef75746a
Brandon Benefield <bsquared18@gmail.com>
2018-04-16 15:11:38 -0400

    objects.js: Finish challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -74,9 +74,19 @@ console.log(antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+kennan.speak = function () {
+  return `Hello, my name is ${this.name}`;
+};
+
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums = function (x, y) {
+  return x * y;
+};
+
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
