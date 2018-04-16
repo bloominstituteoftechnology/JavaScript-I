fa6e59ea12b51a5d974163cad6cc38ed452d16a7
kelly <kelfro@fastmail.com>
2018-04-16 16:19:03 -0700

    arrays - I think this is mostly working

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,10 +104,7 @@ for (let i=0; i<inventory.length; i++) {
 let BMWAndAudi = [];
 for (let i=0; i<inventory.length; i++) {
     if ((inventory[i].car_make = "BMW") || (inventory[i].car_make = "Audi")){
-	BMWAndAudi.push(inventory[i].id);
+	     BMWAndAudi.push(inventory[i].id);
     };
     console.log(JSON.stringify(BMWAndAudi));
 }
-
-
-
