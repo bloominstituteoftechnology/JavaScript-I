3ad55dbec1a317a4d761648bc00c74f8ec83f0d5
kelly <kelfro@fastmail.com>
2018-04-16 15:34:24 -0700

    callbacks - still getting undefined on repl.it but maybe I am on the right track

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,35 +2,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
-    let first=arr[0];
-    cb(first);
+    cb(arr[0]);
     
 }
 
 function getLength(arr, cb) {
     // getLength passes the length of the array into the callback.
-    let lgth = arr.length;
-    cb(lgth);
+    cb(arr.length);
     
 }
 
 function last(arr, cb) {
     // last passes the last item of the array into the callback.
-    let last = arr[arr.length-1];
-    cb(last);
+    cb(arr[arr.length-1]);
     
 }
 
 function sumNums(x, y, cb) {
     // sumNums adds two numbers (x, y) and passes the result to the callback.
-    let sum = x + y;
-    cb(sum);
+    cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
     // multiplyNums multiplies two numbers and passes the result to the callback.
-    let multiply = x * y;
-    cb(multiply);
+    cb(x*y);
     
 }
 
@@ -38,14 +33,12 @@ function contains(item, list, cb) {
     // contains checks if an item is present inside of the given array/list.
     // Pass true to the callback if it is, otherwise pass false.
     for (let i=0;i<list.length;i++) {
-	if list[i] === item {
+	if (list[i] === item) {
 	    return true;
 	}
 	else {
 	    return false;
 	}
-
-	
     }
 }
 
