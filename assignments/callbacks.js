ac484d8f1235d07dd80716235e1d6f54129356b6
kelly <kelfro@fastmail.com>
2018-04-16 16:37:02 -0700

    Callbacks - I think these are right but I don't know how to check them in repl.it.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,30 +2,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
-    return cb(arr[0])
+    cb(arr[0]);
 
 }
 
 function getLength(arr, cb) {
     // getLength passes the length of the array into the callback.
-    return cb(arr.length)
+    cb(arr.length);
 
 }
 
 function last(arr, cb) {
     // last passes the last item of the array into the callback.
-    return cb(arr[arr.length-1])
+    cb(arr[arr.length-1])
 
 }
 
 function sumNums(x, y, cb) {
     // sumNums adds two numbers (x, y) and passes the result to the callback.
-    return cb(x+y)
+    cb(x+y)
 }
 
 function multiplyNums(x, y, cb) {
     // multiplyNums multiplies two numbers and passes the result to the callback.
-    return cb(x*y)
+    cb(x*y)
 
 }
 
@@ -34,10 +34,10 @@ function multiplyNums(x, y, cb) {
 	    // Pass true to the callback if it is, otherwise pass false.
 	  for (let i=0;i<list.length;i++) {
 		  if (list[i] === item) {
-		    return true;
+		    return cb(true);
 		  }
 		  else {
-		    return false;
+		    return cb(false);
 	  	}
 	  }
 	}
