4d8a5dcd544ebb90a1ba23c063b333a9e9b379bf
maribelcuales <maribelcuales@gmail.com>
2018-04-17 06:13:28 +0800

    added callbacks assignment

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,22 +2,29 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  arr = [];
+  const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+  return cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.length - 1);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
