db5b1a54692df067ecb9d9eea1a9f9647db00112
jesuarva <jean.ariza@jesuarva.com>
2018-04-16 22:55:18 +0200

    callback -> END contains(item, list, cb)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -32,7 +32,9 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb( list.includes(item) );
 }
+
 
 /* STRETCH PROBLEM */
 
