42931598fb42026e8d2c435903010a07fda436fc
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-16 15:30:57 -0400

    Completed objects.js Challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -54,9 +54,19 @@ console.log(internAntonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+internKennan.speak = function(){
+  return `Hello, my name is ${this.name}!`;
+};
+
+console.log(internKennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+internAntonietta.multiplyNums = function(x,y){
+  return x*y;
+};
+
+console.log(internAntonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
