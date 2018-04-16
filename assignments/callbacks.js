b4a78baf6503ad514d87b758f5ddf9ccb95aa607
mister-corn <code@ronaldlibago.com>
2018-04-16 15:19:45 -0700

    3:18 PM PDT: Refactored into ES6

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,35 +4,23 @@ const firstItem = (arr, cb) => cb(arr[0]);
 
 const getLength = (arr, cb) => cb(arr.length);
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
-}
+const last = (arr, cb) => cb(arr[arr.length -1]);
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
-}
+const sumNums = (x, y, cb) => cb(x + y);
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
-}
+const multiplyNums = (x, y, cb) => cb(x * y);
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+const contains = (item, list, cb) => {
   if (list.includes(item)) {
     cb(true);
   } else {
     cb(false);
   }
-}
+};
 
 /* STRETCH PROBLEM */
 
-
-function removeDuplicates(array, cb) {
+const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
