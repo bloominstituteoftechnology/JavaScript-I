e63b36a30611b1ec598d26b48131d61634cc9118
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-04-16 12:29:20 -0700

    Add print function for testing

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,34 +1,43 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
+function print(a) {
+  console.log(a);
+}
+
 // function firstItem(arr, cb) {
 //   // firstItem passes the first item of the given array to the callback function.
 //   cb(arr[0]);
 // }
 firstItem = (arr, cb) => {cb(arr[0])};
+firstItem(items, print);
 
 // function getLength(arr, cb) {
 //   // getLength passes the length of the array into the callback.
 //   cb(arr.length);
 // }
 getLength = (arr, cb) => {cb(arr.length)};
+getLength(items, print);
 
 // function last(arr, cb) {
 //   // last passes the last item of the array into the callback.
 //   cb(arr[arr.length-1]);
 // }
 last = (arr,cb) => {cb(arr[arr.length-1])};
+last(items, print);
 
 // function sumNums(x, y, cb) {
 //   // sumNums adds two numbers (x, y) and passes the result to the callback.
 //   cb(x+y);
 // }
 sumNums = (x, y, cb) => {cb(x+y)};
+sumNums(3, 4, print);
 
 // function multiplyNums(x, y, cb) {
 //   // multiplyNums multiplies two numbers and passes the result to the callback.
 //   cb(x*y);
 // }
 multiplyNums = (x, y, cb) => {cb(x*y)};
+multiplyNums(3, 4, print);
 
 // function contains(item, list, cb) {
 //   // contains checks if an item is present inside of the given array/list.
@@ -36,6 +45,7 @@ multiplyNums = (x, y, cb) => {cb(x*y)};
 //   cb(list.includes(item));
 // }
 contains = (item, list, cb) => {cb(list.includes(item))};
+contains('pen', items, print);
 
 /* STRETCH PROBLEM */
 
@@ -61,3 +71,4 @@ removeDuplicates = (array, cb) => {
   }
   cb(noDuplicates);
 }
+removeDuplicates(items, print);
