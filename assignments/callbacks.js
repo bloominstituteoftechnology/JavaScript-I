52c141a6efbe5e9095997aa8cd56e952204f0d41
AmyShackles <amyshackles@gmail.com>
2018-04-16 15:47:51 -0700

    Finished callback problems with the exception of stretch goal

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,29 +1,38 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
-  // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0])
 }
+firstItem([1,5,4,3],function(arr){return arr + 1})
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+getLength([1,2,4],function(arr){return arr * 2})
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1])
 }
+last([1,5,7],function(arr){return arr - 5})
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y)
 }
+sumNums(5, 4, function(x){return x*5})
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
+multiplyNums(5, 4, function(x){return x + 1})
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
+contains(7, [5, 4, 2], function(x){return x})
 
 /* STRETCH PROBLEM */
 
