469fc546b1144c1d35edad7a1585a83f433f99a8
masontmorrow <masontmorrow@gmail.com>
2018-04-17 00:40:10 +0200

    Worked with Luis on Arrays, Callbacks, and Object

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,34 +1,68 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const numbers = [1, 1, 2, 2, 3, 3, 4, 4];
+
+let numOne = function (arr) {
+  return arr;
+};
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item) === true) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
+
+
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let newArr = [];
+  for (i = 0; i < array.length; i++) {
+    newArr.push(array[i]);
+  }
+    for (let a = i + 1; a < array.length; a++) {
+      if (a === i) {
+        newArr.pop(array[a]);
+      }
+    
+  }
+
+  return cb(newArr);
 }
+
+console.log(removeDuplicates(numbers, numOne));
+
+// for (let a = i + 1; a < array.length; a++) {
+//   if (a === i)
