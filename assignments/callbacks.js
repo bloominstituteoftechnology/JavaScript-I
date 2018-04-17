5bdf0c0b92a28c2ff8106ab3b2722951685156ab
mr-burton <taylormade800@gmail.com>
2018-04-16 20:48:48 -0700

    solving callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,19 +1,34 @@
+
+let work = function(output) {
+  console.log(output);
+}
+
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
+firstItem(items, work)
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, work);
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, work);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  
 }
 
 function multiplyNums(x, y, cb) {
@@ -32,3 +47,5 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+ 
+
