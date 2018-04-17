99facc648c3cad20db6b8d1c93c671b703dcc594
maribelcuales <maribelcuales@gmail.com>
2018-04-18 02:48:57 +0800

    revised and submitted Objects assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,36 +10,29 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
-// Write your intern objects here:
 let mitzi = {
   "id": 1,
   "name": "Mitzi",
   "email": "mmelloy0@psu.edu",
-  "gender": "F"
+  "gender": "F",
 };
 let kennan = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
 };
 let keven = {
   "id": 3,
   "name": "Keven",
   "email": "kmummery2@wikimedia.org",
-  "gender": "M"
+  "gender": "M",
 };
 let gannie = {
   "id": 4,
   "name": "Gannie",
   "email": "gmartinson3@illinois.edu",
-  "gender": "M"
+  "gender": "M",
 };
 let antonietta = {
   "id": 5,
@@ -79,16 +72,37 @@ console.log(antonietta.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {
+  "name": "Susan",
+  "age": 70,
+  "speak": function() {
+    return "My name is: " + this.name;
+  },
+  "child": {
+    "name": "George", 
+    "age": "50",
+    "speak": function() {
+      return "My name is: " + this.name;
+    },
+    "grandchild": {
+      "name": "Sam",
+      "age": "30",
+      "speak": function() {
+        return "My name is: " + this.name;
+      }
+    }
+  }
+};
 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name, parent.child.grandchild.age);
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandschild.speak());
