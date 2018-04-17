c2e36ffcc682cad435ea0f9a948b63f87b5c3b08
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-17 23:44:47 +0700

    removed return statements

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,33 +2,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-  return cb(arr[0]);
+  cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
+  cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(list.includes(item));
+  cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
@@ -47,5 +47,5 @@ function removeDuplicates(array, cb) {
       lastItem = array[i];
     }
   }
-  return cb(dupeFreeArr);
+  cb(dupeFreeArr);
 }
