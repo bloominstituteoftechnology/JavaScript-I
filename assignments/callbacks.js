aec3118445701ed52c5e84af1311891e6ac6d354
mr-burton <taylormade800@gmail.com>
2018-04-16 20:51:17 -0700

    solving callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -28,16 +28,20 @@ last(items, work);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  
+  return cb(x + y);
 }
+sumNums(5, 6, work);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(7, 3, work);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return 
 }
 
 /* STRETCH PROBLEM */
