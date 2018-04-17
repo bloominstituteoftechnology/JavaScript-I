f42b6e6fd584fd72784d8ec29077ddee12997996
Brandon Hopper <BrandonHopper0@gmail.com>
2018-04-17 13:21:08 -0400

    added json stringfy

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -119,7 +119,7 @@ for(i = 0; i < inventory.length; i++)
     if(inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi"){
         BMWAndAudi.push(inventory[i].car_make)
     }
-console.log(BMWAndAudi);
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
