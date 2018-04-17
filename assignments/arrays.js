4b509204401dcd69e8c0047aba46c5e01b3ce1da
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-17 23:31:54 +0700

    added car to lastCar variable in challenge 2. The instructions were unclear on what to do with that variable.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -70,7 +70,7 @@ console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${i
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = inventory[inventory.length - 1];
-console.log(`${inventory[inventory.length - 1].car_make} ${inventory[inventory.length - 1].car_model}`);
+console.log(`${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
