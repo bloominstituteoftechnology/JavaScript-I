ba45486e4e107f5727ef1289c27a8c3d682a68b6
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-16 19:07:06 -0700

    challenges completed

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -102,8 +102,10 @@ console.log(carYears);
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
 
-for (let i = 0; i<inventory.length; i++) {
-    oldCars.splice(carYears < 2000); //pretty sure my mistake is on this line
+for (let i = 0; i<carYears.length; i++) {
+    if (carYears[i]<2000) {
+      oldCars.push(carYears[i]);
+    }
   }
 
 console.log(oldCars.length); 
@@ -111,7 +113,15 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
+
+for (let i=0; i<inventory.length; i++) {
+    if (inventory[i].car_make === "Audi" || inventory[i].car_make === "BMW"); {
+        BMWAndAudi.push(inventory[i]);
+       
+    }
+}
+
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
