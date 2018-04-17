ac1802fdee32e4fab7127773d37abafcd30688a6
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-17 20:02:08 +0800

    Added answers to callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,12 +1,12 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'yo-yo'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-  return cb(arr[0]);
+  cb(arr[0]);
 }
 
-firstItem(items, function() {
-  return items[0];
+firstItem(items, function(first) {
+  console.log(first);
 });
 
 function getLength(arr, cb) {
@@ -14,8 +14,8 @@ function getLength(arr, cb) {
   cb(arr.length);
 }
 
-getLength(items, function() {
-  console.log(items.length);
+getLength(items, function(length) {
+  console.log(length);
 });
 
 function last(arr, cb) {
@@ -23,8 +23,8 @@ function last(arr, cb) {
   cb(arr[arr.length - 1]);
 }
 
-last(items, function() {
-  console.log(items[items.length - 1]);
+last(items, function(lastItem) {
+  console.log(lastItem);
 });
 
 function sumNums(x, y, cb) {
@@ -32,28 +32,50 @@ function sumNums(x, y, cb) {
   cb(x + y);
 }
 
-sumNums(3, 3, function() {
-  console.log(3 + 3);
+sumNums(10, 15, function(total) {
+   console.log(total);
 });
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  cb(x * y);
+   cb(x * y);
 }
 
-multiplyNums(3, 3, (product) => {
-  console.log(product);
+multiplyNums(10, 15, function(total) {
+   console.log(total);
 });
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return cb(true);
+    }
+  }
+  return cb(false);
 }
+
+contains('Bats', items, function(flag) {
+  console.log(flag); // should be false
+});
+
+contains('Notebook', items, function(flag) {
+  console.log(flag); // should be true
+});
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+function removeDuplicates(arr, cb) {
+  const seen = {};// a map of the keys. This will hold the values as a bool if 'seen' in the given array.
+  for (let i = 0; i < arr.length; i++) { // loop over array list and assign the object a 'key' of the given item w/ a value of 'true'
+    seen[arr[i]] = true; // this will set the key/value pair of each item found in the array to `true`. 
+    // It will not duplicate the item because Objects can only have one key of of each 'like' so. `yo-yo` will only be added 1 time etc. 
+  }
+  cb(Object.keys(seen));// Object.keys generates a list as an array of each key in the object passed to it.
+  // Could do the same of each value with `Object.values` :) 
 }
+
+removeDuplicates(items, function(dupeFreeArray) { 
+  console.log(dupeFreeArray);
+});
