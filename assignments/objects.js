0966a411d6c74b5c62692cf91827cfc6d7f82f39
Chris Honda <christopher.honda92@gmail.com>
2018-04-17 09:21:52 -0700

    updated part of challenge 3 after solution review

--- assignments/objects.js
+++ assignments/objects.js
@@ -69,7 +69,11 @@ console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-console.log("Hello, my name is " + Kennan.name + "!");
+Kennan.speak = function() {
+  return "Hello, my name is Kennan!";
+}
+
+console.log(Kennan.speak);
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 console.log(Antonietta.multiplyNums(3,4));
