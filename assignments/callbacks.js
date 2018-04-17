927e7d9280a0e22eaffb2fe3746862a121886ad9
kelly <kelfro@fastmail.com>
2018-04-17 15:46:15 -0700

    callbacks - I called the functions and used console.log to check that my callbacks worked

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,34 +2,45 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
-    cb(arr[0]);
-
+    return cb(arr[0]);
 }
+
+
+firstItem([1,2,3], console.log);
 
 function getLength(arr, cb) {
     // getLength passes the length of the array into the callback.
-    cb(arr.length);
+    return cb(arr.length);
 
 }
+
+getLength([5,6,7], console.log);
 
 function last(arr, cb) {
     // last passes the last item of the array into the callback.
-    cb(arr[arr.length-1])
+    return cb(arr[arr.length-1]);
 
 }
+
+last(items, console.log);
 
 function sumNums(x, y, cb) {
     // sumNums adds two numbers (x, y) and passes the result to the callback.
-    cb(x+y)
+    return cb(x+y);
 }
+
+sumNums(5,6,console.log);
+
 
 function multiplyNums(x, y, cb) {
     // multiplyNums multiplies two numbers and passes the result to the callback.
-    cb(x*y)
+    return cb(x*y);
 
 }
 
-	function contains(item, list, cb) {
+multiplyNums(5,6,console.log);
+
+function contains(item, list, cb) {
 	    // contains checks if an item is present inside of the given array/list.
 	    // Pass true to the callback if it is, otherwise pass false.
 	  for (let i=0;i<list.length;i++) {
@@ -41,6 +52,9 @@ function multiplyNums(x, y, cb) {
 	  	}
 	  }
 	}
+
+
+contains('Pencil', items, console.log);
 
 	/* STRETCH PROBLEM */
 
