e251e7cf53df8a1f321acc89c68da546fc063c83
Tai <bowlofryce@gmail.com>
2018-05-21 18:41:38 -0500

    Forgot to add .length in for loop.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -101,11 +101,9 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-for(let i = 0; i < inventory; i++){
-  if(inventory[i].car_make == 'BMW' || inventory[i].car_make == 'Audi'){
+for(let i = 0; i < inventory.length; i++){
+  if(inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi'){
     BMWAndAudi.push(inventory[i]);
   }
 }
-let cars = JSON.stringify(BMWAndAudi);
-console.log(BMWAndAudi);
 console.log(JSON.stringify(BMWAndAudi));
