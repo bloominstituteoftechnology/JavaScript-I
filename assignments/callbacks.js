e7c6271b768a7c051878239dd6c29b083aa3abab
Allen Hai <allenhai03@gmail.com>
2018-05-21 17:15:53 -0500

    refactored all functions in callback.js to use es6 arrow functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,31 +1,32 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-function firstItem(arr, cb) {
+
+let firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
   cb(arr[0])
 }
 
-function getLength(arr, cb) {
+let getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
   cb(arr.length)
 }
 
-function last(arr, cb) {
+let last = (arr, cb) => {
   // last passes the last item of the array into the callback.
   cb(arr[arr.length - 1])
 }
 
-function sumNums(x, y, cb) {
+let sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   cb(x + y)
 }
 
-function multiplyNums(x, y, cb) {
+let multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   cb(x * y)
 }
 
-function contains(item, list, cb) {
+let contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   cb(list.includes[item])
@@ -33,13 +34,13 @@ function contains(item, list, cb) {
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+let removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
   let dupFreeArr = []
   array.forEach(item => {
     if(!dupFreeArr.includes[item]) dupFreeArr.push(item)
-  }
+  })
   cb(dupFreeArr)
 }
