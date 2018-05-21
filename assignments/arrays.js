16c2d86a49201ab7ea2f00d7dc49cd018da70217
Katie Gorbell <katiegorbell@gmail.com>
2018-05-21 17:45:11 -0500

    fixed array challenge 4

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,13 +75,15 @@ console.log(lastCar);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-carModels = inventory.sort(inventory["car_model"]);
+carModels = inventory.sort(inventory.car_model);
 console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-carYears = inventory.car_year;
+for (let i=0; i < inventory.length; i++) {
+    carYears[i] = inventory[i].car_year;
+}
 console.log(carYears);
 
 // ==== Challenge 5 ====
