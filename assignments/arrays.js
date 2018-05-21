f801681b9cceb151bbf5b319410578a8cfa929f0
CLN <cln@Sags-MacBook-Pro.local>
2018-05-21 17:54:09 -0400

    Finish challenge 3 in array, .

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,13 +72,16 @@ for (i=0; i< inventory.length; i++) {
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory.length-1;
+console.log(inventory[lastCar]);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+for (i=0; i<inventory.length; i++){
+    carModels.push(inventory[i].car_model);
+}
+console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
