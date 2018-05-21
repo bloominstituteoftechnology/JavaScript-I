e9d627928925962c0161e1798b1f0680d4119ad7
M.Real <mreal126@protonmail.com>
2018-05-21 19:35:13 -0400

    Syntax fix Objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -32,7 +32,7 @@ let Kennan = {
   "gender": "M",
   //Method
   "speak": function() {
-    console.log("Hello, my name is Keenan!")
+    return ("Hello, my name is Keenan!");
   }
 }
 let Keven = {
@@ -81,7 +81,7 @@ console.log(Antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-console.log(Kennan.speak())
+console.log(Kennan.speak()); //?
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
