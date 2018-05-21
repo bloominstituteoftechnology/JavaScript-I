dd9667c4900fc4b5633b129acfbde08cb1ee8869
tangled-tessellations <thepolitepanda@gmail.com>
2018-05-21 13:46:29 -0700

    Created parent object with nested child and grandchild

--- assignments/objects.js
+++ assignments/objects.js
@@ -61,22 +61,33 @@ let intern5 = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(intern1.name);
 // Kennan's ID
-
+console.log(intern2.id);
 // Keven's email
-
+console.log(intern2.email);
 // Gannie's name
-
+console.log(intern4.name);
 // Antonietta's Gender
+console.log(intern5["gender"]);
+
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+intern2.speak = function () {
+  console.log(`Hello, my name is ${this.name}`);
+}
 
+intern2.speak();
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
+intern5.multiplyNums = function (a,b) {
+  return (a*b);
+}
+
+console.log(intern5.multiplyNums(2,5));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ====
@@ -86,7 +97,24 @@ let intern5 = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {
+  "name": "Susan",
+  "age": 70,
+  speak: function () {
+    console.log(`My name is $(this.name)`);
+  },
+  "child": {"name": "George", "age": 50,
+    speak: function () {
+      console.log(`My name is $(this.name)`);
+    },
+    "grandchild": { "name": "Sam", "age": 30,
+      speak: function () {
+        console.log(`My name is $(this.name)`);
+      }
+    }
+  }
+}
+
 
 // Log the parent object's name
 
