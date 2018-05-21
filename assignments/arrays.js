405875260125dc6d8b95b964255acc02a406b55c
Daniel Chamorro <dan@dcitllc.com>
2018-05-21 18:47:59 -0400

    Got stuck on Challenge 4 arrays. Final push ug.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -92,11 +92,10 @@ for (let i = 0; i < inventory.length; i++) {
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-// let carModels = [];
+let carModels = [];
 for (let i = 0; i < inventory.length; i++) {
-    let carModels = [];
-    let carModels = inventory[i].car_model; //?
-    carModels.sort();
+    inventory[i].car_model; //?
+
 }
 
 // ==== Challenge 4 ====
