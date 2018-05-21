5d74a85fc22a0c72ee5afcb0570f8e008429c413
tangled-tessellations <thepolitepanda@gmail.com>
2018-05-21 14:44:31 -0700

    Completed callback function challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,29 +1,53 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+function callback (item) {
+  console.log(item);
+}
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 }
+
+firstItem(items, callback);
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items,callback);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr.pop());
 }
+
+last(items,callback);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 }
+
+sumNums(10,5,callback);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 }
+
+multiplyNums(2,4,callback);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item))
+  { cb(true); }
+  else {
+  { cb(false);  }
+  }
 }
+
+contains('yo-yo', items, callback);
 
 /* STRETCH PROBLEM */
 
@@ -31,4 +55,8 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  cb(Array.from(new Set(array)));
 }
+
+let duplicates = [1,1,2,2,3,3,4,4,5,5];
+removeDuplicates(duplicates, callback);
