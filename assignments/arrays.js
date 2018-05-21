e70aff76bc7506b0b39ceb49d458a8b98754ea69
Terrance Danckert <tbarnett2020@gmail.com>
2018-05-21 18:32:34 -0400

    Finished***

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -82,10 +82,6 @@ for (i = 0; i < inventory[i].length; i++) {
   }
 }
 
-console.log(
-  `Car 33 is a *car year goes here* *car make goes here* *car model goes here*`
-);
-
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = (function(selection) {
@@ -103,7 +99,7 @@ for (i = 0; i < inventory.length; i++) {
   carModels.push(inventory[i].car_model);
 }
 
-console.log();
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
@@ -113,7 +109,7 @@ for (i = 0; i < inventory.length; i++) {
   carYears.push(inventory[i].car_year);
 }
 
-console.log();
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -124,7 +120,7 @@ for (i = 0; i < inventory.length; i++) {
   }
 }
 
-console.log();
+console.log(oldCars);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
