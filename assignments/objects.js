3203283b04f03256a90eca5a9237942e01ccb8fd
raymondgrc <raymond_grc@yahoo.com>
2018-05-21 14:46:04 -0500

    Raymond Garcia; Javascript 1

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
@@ -18,22 +18,57 @@ let example = {
 }
 
 // Write your intern objects here:
+let example1 = {
+  "id": 1,
+  "name" : "Mitzi",
+  "email" : "mmelloy0@psu.edu",
+  "gender" : "f"
+}
+let example2 = {
+  "id": 2,
+  "name" : "Kennen",
+  "email" : "kdiben1@tinypic.com",
+  "gender" : "m",
+  "speak" : function(){
+    console.log("Hello, my name is " + example2.name)
+  }
+}
+let example3 = {
+  "id": 3,
+  "name" : "Keven",
+  "email" : "kmummery2@wikimedia.org",
+  "gender" : "m"
+}
+let example4 = {
+  "id": 4,
+  "name" : "Gannie",
+  "email" : "gmartinson3@illinois.edu",
+  "gender" : "m"
+}
+let example5 = {
+  "id": 5,
+  "name" : "Antonietta",
+  "email" : "adaine5@samsung.com",
+  "gender" : "f",
+  "multiplyNums": function(num1, num2){
+    return num1 * num2;
+  }
+}
 
-
-// ==== Challenge 2: Reading Object Data ==== 
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(example1.name);
 // Kennan's ID
-
+console.log(example2.id);
 // Keven's email
-
+console.log(example3.email);
 // Gannie's name
-
+console.log(example4.name);
 // Antonietta's Gender
-
-// ==== Challenge 3: Object Methods ==== 
+console.log(example4.gender);
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
@@ -42,7 +77,7 @@ let example = {
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
