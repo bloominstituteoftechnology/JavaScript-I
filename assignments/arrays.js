a648cc86a53cf9f7d1da6c90eeda9e19c2187501
JacobLayton <jlayjlay3@gmail.com>
2018-05-21 23:44:33 -0700

    arrays done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -76,7 +76,7 @@ console.log(inventory[inventory.length - 1].car_make, inventory[inventory.length
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 for (let i = 0; i < inventory.length; i++) {
-    carModels.push(inventory[i].car_model)
+    carModels.push(inventory[i].car_model);
 }
 
 console.log(carModels.sort());
@@ -84,17 +84,32 @@ console.log(carModels.sort());
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i].car_year);
+}
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-console.log(); 
+for (let i = 0; i < carYears.length; i++) {
+    if (carYears[i] < 2000) {
+        oldCars.push(carYears[i]);
+    }
+}
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make === "BMW") {
+        BMWAndAudi.push(inventory[i])
+    }else if (inventory[i].car_make === "Audi") {
+        BMWAndAudi.push(inventory[i])
+    }
+}
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
