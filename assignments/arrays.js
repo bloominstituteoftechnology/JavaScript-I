e76c5def5eb8d2c71cef9b4671fdbde2c5a45ab9
emaquino44 <prexy44@yahoo.com>
2018-05-21 17:49:01 -0700

    working on array assignment

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,20 +73,29 @@ for(let i = 0; i < inventory.length; i++);
 let lastCar = 0;
 console.log();
 inventory[inventory.length-1];
+
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 console.log();
 inventory.sort();
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 console.log();
+let carYears = ['car_year'];
+console.log("what is this");
+for(let i = 0; i < inventory.length; i++);
+  inventory[9][carYears];
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
 console.log();
+
+
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
