70e901508fa7a49a85a65495386f78435ce0f3da
emaquino44 <prexy44@yahoo.com>
2018-05-22 11:25:27 -0700

    some work on callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-}
+  cb(items[0]),
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
