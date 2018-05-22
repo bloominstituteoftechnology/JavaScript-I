a4ce4def5b1508d166a75b4fab4aae41677be96a
Adrian Adames <adrianadames@gmail.com>
2018-05-22 01:29:52 -0400

    Finished array assignment challenge 3, so array assignment challenges 1, 2, 3, 4, and 5 are done. Tabling challenge 6 until next work day.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -82,7 +82,13 @@ console.log(lastCar);
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 // let carModels = [];
 // console.log();
+let carModelsSimpleWay = [];
 
+for (let i = 0; i < inventory.length; i++) {
+  carModelsSimpleWay.push(inventory[i].car_model)
+}
+// carModelsSimpleWay = 
+console.log(carModelsSimpleWay.sort());
 
 
 
@@ -273,10 +279,34 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-console.log(); 
+for (let i=0; i< inventory.length; i++) {
+    if (inventory[i].car_year < 2000) {
+        oldCars.push(inventory[i].car_year)
+    }
+}
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi =[];
-console.log();
 
+//Attempt 1
+
+// let BMWAndAudi =[];
+// for (let i=0; i < inventory.length; i++) {
+//  if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
+//      BMWAndAudi.push(inventory[i].car_make)
+//  }
+// }
+// console.log(BMWAndAudi);
+// JSON.stringify(BMWAndAudi);
+
+//Attempt 2
+
+let BMWAndAudi =[];
+for (let i=0; i < inventory.length; i++) {
+ if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
+     BMWAndAudi.push(inventory[i])
+ }
+}
+console.log(BMWAndAudi);
+JSON.stringify(BMWAndAudi);
