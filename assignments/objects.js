6d9f7e6944ea13d80786599e0c30a7f63603c649
emaquino44 <prexy44@yahoo.com>
2018-05-21 19:33:02 -0700

    added solutions

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,90 +11,78 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-};
-
-// Write your intern objects here:
-let internList = [{
 	"id": 0,
-	"email": "mmelloy0@psu.edu",
-	"first_name": "Mitzi",
+	"name": "Example",
+	"email": "examples@you.edu",
 	"gender": "F"
-	}, {
+  };
+
+  let mitzi = {
 	"id": 1,
-	"email": "kdiben1@tinypic.com",
-	"first_name": "Kennan",
-	"gender": "M",
-	"speak": function() {
-		console.log("what's up");
-		return "what's up" + internList["first_name"];
-	}
-}, {
+	"name": "Mitzi",
+	"email": "mmelloy0@psu.edu",
+	"gender": "F"
+  };
+  console.log(mitzi.name)
+
+  let kennan = {
 	"id": 2,
-	"email": "kmummery2@wikimedia.org",
-	"first_name": "Keven",
-	"gender": "F"
-}, {
-	"id": 3,
-	"email": "gmartinson3@illinois.edu",
-	"first_name": "Gannie",
+	"name": "Kennan",
+	"email": "kdiben1@tinypic.com",
 	"gender": "M"
-}, {
+  }
+console.log(kennan.id)
+  let keven = {
+	"id": 3,
+	"name": "Keven",
+	"email": "kmummery2@wikimedia.org",
+	"gender": "M"
+  }
+console.log(keven.email)
+  let gannie = {
 	"id": 4,
-	"email": "daine5@samsung.com",
-	"first_name": "Antonietta",
+	"name": "Gannie",
+	"email": "mmelloy0@psu.edu",
+	"gender": "M"
+  }
+console.log(gannie.name)
+  let antonietta = {
+	"id": 1,
+	"name": "Antonietta",
+	"email": "mdaine5@samsung.com",
 	"gender": "F"
-}];
-
-// for(let i = 0; i < internList.length; i++){
-//   internList[i]["id"]
-// }
-
+  }
+  console.log(antonietta.gender)
 // ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-internList[0].first_name;
-
+console.log(mitzi."name")
 
 //Kennen's ID
-internList[1].id;
+
 
 // Keven's email
-internList[2].email;
+
 
 // Gannie's name
-internList[3].first_name;
+
 
 // Antonietta's Gender
-internList[4].gender;
+
 
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
 
-let internList = {
-
-	"id": 1,
-	"email": "kdiben1@tinypic.com",
-	"first_name": "Kennan",
-	"gender": "M",
-	"speak": function() {
-		return "what's up" + internList["first_name"];
-  }
+kennan.speak() = function () {
+	return "Hello, my name is Kennan!";
 };
-internList.speak();
-
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-function multiplyNums(a,b) {
-	return a * b
-}
+
 
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
@@ -106,21 +94,43 @@ function multiplyNums(a,b) {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {
+	"name": "Susan",
+	"age": 70,
+	"speak": function() {
+	  return "My name is: " + this.name; 
+	},
+	"child": {
+	  "name": "George",
+	  "age": 50,
+	  "speak": function() {
+		return "My name is: " + this.name; 
+	  },
+	  "grandchild": {
+		"name": "Sam",
+		"age": 30,
+		"speak": function() {
+		  return "My name is: " + this.name; 
+		}
+	  }
+	}
+  }
+  
 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.name)
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name, parent.child.grandchild.age)
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
 
-console.log("hello world");
+
 
 
 
