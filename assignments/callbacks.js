5490e28ee2966c4055d135963566aaf6347d87b6
snowcoding <vimalshah77@gmail.com>
2018-05-22 17:23:07 -0700

    Modifed callbacks to take into account the calling of  the function!

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -5,31 +5,44 @@ function firstItem(arr, cb) {
   cb(arr[0]);
 }
 
+firstItem(items, item => {console.log(item)})
+
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   cb(arr.length);
 }
+
+getLength(items, length => {console.log(length)})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   cb(arr[arr.length-1]);
 }
 
+last(items, length => {console.log(length)})
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   cb(x+y);
 }
 
+sumNums(2,4,sum => {console.log(sum)})
+
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   cb(x*y);
 }
+multiplyNums(2,4,m => {console.log(m)})
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   cb((list.indexOf(item) === -1) ? false : true);
 }
+contains('Pencil',items, c => {console.log(c)})
 
 /* STRETCH PROBLEM */
 
@@ -37,10 +50,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  let rd = array.filter(el => array.indexOf(el) == array.lastIndexOf(el));
+  let rd = array.filter(function(item, pos,arr) {
+    return arr.indexOf(item) == pos;
+  })
+
   cb(rd);
 }
 
-function cb(x){
-  console.log(x);
-}
+removeDuplicates([1,2,1,2,3,4],c => {console.log(c)})
