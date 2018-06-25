59ba02cf590f8738e6b030f405102a251f973086
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-06-25 14:41:48 -0400

    first comment it of the day

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
@@ -19,8 +19,44 @@ let example = {
 
 // Write your intern objects here:
 
+let Mitzi ={
+"id":0,
+"name":"Mitzi F",
+"email":"mmelloy@psu.edu",
+"gender":"F"
+}
 
-// ==== Challenge 2: Reading Object Data ==== 
+let Kennan = {
+  "id":1,
+"name":"Kennan M",
+"email":"kdiben1@tinypic.com",
+"gender":"M"
+}
+
+let Keven = {
+  "id":2,
+"name":"Keven M",
+"email":"kmummery2@wikimedia.org",
+"gender":"M"
+}
+
+let Gannie = {
+  "id":3,
+"name":"Gannie M",
+"email":"gmartinson3@illinois.edu",
+"gender":"F"
+}
+
+
+let Antonietta = {
+  "id":4,
+"name":"AntoniettaF",
+"email":"adaine5@samsung.com",
+"gender":"F"
+}
+
+
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
@@ -33,7 +69,7 @@ let example = {
 
 // Antonietta's Gender
 
-// ==== Challenge 3: Object Methods ==== 
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
@@ -42,7 +78,7 @@ let example = {
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
