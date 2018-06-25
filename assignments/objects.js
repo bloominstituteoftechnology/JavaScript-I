ce039cccec01411622ea65e7236860eabfde1820
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-06-25 17:00:17 -0400

    still working with arrays and call backs

--- assignments/objects.js
+++ assignments/objects.js
@@ -53,6 +53,9 @@ let Antonietta = {
 "name":"AntoniettaF",
 "email":"adaine5@samsung.com",
 "gender":"F"
+multiplyNums: function(num1, num2) {
+  return num1 * num2;
+  }
 }
 
 
@@ -60,21 +63,32 @@ let Antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+name : 'Mitzi'
 
 // Kennan's ID
+id:1
 
 // Keven's email
 
+email: 'kmummery2@wikimedia.org'
+
 // Gannie's name
 
+name: 'Gannie'
+
 // Antonietta's Gender
+
+gender:'F'
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+name : 'Kennan'
+speak: 'Hello , my name is Kennan!';
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
+
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
