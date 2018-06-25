6022b8a53d523717f7884d108e83d8a7abc98c2e
keegn <keegan.t.burkett@gmail.com>
2018-06-25 16:03:52 -0500

    finished callbacks.js
    working on callbacks.js stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,27 +2,42 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+   cb(arr[0]);
 }
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let product = x * y;
+  cb(product);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let thing of list) {
+    if (item === thing) {
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
 }
 
 /* STRETCH PROBLEM */
