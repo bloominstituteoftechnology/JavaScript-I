25f85d7d3d931c880e1c8465802a77099a6c730b
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-25 17:23:45 -0500

    completed challenges one and two on cbs

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,12 +2,13 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-  cb(arr[0]);
+  return cb(arr[0]);
 }
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
