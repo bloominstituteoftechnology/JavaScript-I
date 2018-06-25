d299e31b2dd093cc8b08ec09199732c743f912dd
sliw <slioweng@gmail.com>
2018-06-26 04:59:49 +0800

    Steven Lai

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,27 +2,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const sum = x + y;
+  return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x * y;
+  return cb(product);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.indexOf(item)) {
+    return cb(false);  
+  }
+
+  return cb(true);
 }
 
 /* STRETCH PROBLEM */
@@ -31,4 +43,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let noDuplicatedArray = new Set(array);
+  return cb(noDuplicatedArray);
 }
+
--- assignments/objects.js
+++ assignments/objects.js
@@ -1,4 +1,7 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
+function log (x) {
+  console.log(x);
+}
 
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
@@ -10,36 +13,73 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+// let example = {
+//   "id": 0,
+//   "name": "Example",
+//   "email": "examples@you.edu",
+//   "gender": "F"
+// }
 
 // Write your intern objects here:
+let Mitzi = {
+  'id': 1,
+  'name': 'Mitzi',
+  'email': 'mmelloy0@psu.edu',
+  'gender': 'F'
+}
 
+let Kennan = {
+  'id': 2,
+  'name': 'Kennan',
+  'email': 'kdiben1@tinypic.com',
+  'gender': 'M'
+}
+
+let Keven = {
+  'id': 3,
+  'name': 'Keven',
+  'email': 'kmummery2@wikimedia.org',
+  'gender': 'M'
+}
+
+let Gannie = {
+  'id': 4,
+  'name': 'Gannie',
+  'email': 'gmartinson3@illinois.edu',
+  'gender': 'M'
+}
+
+let Antonietta = {
+  'id': 4,
+  'name': 'Antonietta',
+  'email': 'adaine5@samsung.com',
+  'gender': 'F'
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+log(Mitzi.name);
 // Kennan's ID
-
+log(Kennan.id);
 // Keven's email
-
+log(Keven.email);
 // Gannie's name
-
+log(Gannie.name);
 // Antonietta's Gender
-
+log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+Kennan.speak = function () {
+  log(`Hello, my name is ${this.name}!`);
+}
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+Antonietta.multiplyNums = function (x , y) {
+  return x * y ;
+}
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
@@ -49,16 +89,37 @@ let example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {
+  'name': 'Susan',
+  'age': 70,
+  speak: function () {
+    log(this.name);
+  },
+  child: {
+    'name': 'George',
+    'age': 50,
+    speak: function () {
+      log(this.name);
+    },
+    grandchild: {
+      'name': 'Sam',
+      'age': 30,
+      speak: function () {
+        log(this.name);
+      }
+    }
+  }
+}
 
 // Log the parent object's name
-
+log(parent.name);
 // Log the child's age
-
+log(parent.child.age);
 // Log the name and age of the grandchild
-
+log(parent.child.grandchild.name, parent.child.grandchild.age);
 // Have the parent speak
-
+parent.speak();
 // Have the child speak
-
+parent.child.speak();
 // Have the grandchild speak
+parent.child.grandchild.speak();
