7b82828719152c60b465095bff989f81a5aed71a
Trevor <trevorfehrman@gmail.com>
2018-06-25 13:44:23 -0700

    Entire Project Complete working on stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,18 +2,22 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
@@ -23,7 +27,16 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  function contains(item, list, cb) {
+    for (let i=0; i<list.length; i++) {
+      if (item === list[i]) {
+        cb(true);
+        return;
+      }
+    }
+    cb(false);
+  }
+  contains("trevor", ["ash", "trevor"], console.log);
 
 /* STRETCH PROBLEM */
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -54,20 +54,39 @@ let example = {
 
 // Mitzi's name
 
+console.log(mitziF.name);
+
 // Kennan's ID
+
+console.log(kennanM.id);
 
 // Keven's email
 
+console.log(kevenM.email);
+
 // Gannie's name
+
+console.log(gannieM.name);
 
 // Antonietta's Gender
 
+console.log(antoniettaF.gender);
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+kennanM.speak = function() {
+  return "Hello, my name is Kennan!";
+}
+
+console.log(kennanM.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+antoniettaF.multiplyNums = function(x,y) {
+  return x*y;
+}
+console.log(antoniettaF.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
