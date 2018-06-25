8592aa9afecc0c0985a9c512ee5219d35a46b84f
keegn <keegan.t.burkett@gmail.com>
2018-06-25 17:16:51 -0500

    added logs to test functions
    everyting should be complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,44 +1,53 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
+// my callback test function takes an item and logs it to the console
+function cb(item) {
+  console.log(item);
+}
+
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
    cb(arr[0]);
 }
+console.log(firstItem(items, cb));
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
     cb(arr.length);
 }
+console.log(getLength(items, cb));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
     cb(arr[arr.length - 1]);
 }
+console.log(last(items, cb));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   let sum = x + y;
   cb(sum);
 }
+console.log(sumNums(10, 40, cb));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   let product = x * y;
   cb(product);
 }
+console.log(multiplyNums(10, 20, cb));
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-  for (let thing of list) {
-    if (item === thing) {
-      cb(true);
-    } else {
-      cb(false);
-    }
-  }
+	// contains checks if an item is present inside of the given array/list.
+	// Pass true to the callback if it is, otherwise pass false.
+	if (list.includes(item)) {
+		cb(true);
+	} else {
+		cb(false);
+	}
 }
+console.log(contains('Notebook', items, cb));
 
 /* STRETCH PROBLEM */
 
@@ -49,3 +58,6 @@ function removeDuplicates(array, cb) {
   let duplicateFree = Array.from(new Set(array));
   cb(duplicateFree);
 }
+console.log(removeDuplicates(items, cb));
+
+
