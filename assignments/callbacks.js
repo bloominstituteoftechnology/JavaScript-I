6aecf9938b67314e2c119e7c24cb0420b419e5dc
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-25 17:37:38 -0500

    completed final callback objectives meaning all non-stretch goals completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -13,19 +13,25 @@ function getLength(arr, cb) {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+if (list.includes(item)){
+return cb(true);
+} else return cb(false);
 }
 
 /* STRETCH PROBLEM */
