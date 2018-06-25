c29bc2c0f5e45d71c5119aeb4f6a7fbd9bdef0ba
William <kwontaewoongah@gmail.com>
2018-06-25 16:58:43 -0700

    made changes toc allbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,36 +2,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
-  return arr[0];
+  cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return arr.length;
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return arr[arr.length-1];
+  cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return x+y;
+  cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return x*y;
+  cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   if (list.indexOf(item)!==-1) {
-    return true;
+    cb(true);
   } else {
-    return false;
+    cb(false);
   }
 }
 
@@ -47,5 +47,5 @@ function removeDuplicates(array, cb) {
       newArr.push(array[i]);
     }
   }
-  return newArr;
+  cb(newArr);
 }
