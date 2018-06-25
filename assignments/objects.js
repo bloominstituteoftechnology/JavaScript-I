111d7609d86d7d382b6644ecd31ebe5ed4b7206d
Trevor <trevorfehrman@gmail.com>
2018-06-25 16:11:21 -0700

    Made Object stretch DRYer

--- assignments/objects.js
+++ assignments/objects.js
@@ -96,32 +96,28 @@ console.log(antoniettaF.multiplyNums(3,4));
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
+const speakFunc = function() {
+  return `Hello, my name is ${this.name}`;
+}
 
 let parent = {
-  "name": "Susan",
-  "age": 70,
-  speak: function() {
-    return `Hello, my name is ${this.name}`;
-  },
+  name: "Susan",
+  age: 70,
+  speak: speakFunc,
   child: {
-    "name": "George",
-    "age": 50,
-    speak: function() {
-      return `Hello, my name is ${this.name}`
-    },
+    name: "George",
+    age: 50,
+    speak: speakFunc,
     grandchild: {
-      "name": "Sam",
-      "age": 30,
-      speak: function() {
-        return `Hello, my name is ${this.name}`
-      }
+      name: "Sam",
+      age: 30,
+      speak: speakFunc,
     }
   }
 }
 
 
-console.log(parent.child.grandchild.speak());
-
+console.log(parent.speak());
 // Log the parent object's name
 
 // Log the child's age
