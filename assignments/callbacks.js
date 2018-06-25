99c3b811a87be1a324561decb21e3d54fd7aa71c
keegn <keegan.t.burkett@gmail.com>
2018-06-25 16:23:59 -0500

    finished callbacks stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -46,4 +46,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let duplicateFree = Array.from(new Set(array));
+  cb(duplicateFree);
 }
