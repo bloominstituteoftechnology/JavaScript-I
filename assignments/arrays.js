728f4f072bae380c6ad96221286e669091fec6fd
adamcglee <adamcglee@gmail.com>
2018-06-25 15:49:15 -0700

    fixed issue with counting cars

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,7 +104,7 @@ for (let i = 0; i < inventory.length; i++) {
     oldCars.push(inventory[i].car_year);
     }
 }
-console.log(oldCars); 
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -42,4 +42,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
 }
