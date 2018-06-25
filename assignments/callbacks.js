3100f4de8673cc8e1beecf13097c00011bf9b18d
francistse23 <wtse.ucsd@gmail.com>
2018-06-25 15:14:59 -0700

    callbacks.js completed. Stretch goal pending.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,27 +2,40 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.length-1);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let multiply = x * y;
+  return cb(multiply);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list[i] === item){
+    return cb(true);
+  }
+  else{
+    return cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
