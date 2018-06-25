9b27b4731b93a3cf3f51d733aa20dc4d39207221
bri79319 <bri79319@gmail.com>
2018-06-25 15:25:38 -0400

    JS
    Create objects for each intern

--- assignments/objects.js
+++ assignments/objects.js
@@ -17,15 +17,41 @@ let example = {
   "gender": "F"
 }
 
-let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+// Write your intern objects here:
+let intern_1 = {
+  "id": 1,
+  "name": 'Mitzi',
+  "email": 'mmelloy0@psu.edu',
+  "gender": 'F'
 }
 
-// Write your intern objects here:
+let intern_2 = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
 
+let intern_3 = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+let intern_4 = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+let intern_5 = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
