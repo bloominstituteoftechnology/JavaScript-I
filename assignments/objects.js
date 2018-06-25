3f16bdfb3b9fd0d18c92341189e860efdc0d7887
Ashwin S <aswinjeganath@gmail.com>
2018-06-26 02:23:29 +0530

    Finish challenge 2 in objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,62 +11,61 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+  id: 0,
+  name: "Example",
+  email: "examples@you.edu",
+  gender: "F"
+};
 
 // Write your intern objects here:
 let mitzi = {
   id: 1,
-  name: 'Mitzi',
-  email: 'mmelloy0@psu.edu',
-  gender: 'F'
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
 };
 
 let kennan = {
   id: 2,
-  name: 'Kennan',
-  email: 'kdiben1@tinypic.com',
-  gender: 'M'
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M"
 };
 
 let keven = {
   id: 3,
-  name: 'Keven',
-  email: 'kmummery2@wikimedia.org',
-  gender: 'M'
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
 };
 // 4,gmartinson3@illinois.edu,Gannie,M
 // 5,adaine5@samsung.com,Antonietta,F
-let gannie =  {
+let gannie = {
   id: 4,
-  name: 'Gannie',
-  email: 'gmartinson3@illinois.edu',
-  gender: 'M'
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
 };
 let antoinetta = {
   id: 5,
-  name: 'Antonietta',
-  email: 'adaine5@samsung.com',
-  gender: 'F'
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F"
 };
-
 
 // ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(mitzi.name);
 // Kennan's ID
-
+console.log(kennan.id);
 // Keven's email
-
+console.log(keven.email);
 // Gannie's name
-
+console.log(gannie.name);
 // Antonietta's Gender
-
+console.log(antoinetta.gender);
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
@@ -83,7 +82,7 @@ let antoinetta = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {};
 
 // Log the parent object's name
 
