2c70c8fad00da1d6f73922c3c4f9b5ef35854574
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-06-25 15:45:04 -0500

    finished object challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -81,8 +81,22 @@ console.log(Antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+Kennan.speak = function(){
+console.log("Hello, my name is " + this.name + "!");
+}
+
+Kennan.speak();
+
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+Antonietta.multiply = function(x,y){
+  let product = (x*y);
+  console.log(product);
+}
+
+Antonietta.multiply(3,4);
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
