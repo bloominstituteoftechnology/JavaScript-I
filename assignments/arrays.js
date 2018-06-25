e6a851482c58b43040376f1fb2991121ba8b8fa7
LizBaker <lizbaker.pdx@gmail.com>
2018-06-25 14:56:07 -0700

    test
    -otherstuff

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -83,7 +83,7 @@ for (let i = 0; i < inventory.length; i++) {
 }
 console.log(carModels);
 
-// ==== Challenge 4 ====
+// ==== Challenge 4 test ====
 // The accounting team needs all the years from every car on the lot. 
 // Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
