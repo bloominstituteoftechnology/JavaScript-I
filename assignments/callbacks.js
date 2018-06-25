73b0658722b378857e0a3efdba28ef428d0759c5
dasgma <dasgrigoma@gmail.com>
2018-06-25 16:24:29 -0700

    Finished stretch.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -58,4 +58,10 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+ const seen = {};
+ for (let i = 0; i < array.length; i++){
+   seen[array[i]] = true;
+   cb(Object.keys(seen));
+ }
 }
