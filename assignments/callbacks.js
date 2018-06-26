f7ab13b182d25e8dc88376d6d8a09b60227dc082
sliw <slioweng@gmail.com>
2018-06-27 05:25:41 +0800

    correct errors

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -52,7 +52,7 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  if (list.indexOf(item)) {
+  if (list.indexOf(item) === -1) {
     cb(false);  
   } else {
     cb(true);
@@ -69,7 +69,18 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  let noDuplicatedArray = new Set(array);
+  let noDuplicatedArray = Array.from( new Set(array) );
+  cb(noDuplicatedArray);
+}
+
+function removeDuplicates(array, cb) {
+  let noDuplicatedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (noDuplicatedArray.indexOf(array[i]) === -1) {
+      noDuplicatedArray.push(array[i]);
+    }
+  }
+  console.log(array, noDuplicatedArray);
   cb(noDuplicatedArray);
 }
 
