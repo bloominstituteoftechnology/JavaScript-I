2a76e5b92ca1f3c84d9f1873e0cbe7053325ba96
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-10 10:06:36 -0600

    Updating files

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,28 +2,67 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 }
+
+firstItem(items, function(first) {
+  console.log(first);
+})
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
+
+sumNums(10, 15, function(total) {
+  console.log(total);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+
+multiplyNums(10, 15, function(total) {
+  console.log(total);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return cb(true);
+    }
+  }
+  return cb(false);
+};
+
+contains('Bats', items, function(flag) {
+  console.log(flag);
+});
+
+contains('Notebook', items, function(flag) {
+  console.log(flag);
+});
 
 /* STRETCH PROBLEM */
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -76,8 +76,19 @@ console.log(Antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+Kennan.speak = function () {
+  return("Hello, my name is Kennan.");
+};
+
+console.log(Kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+Antonietta.multiplyNums = function (x,y) {
+  return x * y;
+}
+
+console.log(Antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
