bb929d701c67aa273ab60f785886885c4858de11
travis-russell <35874527+travis-russell@users.noreply.github.com>
2018-08-06 17:17:14 -0400

    finished

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,40 @@ let example = {
 }
 
 // Write your intern objects here:
+const intern1 = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "melloy@psu.edu",
+  "gender": "F"
+}
 
+const intern2 = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+
+const intern3 = {
+  "id": 3,
+  "name": "Kevin",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const intern4 = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+const intern5 = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
@@ -33,12 +66,25 @@ let example = {
 
 // Antonietta's Gender
 
+console.log(intern1["name"]);
+console.log(intern2["id"]);
+console.log(intern3["email"]);
+console.log(intern4["name"]);
+console.log(intern5["gender"]);
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+intern2.speak = function() {
+  console.log("Hello, my name is Kennan!")
+}
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+intern5.multiplyNums = function(x, y) {
+  console.log(x * y);
+}
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,8 +95,34 @@ let example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
+let parent = {
+  name: "Susan",
+  age: "70",
+  speak: function() {
+    console.log(this.name);
+  },
+  child: {
+    name: "George",
+    age: "50",
+    speak: function() {
+      console.log(this.name);
+    }
+  },
+  grandChild: {
+    name: "Sam",
+    age: "30",
+    speak: function() {
+      console.log(this.name);
+    }
+  }
+}
 
+console.log(parent["name"]);
+console.log(parent["child"]["name"]);
+console.log(parent["child"]["grandChild"]["name"], parent["grandChild"]["age"]);
+console.log(parent.speak());
+console.log(parent.child.speak());
+console.log(parent.grandChild.speak());
 // Log the parent object's name
 
 // Log the child's age
