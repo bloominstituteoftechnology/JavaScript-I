f6ae900ef3b5fbb1533b23b6655ad1593dab8836
Keiran <keirankozlowski@gmail.com>
2018-08-06 16:22:06 -0400

    Finished objects.js questions.

--- assignments/objects.js
+++ assignments/objects.js
@@ -74,10 +74,19 @@ console.log(antoniettaObj["gender"]);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+kennanObj.prototype.speak = function () {
+    return "Hello, my name is Kennan!";
+}
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+antoniettaObj.prototype.multiplyNums = function (a,b) {
+    return a * b;
+}
+console.log(antonietta.multiplyNums(3, 4));
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
