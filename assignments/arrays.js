0d485f0c8e73b5ddd147441bb4065046e0877825
Timothy Hoang <timh1203@gmail.com>
2018-08-06 17:06:58 -0500

    Add array challenge 3 stretch

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -89,6 +89,10 @@ for (let i=0; i < inventory.length; i++) {
 }
 console.log(carModels.sort());
 
+// Challenge 3 Stretch
+const alphabetizedModels = inventory.map(car => car["car_model"]).sort();
+console.log(alphabetizedModels);
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
