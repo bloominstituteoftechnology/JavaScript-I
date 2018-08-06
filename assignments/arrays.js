df6f426af4f0dd9f0cf1794a564b47f09ab00848
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-06 17:55:51 -0400

    adjusted issue with challenge 6 loop

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,9 +104,9 @@ for(let i = 0; i < carYears.length; i++){
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
 for(let i = 0; i < inventory.length; i++) {
-  if([i.car_make] === 'BMW' || 'Audi') {
+  if(i.car_make === "BMW" || inventory[i].car_make === "Audi") {
     BMWAndAudi.push(inventory[i]);
   }
-  console.log(BMWAndAudi);
+  console.log(JSON.stringify(BMWAndAudi));
 }
 // not working just yet, so I haven't done the JSON.stringify() until the code is correct
