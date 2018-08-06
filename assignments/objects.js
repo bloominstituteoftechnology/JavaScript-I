7b0e1b5a92b72ed7e9e266e65b1381dfb6c4689a
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-08-06 15:57:37 -0700

    final commit

--- assignments/objects.js
+++ assignments/objects.js
@@ -99,16 +99,43 @@ console.log(intern5.multiplyNums(3, 6));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  "name": "Susan",
+  "age": 70,
+  child: {
+    "name": "George",
+    "age": 50,
+    grandchild: {
+      "name": "Sam",
+      "age": 30,
+    }
+  }
+}
 
 // Log the parent object's name
+console.log(parent["name"]);
 
 // Log the child's age
+console.log(parent["child"]["age"]);
 
 // Log the name and age of the grandchild
+console.log(parent["child"]["grandchild"]);
 
 // Have the parent speak
+parent["speak"] = function() {
+  return this["name"];
+}
+console.log(parent.speak());
 
 // Have the child speak
+parent["child"]["speak"] = function() {
+  return this["name"];
+}
+console.log(parent["child"].speak());
 
 // Have the grandchild speak
+parent["child"]["grandchild"]["speak"] = function() {
+  return this["name"];
+}
+console.log(parent["child"]["grandchild"].speak());
+
