49d4cdf316b61a277837d6159526e3bff1c8cd2d
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-08-13 23:34:40 -0600

    Completed Arrays: Challenge 4

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -93,7 +93,15 @@ console.log(carModels);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i].car_year);
+}
+carYears.sort()
+console.log(carYears);
+// for (let i = 0; i < carYears.length; i++) {
+//     console.log(carYears[i]);
+// }
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
