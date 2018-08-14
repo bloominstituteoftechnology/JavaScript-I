d966c484a7aa1082bcdd50b3eae940d922f0710b
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-08-13 22:20:07 -0600

    Completed Objects: Challenge 2

--- assignments/objects.js
+++ assignments/objects.js
@@ -58,14 +58,19 @@ const antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mitzi.name);
 
 // Kennan's ID
+console.log(kennan.id);
 
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
