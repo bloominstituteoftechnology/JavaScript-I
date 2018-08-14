4880f4d2b8d41f971cbb0bc3588bee0cb0af84b2
kurtve <kurt@vanetten.us>
2018-08-14 02:50:05 -0400

    Complete Arrays challenge 1

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -1,6 +1,8 @@
-// To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
+// To help us use arrays with real world problems we are going to simulate a used car
+// dealer that has 50 cars in their inventory.
 
-// The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
+// The car dealer has all of their inventory housed in the array seen below.  Scroll
+// down past the data to find out how you can help the car dealer.
 
 let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},
@@ -53,7 +55,8 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":49,"car_make":"Chrysler","car_model":"Sebring","car_year":1996},
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
 
-// PROJECT RESTRICTION: You can't use map, reduce, or filter to solve these problems.  Only use native JavaScript for loops.
+// PROJECT RESTRICTION: You can't use map, reduce, or filter to solve these problems.
+//  Only use native JavaScript for loops.
 
 // Example for loop:
 // arr = [1,2,3,4];
@@ -62,35 +65,61 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // }
 
 // ==== Challenge 1 ====
-// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+// The dealer can't recall the information for a car with an id of 33 on his lot. Help the
+// dealer find out which car has an id of 33 by logging the car's year, make, and model in
+// the console log provided to you below:
 
+// let's not assume the ids are all in order with no gaps
+let carId = 33;
+let carEntry = null;
+for (let i = 0; i < inventory.length; i++) {
+    carEntry = inventory[i];
+    if (carEntry.id === carId) {
+        break;
+    }
+}
+
+console.log(`Car ${carId} is a ${carEntry.car_year} ${carEntry.car_make} ${carEntry.car_model}` );
 
 
 // ==== Challenge 2 ====
-// The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
+// The dealer needs the information on the last car in their inventory.  What is the make and
+// model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
 console.log();
 
+
+
 // ==== Challenge 3 ====
-// The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+// The marketing team wants the car models listed alphabetically on the website. Sort all the
+// car model names into alphabetical order and log the results in the console
 let carModels = [];
 console.log();
 
+
+
 // ==== Challenge 4 ====
-// The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
+// The accounting team needs all the years from every car on the lot. Create a new array from the
+// dealer data containing only the car years and log the result in the console.
 let carYears = [];
 console.log();
 
+
+
 // ==== Challenge 5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
+// The car lot manager needs to find out how many cars are older than the year 2000. Using the
+// carYears array you just created, find out how many cars were made before the year 2000 by
+// populating the array oldCars and logging it's length.
 let oldCars =[];
 console.log(); 
 
+
+
 // ==== Challenge 6 ====
-// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an
+// array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array,
+// use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
 console.log();
-
 
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -1,7 +1,8 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
 // ==== Challenge 1: Writing Objects ==== 
-// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
+// HR needs some information on the new interns put into a database.  Given an id, email,
+// first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
 // 2,kdiben1@tinypic.com,Kennan,M
@@ -20,6 +21,7 @@ const example = {
 // Write your intern objects here:
 
 
+
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
@@ -33,20 +35,29 @@ const example = {
 
 // Antonietta's Gender
 
+
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+// Antonietta loves math, give her the ability to multiply two numbers together and return the product.
+// Use the console.log provided as a hint.
+// console.log(antonietta.multiplyNums(3,4));
+
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
+
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
-// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
-// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
+// 2. Nest a child object in the parent object with name and age as well.  The name will be George
+//    and the age will be 50.
+// 3. Nest a grandchild object in the child object with properties for name and age.  The name will
+//    be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
 const parent = {}
