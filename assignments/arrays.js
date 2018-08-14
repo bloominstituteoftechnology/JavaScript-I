9b0bff8f5600d2ae58b69c3627c490e19af50e2a
paulapivat <paul.apivat@gmail.com>
2018-08-14 14:01:59 +0700

    complete Challenge 6 array.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -147,5 +147,16 @@ allYears();
 let BMWAndAudi =[];
 console.log();
 
-
+//Answer
+function selectCars() {
+    let BMWAndAudi =[];
+    for (i = 0; i < inventory.length; i++) {
+      if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
+        BMWAndAudi.push(inventory[i]);
+      }
+    }
+    return JSON.stringify(BMWAndAudi);
+  }
+  
+  selectCars();
 
