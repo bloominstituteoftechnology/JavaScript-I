86e797c86ce76384b427ce27990145cf47afe622
kurtve <kurt@vanetten.us>
2018-08-14 20:58:01 -0400

    Complete objects.js assignment (without the stretch goals)

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,42 +11,89 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
+/*
 const example = {
   "id": 0,
   "name": "Example",
   "email": "examples@you.edu",
   "gender": "F"
 }
+*/
 
 // Write your intern objects here:
+const mitzi = {
+    "id": 1,
+    "name": "Mitzi",
+    "email": "mmelly0@psu.edu",
+    "gender": "F",
+};
 
+const kennan = {
+    "id": 2,
+    "name": "Kennan",
+    "email": "kdiben1@tinypic.com",
+    "gender": "M",
+};
+
+const keven = {
+    "id": 3,
+    "name": "Keven",
+    "email": "kmummery2@wikimedia.org",
+    "gender": "M",
+};
+
+const gannie = {
+    "id": 4,
+    "name": "Gannie",
+    "email": "gmartinson3@illinois.edu",
+    "gender": "M",
+};
+
+const antonietta = {
+    "id": 5,
+    "name": "Antonietta",
+    "email": "adaine5@samsung.com",
+    "gender": "F",
+};
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mitzi.name);
 
 // Kennan's ID
+console.log(kennan.id);
 
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
-
+console.log(antonietta.gender);
 
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+kennan.speak = function() {
+    return `Hello, my name is ${this.name}`;
+};
 
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product.
 // Use the console.log provided as a hint.
 // console.log(antonietta.multiplyNums(3,4));
 
+antonietta.muliplyNums = function(a, b) {
+    return a * b;
+};
 
+console.log(antonietta.muliplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -60,13 +107,16 @@ const example = {
 //    be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+// Nested? Does this mean inheritence? Because nesting doesn't make
+// sense to me with item 4, which suggests inheritence instead.
+
 
 // Log the parent object's name
 
 // Log the child's age
 
 // Log the name and age of the grandchild
+
 
 // Have the parent speak
 
