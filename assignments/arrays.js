3b787d315c578604fecf5d6cd4af7d3d1418f62d
cpdis <colin.dismuke@gmail.com>
2018-08-14 22:47:20 -0500

    Added let to index in for loops.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -84,7 +84,7 @@ console.log(carModels.sort());
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 
-for (j = 0; j < inventory.length; j++) {
+for (let j = 0; j < inventory.length; j++) {
     carYears.push(inventory[j].car_year);
 }
 
@@ -94,7 +94,7 @@ console.log(carYears);
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
 
-for (k = 0; k < inventory.length; k++) {
+for (let k = 0; k < inventory.length; k++) {
     if (inventory[k].car_year < 2000) {
         oldCars.push(inventory[k].car_model);
     }
@@ -106,7 +106,7 @@ console.log(oldCars.length);
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
 
-for (l = 0; l < inventory.length; l++) {
+for (let l = 0; l < inventory.length; l++) {
     if (inventory[l].car_make === "BMW" || inventory[l].car_make === "Audi") {
         BMWAndAudi.unshift(inventory[l]);
     }
