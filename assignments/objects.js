b643d97eb5e68b46c320d4139ca8b3c33a74b5b0
Pascale Pierre <pascaleee@gmail.com>
2018-08-14 22:20:10 -0500

    built object in stretch goals

--- assignments/objects.js
+++ assignments/objects.js
@@ -67,7 +67,7 @@ console.log(intern.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {"name": "Susan", "age": 70},
 
 // Log the parent object's name
 
