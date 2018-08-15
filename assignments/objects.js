401f6dafa93c817bc42a3a8e447391392a211f24
Kyle Baker <k@gnomiq.com>
2018-08-14 19:11:44 -0700

    Finished core objectives

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,14 +11,43 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+    "id": 0,
+    "name": "Example",
+    "email": "examples@you.edu",
+    "gender": "F"
+};
 
 // Write your intern objects here:
+const mitzi = {
+    id: 1,
+    name: "Mitzi",
+    email: "melloy0@psu.edu",
+    gender: "F"
+}, kennan = {
+    id: 2,
+    name: "Kennan",
+    email: "kdiben1@tinypic.com",
+    gender: "M",
+    speak: () => "Hello, my name is Kennan!"
+}, keven = {
+    id: 3,
+    name: "Keven",
+    email: "kmummery2@wikimedia.org",
+    gender: "M"
+}, gannie = {
+    id: 4,
+    name: "Gannie",
+    email: "gmartinson3@illinois.edu",
+    gender: "M"
+}, antonietta = {
+    id: 5,
+    name: "Antonietta",
+    email: "adaine5@samsung.com",
+    gender: "F",
+    multiplyNums: (x, y) => parseInt(x) * parseInt(y)
+};
 
+// let interns = [mitzi, kennan, keven, gannie, antonietta];
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
@@ -32,13 +61,17 @@ const example = {
 // Gannie's name
 
 // Antonietta's Gender
+console.log(mitzi.name);
+console.log(kennan.id);
+console.log(keven.email);
+console.log(gannie.name);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
