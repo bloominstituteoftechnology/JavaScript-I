b04bc5bad27d165fdaf04fe133e879a0922da1d4
Pascale Pierre <pascaleee@gmail.com>
2018-08-14 22:03:09 -0500

    completed object challenge 1 and 2

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
@@ -19,22 +19,31 @@ const example = {
 
 // Write your intern objects here:
 
+let intern = [{"id": 1, "name": "Mitzi", "email": "mmelloy0@psu.edu","gender": "F"},
+{"id": 2, "name": "Kennan", "email": "kdiben1@tinypic.com", "gender": "M"},
+{"id": 3, "name": "Keven", "email": "kmummery2@wikimedia.org", "gender": "M"},
+{"id": 4, "name": "Gannie", "email": "adaine5@samsung.com","gender": "M"},
+{"id": 5, "name": "Antonietta", "email": "adaine5@samsung.com","gender": "F"}];
 
-// ==== Challenge 2: Reading Object Data ==== 
+
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
 // Kennan's ID
-
+console.log(intern[0].name);
 // Keven's email
-
+console.log(intern[2].email);
 // Gannie's name
-
+console.log(intern[3].name);
 // Antonietta's Gender
-
-// ==== Challenge 3: Object Methods ==== 
+console.log(intern[4].gender);
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+
+  return `Hello, my name is ${this.namer}`;
+
 // console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
@@ -42,7 +51,7 @@ const example = {
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
