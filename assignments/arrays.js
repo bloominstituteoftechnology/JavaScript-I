e51713ab497e1a600480f2bdccefeedb6fbdfbba
paulapivat <paul.apivat@gmail.com>
2018-08-15 09:31:29 +0700

    add alternative answer, forEach to Challenge 4 array.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -136,6 +136,19 @@ function allYears() {
 
 allYears();
 
+//ALTERNATIVE Answer using forEach()
+
+function allYears() {
+  let carYears = [];
+  inventory.forEach((e,i) => {
+    carYears.push(e.car_year);
+  });
+  return carYears;
+}
+
+allYears();
+
+
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
