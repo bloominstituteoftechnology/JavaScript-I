2eaf173178938953466a0fdcfd0ce328072fdca6
Mitchell Robles <mitchellr92@live.com>
2018-08-14 19:34:13 -0700

    Finished most arrays, still working on challenge 3 for objects and challenge 2 for arrays

--- assignments/objects.js
+++ assignments/objects.js
@@ -82,8 +82,19 @@ console.log(antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+function speak(object) {
+  return "Hello, my name is " + object.name + "!"; 
+}
+ 
+console.log(kennan.speak(kennan));
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+function multiplyNums(x, y) {
+  const sum = x * y;
+  return sum;
+}
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
