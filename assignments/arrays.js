5927b5b3c48188b59635878759dacea3bd7413cb
Kendra Williams <kmonique.williams@gmail.com>
2018-08-14 20:23:03 -0400

    code clean up: changed created index's in array.js to use element.property

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,30 +75,25 @@ console.log(`${lastCar.car_year} ${lastCar.car_make} ${lastCar.car_model}`);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-let index = 0;
 inventory.forEach(function (element){
-    carModels.push(`${inventory[index].car_model}`);
-    index++;
+    carModels.push(`${element.car_model}`);
 });
 console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-index = 0;
 inventory.forEach(function(element){
-    carYears.push(`${inventory[index].car_year}`);
-    index++;
+    carYears.push(`${element.car_year}`);
 });
 console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-index = 0;
 carYears.forEach(function(element){
     if(element > 2000){
-        oldCars.push(`${carYears[index]}`);
+        oldCars.push(`${element}`);
     }
 });
 console.log(oldCars.length); 
@@ -106,11 +101,9 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-index = 0;
 inventory.forEach(function(element){
     if(Object.values(element).includes("BMW") || Object.values(element).includes("Audi")){
-        BMWAndAudi.push(`${inventory[index].car_year} ${inventory[index].car_make} ${inventory[index].car_model}`);
+        BMWAndAudi.push(`${element.car_year} ${element.car_make} ${element.car_model}`);
     }
-    index++;
 });
 console.log(JSON.stringify(BMWAndAudi));
