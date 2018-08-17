0bf07269dc03070f12cb003220d96f3f692e8f67
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-08-16 19:03:19 -0600

    Almost Done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,11 +75,14 @@ console.log(inventory[inventory.length - 1].car_make , inventory[inventory.lengt
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
+const alphaCar = () => {
+    const betaCars = [];
+    betaCar.push (inventory[0]);
 
-for ()
-console.log();
-
+    for (let i = 1; betaCars.length <= inventory.length;i++) {
+        for (let j = 1; betaCars.length <= inventory.length;i++)
+    }
+}
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
