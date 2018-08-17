6ed7e821526b7b2b580024f098be606a4cdb1052
Ken Barlow <kenbrlow@gmail.com>
2018-08-16 19:05:16 -0700

    Solved problem 2, make and model being logged to the console.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,18 +74,16 @@ for (i = 0; i < inventory.length; i++) {
     var value = values[g];
     if (key == 'id') {
       if (value == 33) {
-        console.log('Car 33 is a ' + item.car_year + ' ' + item.car_make + ' ' + item.car_model);
+        //console.log('Car 33 is a ' + item.car_year + ' ' + item.car_make + ' ' + item.car_model);
       }
     }
   }
 }
 
-
-
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory[inventory.length - 1];
+console.log('Make: ' + lastCar.car_make + ' Model: ' + lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
