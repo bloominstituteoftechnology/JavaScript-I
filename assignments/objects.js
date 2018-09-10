a87356c528c84ccd6bd7dafb884caa74c693f15b
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-10 18:53:52 -0500

    updated objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,62 +18,61 @@ const example = {
 }
 
 // Write your intern objects here:
-const newInterns  = [
-  {
-    "id": 1,
-    "email": "mmelloy0@psu.edu", 
-    "first name": "Mitzi",
-    "gender": "F",
-  },
-  {
-    "id": 2,
-    "email": "kdiben1@tinypic.com", 
-    "first name": "Kennan",
-    "gender": "M",
-  },
-  {
-    "id": 3,
-    "email": "kmummery2@wikimedia.org", 
-    "first name": "Keven",
-    "gender": "M",
-  },
-  {
-    "id": 4,
-    "email": "gmartinson3@illinois.edu", 
-    "first name": "Gannie",
-    "gender": "M",
-  },
-  {
-    "id": 5,
-    "email": "adaine5@samsung.com", 
-    "first name": "Antoniette",
-    "gender": "F",
-  },
-]
+const mitzi = {
+  "id": 1,
+  "first name":"Mitzi",
+  "email":"mmelloy0@psu.edu",
+  "gender":"F",
+}
+const kennan = {
+  "id":2,
+  "first name":"Kennan",
+  "email":"kdiben1@tinypic.com",
+  "gender":"M",
+}
+const keven = {
+  "id":3,
+  "first name":"Keven",
+  "email":"kmummery2@wikimedia.org",
+  "gender":"M",
+}
+const gannie= {
+  "id":4,
+  "first name":"Gannie",
+  "email":"gmartinson3@illinois.edu",
+  "gender":"M",
+}
+const antonietta = {
+  "id":5,
+  "first name":"Antonietta",
+  "email":"adaine5@samsung.com",
+  "gender":"F",
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-  console.log(newInterns[0]["first name"]);
+  console.log(mitzi["first name"]);
 // Kennan's ID
-  console.log(newInterns[1].id);
+  console.log(kennan.id);
 // Keven's email
-  console.log(newInterns[2].email);
+  console.log(keven.id);
 // Gannie's name
-  console.log(newInterns[3]["first name"]);
+  console.log(gannie["first name"]);
 // Antonietta's Gender
-  console.log(newInterns[4].gender);
+  console.log(antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-  newInterns[1].speak = function() {
-    console.log(`Hello, my name is ${newInterns[1]["first name"]}`);
+  kennan.speak = function() {
+    console.log(`Hello, my name is ${kennan["first name"]}`);
   }
-  newInterns[1].speak();
+  kennan.speak();
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-newInterns[4].multiplyNums = function (num1, num2) {
+antonietta.multiplyNums = function (num1, num2) {
   return num1 * num2;
 }
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
