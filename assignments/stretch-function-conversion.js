d0a34dafb594179a1f1c3815ea6d01dc325ed1bc
MEBonham <programmer@mckaybonham.com>
2018-09-10 15:41:51 -0600

    Did three out of five arrow stretch problems

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,28 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+// ?????
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+var anotherFunction = param => param
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-// add(1,2);
+var add = (a,b) => a+b
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-// subtract(1,2);
+var subtract = (a,b) => a-b
+console.log(subtract(1,2));
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
+
 // console.log(triple);
