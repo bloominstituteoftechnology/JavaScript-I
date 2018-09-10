04f755e28dacb0f00a218fbfd8e0313879566089
ssmodish <smodish@hotmail.com>
2018-09-10 15:37:58 -0400

    Objects 2 complete and fixed Objects 1

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,7 @@ const example = {
 }
 
 // Write your intern objects here:
-const interns = {
+const interns = [{
   "id": 1,
   "name": "Mitzi",
   "email": "mmelloy0@psu.edu",
@@ -43,7 +43,7 @@ const interns = {
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
   "gender": "F"
-}
+}];
 
 
 
@@ -52,14 +52,21 @@ const interns = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(interns[0].name);
 
 // Kennan's ID
+console.log(interns[1].id);
 
 // Keven's email
+console.log(interns[2].email);
 
 // Gannie's name
+console.log(interns[3].name);
 
 // Antonietta's Gender
+console.log(interns[4].gender);
+
+
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
