6ae4a03408013423453a3c2c1f48e356b0a1caac
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-10 15:51:05 -0600

    added a missing syntax

--- assignments/objects.js
+++ assignments/objects.js
@@ -69,7 +69,7 @@ console.log(antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-console.log(kennan.speak())
+console.log(kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 console.log (antonietta.multiplyNums(3,4));
