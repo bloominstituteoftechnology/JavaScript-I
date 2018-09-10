143d92bbbc0f4f36b2e8e51f9a4cb33fb16e29be
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-09-10 15:43:59 -0700

    Stretch - used carYears.filter()

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -106,6 +106,18 @@ for(let i = 0; i < carYears.length; i++){
 }
 console.log(oldCars, oldCars.length); 
 
+// STRETCH
+// const oldCarsFilter = carYears.filter(function(year){
+//     if(year < 2000){
+//         return true;
+//     }
+// })
+
+const oldCarsFilter = carYears.filter(year => year < 2000);
+
+console.log(oldCarsFilter, oldCarsFilter.length);
+
+
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
