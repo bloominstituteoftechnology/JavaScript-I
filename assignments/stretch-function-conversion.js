dbf1baf7782545788d40758d4b85d6cc733830a9
brianmgre <brian.gregorius.com>
2018-09-10 17:45:33 -0400

    updated stretch questions

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -6,18 +6,29 @@
 //   return param;
 // };
 
+let anotherFunction=param => param;
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let add = (param1, param2) => {
+    return param1 + param2
+}
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => {
+    return param1 - param2
+}
+
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
