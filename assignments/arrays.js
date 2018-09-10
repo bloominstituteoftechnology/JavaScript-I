6782e2705ede9c1cab3aee1a4aecd880d0b8e209
theryanclausen <theryanclausen@gmail.com>
2018-09-10 14:31:00 -0700

    all stretched

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,7 +73,8 @@ for (let i = 0; i < inventory.length; i++){
 
 console.log(`Car 33 is a ${car33['car_year']} ${car33['car_make']} ${car33['car_model']}`);
 
-
+let car33stretch = inventory.filter(x => x['id'] === 33);
+console.log(`Car 33 is a ${car33['car_year']} ${car33['car_make']} ${car33['car_model']}`);
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
@@ -88,6 +89,8 @@ for (let i = 0; i < inventory.length; i++){
 }
 console.log(carModels.sort());
 
+let carModelsStretch = inventory.map(x => x['car_model']).sort()
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
@@ -95,6 +98,8 @@ for (let i = 0; i < inventory.length; i++){
     carYears.push(inventory[i]['car_year']);
 }
 console.log(carYears);
+
+let carYearsStretch = inventory.map(x => x['car_year']);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -106,6 +111,7 @@ for (let i = 0; i < inventory.length; i++){
 }
 console.log(oldCars.length); 
 
+let OldCarsStretch = inventory.filter(x => x['car_year'] < 2000);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
