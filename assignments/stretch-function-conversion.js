6298b0b22751ca349ff5013c5af3a0a5ce1e9e43
Rahul Desai <rd825@georgetown.edu>
2018-09-10 15:37:13 -0400

    Completed stretch goal about arrow functions

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,33 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+    const myFunction = () => {};
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+    const anotherFunction = param => param;
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+    const add = (param1,param2) => param1 + param2;
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+    const subtract = (param1,param2) => param1 - param2;
+
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+    const triple = exampleArray.map(num => num*3);
+    console.log(triple);
