9dc024af30b1886a5082eccc519657b86a8a0998
zacharyarney <zachary.arney@gmail.com>
2018-09-10 16:07:07 -0500

    Completed objects.js challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -30,7 +30,10 @@ const kennan = {
   id: 2,
   name: "Kennan",
   email: "kdiben1@tinypic.com",
-  gender: "M"
+  gender: "M",
+  speak: function() {
+    return `Hello, my name is ${kennan.name}!`;
+  }
 };
 
 const keven = {
@@ -51,7 +54,10 @@ const antonietta = {
   id: 5,
   name: "Antonietta",
   email: "adaine5@samsung.com",
-  gender: "F"
+  gender: "F",
+  multiplyNums: function(x, y) {
+    return x + y;
+  }
 };
 
 // ==== Challenge 2: Reading Object Data ====
@@ -79,10 +85,10 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
