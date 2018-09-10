ae0c00927414beedbc1647df8408f3d83c6ef8b2
JacobABrennan <jacobabrennan@gmail.com>
2018-09-10 16:50:59 -0400

    Finished arrays.js Challenge 1

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,9 +63,12 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
-
-
+for(let inventoryIndex = 0; inventoryIndex < inventory.length; inventoryIndex++){
+    if(inventoryIndex !== 33){ continue;}
+    var car33 = inventory[inventoryIndex];
+    console.log(`Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}`);
+    break;
+}
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
