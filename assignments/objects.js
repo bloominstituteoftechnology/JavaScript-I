8efcf241bc67550200032b59a2eef7db3d661ade
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-09-10 17:02:47 -0600

    array

--- assignments/objects.js
+++ assignments/objects.js
@@ -81,6 +81,7 @@ console.log(intern5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(intern2.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
