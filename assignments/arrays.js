7b31a118ce70237ef37b204c148aa546d48fe1ce
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-09-10 17:21:04 -0400

    Edit on Arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -128,7 +128,7 @@ console.log(oldCars.length)
 let BMWAndAudi =[];
 
 for (i=0; i<inventory.length; i++) {
-  if (inventory[i].car_make === "BWM" || inventory[i].car_make === "Audi") {
+  if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
     BMWAndAudi.push(inventory[i])
   }
 }
