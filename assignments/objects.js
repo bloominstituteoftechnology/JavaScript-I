e4ffd91c9f7b738bc9bb60ad97a28a4fc0eac0ea
Axolotl <samuelmachat@gmail.com>
2018-09-10 16:32:50 -0400

    wrote code for automatically generating student objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
@@ -11,19 +11,83 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+  id: 0,
+  name: 'Example',
+  email: 'examples@you.edu',
+  gender: 'F'
 }
-
-
-
 
 // Write your intern objects here:
 
+const internsRaw = `1,mmelloy0@psu.edu,Mitzi,F,
+2,kdiben1@tinypic.com,Kennan,M,
+3,kmummery2@wikimedia.org,Keven,M
+4,gmartinson3@illinois.edu,Gannie,M
+5,adaine5@samsung.com,Antonietta,F`
+  .split('\n')
+  .map(chunk => chunk.split(','))
+  .map(student => ({
+    id: student[0],
+    name: student[2],
+    email: student[1],
+    gender: student[3]
+  }))
+  .map(student => {
+    console.log(
+      `const ${student.name} = ${JSON.stringify(student)
+        .split('')
+        .map(
+          char =>
+            char === '{'
+              ? char + `\n  `
+              : char === ','
+                ? `${char}\n  `
+                : char === '}'
+                  ? '\n' + char
+                  : char
+        )
+        .join('')}\n`
+    )
+  })
 
-// ==== Challenge 2: Reading Object Data ==== 
+// objects below copied in from console
+
+const Mitzi = {
+  "id":"1",
+  "name":"Mitzi",
+  "email":"mmelloy0@psu.edu",
+  "gender":"F"
+}
+
+const Kennan = {
+  "id":"2",
+  "name":"Kennan",
+  "email":"kdiben1@tinypic.com",
+  "gender":"M"
+}
+
+const Keven = {
+  "id":"3",
+  "name":"Keven",
+  "email":"kmummery2@wikimedia.org",
+  "gender":"M"
+}
+
+const Gannie = {
+  "id":"4",
+  "name":"Gannie",
+  "email":"gmartinson3@illinois.edu",
+  "gender":"M"
+}
+
+const Antonietta = {
+  "id":"5",
+  "name":"Antonietta",
+  "email":"adaine5@samsung.com",
+  "gender":"F"
+}
+
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
@@ -36,7 +100,7 @@ const example = {
 
 // Antonietta's Gender
 
-// ==== Challenge 3: Object Methods ==== 
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
@@ -45,7 +109,7 @@ const example = {
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
