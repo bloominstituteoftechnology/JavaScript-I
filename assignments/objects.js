2000b8dc11375790ef4276e745c844cb6e815b44
Brad Mortensen <b.mort86@gmail.com>
2018-09-10 13:30:49 -0600

    finished objects.js assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -13,33 +13,74 @@
 const example = {
   "id": 0,
   "name": "Example",
-  "email": "examples@you.edu",
+  "email": "mmelloy0@psu.edu",
   "gender": "F"
 }
 
 // Write your intern objects here:
+
+const interns = [
+
+  {
+      "id": 1,
+      "name": "Mitzi",
+      "email": "kdiben1@tinypic.com",
+      "gender": "F" 
+  },
+  {
+      "id": 2,
+      "name": "Kennan",
+      "email": "examples@you.edu",
+      "gender": "M",
+      speak:function(){
+          return ("Hello, my name is " + interns[1].name +"!");
+      }
+  },
+  {
+      "id": 3,
+      "name": "Keven",
+      "email": "kmummery2@wikimedia.org",
+      "gender": "M"
+  },
+  {
+      "id": 4,
+      "name": "Gannie",
+      "email": "gmartinson3@illinois.edu",
+      "gender": "M"   
+  },
+  {
+      "id": 5,
+      "name": "Antonietta",
+      "email": "adaine5@samsung.com",
+      "gender": "F",
+      multiplyNums: function(num1, num2){
+         return num1*num2;
+      }
+      
+  }
+];
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(interns[0].name);
 // Kennan's ID
-
+console.log(interns[1].id);
 // Keven's email
-
+console.log(interns[2].email);
 // Gannie's name
-
+console.log(interns[3].name);
 // Antonietta's Gender
-
+console.log(interns[4].gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(interns[1].speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(interns[4].multiplyNums(5,3));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
