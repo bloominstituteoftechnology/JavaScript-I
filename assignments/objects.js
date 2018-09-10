b60616a82ecd911ab623f47a0fb82bfc13f79bdf
ssmodish <smodish@hotmail.com>
2018-09-10 15:26:25 -0400

    Init commit/PR

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,6 +18,34 @@ const example = {
 }
 
 // Write your intern objects here:
+const interns = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}, {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}, {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}, {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}, {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
+
+
 
 
 // ==== Challenge 2: Reading Object Data ==== 
