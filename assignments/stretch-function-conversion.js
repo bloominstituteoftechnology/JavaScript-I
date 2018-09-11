59ebe2f986076263dfd38b3fdde52905f7788c3f
Yanrong Xu <xuyanrong@icloud.com>
2018-09-11 01:23:57 -0400

    stretch complete

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,42 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+let myFunction = () => {};
+console.log(myFunction);
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+let anotherFunction = (param) => {
+    return param;
+}
+console.log(anotherFunction);
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => {
+    return param1+param2;
+}
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => {
+    return param1-param2;
+}
+console.log(subtract(1,2))
 
-// exampleArray = [1,2,3,4];
+//exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => {
+    return num*3;
+});
+console.log(triple);
