dc0a27062981c8331f6f798ece48f6a992e13730
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-10 17:25:52 -0700

    Logged all the properties of each cars instead of just their car model

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -125,13 +125,13 @@ let BMWAndAudi =[];
 
 for (let i=0; i < inventory.length; i++) {
   let carMake = inventory[i].car_make;
-  let carModel = inventory[i].car_model;
 
   if (carMake === 'BMW' || carMake === 'Audi') {
-    BMWAndAudi.push(carModel);
+    BMWAndAudi.push(inventory[i]);
   }
 }
 
-console.log(JSON.stringify(BMWAndAudi));  // => "["riolet","4000CS Quattro","525","6 Series","Q7","TT"]"
+console.log(JSON.stringify(BMWAndAudi));
+// => [{"id":6,"car_make":"Audi","car_model":"riolet","car_year":1995},{"id":8,"car_make":"Audi","car_model":"4000CS Quattro","car_year":1987},{"id":25,"car_make":"BMW","car_model":"525","car_year":2005},{"id":30,"car_make":"BMW","car_model":"6 Series","car_year":2010},{"id":44,"car_make":"Audi","car_model":"Q7","car_year":2012},{"id":45,"car_make":"Audi","car_model":"TT","car_year":2008}]
 
 
