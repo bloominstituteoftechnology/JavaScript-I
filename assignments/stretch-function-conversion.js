3688601dcc13c38f2ae432be5db2a55ac618dc25
Peter Pham <phamhere@gmail.com>
2018-09-11 16:55:44 -0600

    minor change to stretch-function-conversion.js

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,7 +1,7 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
-let myFunction = () => 
+let myFunction = () => null;
 
 // let anotherFunction = function (param) {
 //   return param;
