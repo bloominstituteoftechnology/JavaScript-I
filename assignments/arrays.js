973582d1ef7b78f63767951f9696fcca562b0e7d
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-11 10:04:27 -0500

    updates

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -87,7 +87,6 @@ let carYears = [];
 for (let i = 0; i < inventory.length; i++) {
     carYears.push(inventory[i].car_year);
 }
-carYears.sort();
 console.log(carYears);
 
 // ==== Challenge 5 ====
@@ -98,14 +97,11 @@ for (let i = 0; i < inventory.length; i++) {
       oldCars.push(inventory[i].car_year);  
     } 
 }
-oldCars.sort();
 console.log(oldCars);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
-
 for (let i = 0; i < inventory.length; i++){
          if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi'){
            BMWAndAudi.push(inventory[i]);
