a504cffa8f06730f1625d67d1c0f3071b2b9d003
Daniel Ferrer <danfrrr13@gmail.com>
2018-09-11 11:21:26 -0400

    fixed last challenge

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -109,7 +109,7 @@ let BMWAndAudi =[];
 
 for (let i = 0; i < inventory.length; i++) {
     if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
-        BMWAndAudi += inventory[i];
+        BMWAndAudi.push(inventory[i]);
     }
 }
 JSON.stringify(BMWAndAudi);
