ce6418e9dbc5158438223cdf1b1a84a0a69a03bb
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-09-10 21:54:28 -0600

    created nested array

--- assignments/objects.js
+++ assignments/objects.js
@@ -94,7 +94,17 @@ console.log(Antonietta.multiplyNums(3, 4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: 'Susan',
+  age: 70 {
+    name: 'George',
+    age: 50 {
+      name: 'Sam',
+      age: 30
+    }
+  }
+}
+
 
 // Log the parent object's name
 
