36c4124876c789ca8b416eea4f875686b4aced0f
Jasmine <jasmineanderson315@yahoo.com>
2018-10-15 17:44:28 -0400

    completed challenge for the day

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,77 @@ const example = {
 }
 
 // Write your intern objects here:
+const Intern1 = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
 
+const Intern2 = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+
+const Intern3 = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const Intern4 = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+const Intern5 = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+console.log(Intern1.name);
+
 // Kennan's ID
+
+console.log(Intern2.id);
 
 // Keven's email
 
+console.log(Intern3.email);
 // Gannie's name
+
+console.log(Intern4.name);
 
 // Antonietta's Gender
 
+console.log(Intern5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+Intern2.speak = "Hello, my name is Kennan!"
+
+console.log(Intern2.speak()); // Ask why there are double parathesis here
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+Intern5.doMath = function(a, b) {
+  console.log(a + b);
+}
+
+console.log(Intern5.doMath());
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
