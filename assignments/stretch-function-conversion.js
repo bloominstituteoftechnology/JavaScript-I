582fdb8082ad2a6494a4be77d6c678a6cd9f9148
jamespagedev <jamespagedev@outlook.com>
2018-10-15 12:20:27 -0700

    stretch-function-conversion.js complete

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -6,19 +6,31 @@ let myFUnction = () => {}
 // let anotherFunction = function (param) {
 //   return param;
 // };
+let anotherFunction = param => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => { return param1 + param2}; // option 1
+console.log(add(1, 2));
+addOtherFunc = (param1, param2) => param1 + param2; // option 2
+console.log(addOtherFunc(1, 2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => { return param1 - param2}; // option 1
+console.log(subtract(1, 2));
+subtractOtherFunc = (param1, param2) => param1 - param2; // option 2
+console.log(subtractOtherFunc(1, 2));
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+const exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
