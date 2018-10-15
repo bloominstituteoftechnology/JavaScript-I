d30f042ec466742d1e717b1cf5202ade54c9e850
Trai Lynne <trailynne@outlook.com>
2018-10-15 15:43:10 -0600

    Completed Challenge 3: Defined 2 Object Methods

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,11 @@ const kennan = {
     "id": 2,
     "name": "Kennan",
     "email": "kdiben1@tinypic.com",
-    "gender": "M"
+    "gender": "M",
+    "speak": () => {
+        console.log("Hello, my name is Kennan!");
+    }
+
 }
 
 const keven = {
@@ -50,7 +54,10 @@ const antonietta = {
     "id": 5,
     "name": "Antonietta",
     "email": "adaine5@samsung.com",
-    "gender": "F"
+    "gender": "F",
+    "multiplyNums": (a, b) => {
+        console.log(a * b);
+    }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -73,10 +80,10 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+kennan.speak();
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums(3, 4);
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
