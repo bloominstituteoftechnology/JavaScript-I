486599991649a17f55ee7232f829db9b5f8c0f83
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-15 15:36:31 -0700

    fixed quesiton 3 on array file

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -92,10 +92,17 @@ console.log(
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = inventory.sort(
-  (a, b) => (a.car_model.toLowerCase() > b.car_model.toLowerCase() ? 1 : -1)
-);
-console.log(carModels);
+let carModels = [];
+for (let i = 0; i < inventory.length; i++) {
+  carModels.push(inventory[i].car_model.toLowerCase());
+}
+
+console.log(carModels.sort());
+
+// let carModels = inventory.sort(
+//   (a, b) => (a.car_model.toLowerCase() > b.car_model.toLowerCase() ? 1 : -1)
+// );
+// console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
