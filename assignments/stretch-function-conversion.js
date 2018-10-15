2d41c01f144925fd75ae8d8bcb395b39b6211b3a
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-10-15 17:39:22 -0500

    worked on stretch functions

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -2,14 +2,20 @@
 
 // let myFunction = function () {};
 
+let myFunction = () => {};
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = param => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let add = (param1, param2) => param1 + param2;
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
