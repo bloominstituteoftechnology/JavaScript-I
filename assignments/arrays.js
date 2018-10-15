3f643d06e7938cdf4bf15a3dfe302f170bca92e4
jamespagedev <jamespagedev@outlook.com>
2018-10-15 12:45:32 -0700

    arrays.js stretch map() complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -64,17 +64,22 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 function getInventoryCar(carId) {
-for (i = 0; i < inventory.length; i++){
-  if (inventory[i].id === carId){
-    return inventory[i];
+  for (i = 0; i < inventory.length; i++){
+    if (inventory[i].id === carId){
+      return inventory[i];
+    }
   }
-}
 
-return "car ID: " + carId + " not found";
+  return "car ID: " + carId + " not found";
 }
 console.log(`Car 33 is a ` + getInventoryCar(33).car_year + ' ' + getInventoryCar(33).car_make + ' ' + getInventoryCar(33).car_model);
 
-
+//// Stretch use map()
+inventory.map(function (car){
+  if (car.id === 33) {
+    console.log(`(stretch map()): Car 33 is a ` + car.car_year.toString() + ' ' + car.car_make + ' ' + car.car_model);
+  }
+});
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
