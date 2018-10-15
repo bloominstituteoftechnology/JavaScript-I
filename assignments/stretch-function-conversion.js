6709d754cb73fb2153420387df2ad76aeacc6cd8
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-10-15 14:43:42 -0700

    add solutions to the stretch-function-convertion.js

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -2,22 +2,38 @@
 
 // let myFunction = function () {};
 
+var myFunction = () => {}
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+var anotherFunction = param => param
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+var add = (param1, param2) => param1 + param2
+
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+var subtract = (param1, param2) => param1-param2
+
+subtract(1,2);
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num * 3)
+console.log(triple);
