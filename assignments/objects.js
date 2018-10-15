2b8f5b3c5e34c5267ec280e08e061f1d33fce657
wcolts2000 <wcolts2000@gmail.com>
2018-10-15 13:39:02 -0700

    completed stretch on es6 syntax...working on object stretch with the speak function

--- assignments/objects.js
+++ assignments/objects.js
@@ -92,15 +92,33 @@ antonietta.multiplyNums(3,4);
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: "Susan",
+  age: 70,
+  child: {
+    name: "George",
+    age: 50,
+    grandchild: {
+      name: "Sam",
+      age: 30
+    }
+  },
+ }
+
+
+ parent.speak = () => { console.log("Hi, my name is " + this.name)};
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.age);
 
 // Log the name and age of the grandchild
+console.log(parent["child"]["grandchild"]["name"], parent.child.grandchild.age);
 
 // Have the parent speak
+parent.speak()
 
 // Have the child speak
 
