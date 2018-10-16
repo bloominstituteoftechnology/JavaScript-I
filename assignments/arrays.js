309b0fd6aca9c8bebb86f650eb1da7a77d212eea
Zayna C <zaynacrick@gmail.com>
2018-10-15 21:37:53 -0400

    commit #2

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -7,7 +7,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":3,"car_make":"Land Rover","car_model":"Defender Ice Edition","car_year":2010},
 {"id":4,"car_make":"Honda","car_model":"Accord","car_year":1983},
 {"id":5,"car_make":"Mitsubishi","car_model":"Galant","car_year":1990},
-{"id":6,"car_make":"Audi","car_model":"Riolet","car_year":1995},
+{"id":6,"car_make":"Audi","car_model":"riolet","car_year":1995},
 {"id":7,"car_make":"Smart","car_model":"Fortwo","car_year":2009},
 {"id":8,"car_make":"Audi","car_model":"4000CS Quattro","car_year":1987},
 {"id":9,"car_make":"Ford","car_model":"Windstar","car_year":1996},
@@ -94,16 +94,27 @@ console.log(carYears)
 // // ==== Challenge 5 ====
 // // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (let i=0;i<inventory.length;i++) {
-    oldCars[i]= inventory[i].car_year;
+for (let i=0; i < carYears.length ;i++) {
+    if (carYears[i] < 2000) {
+        oldCars.push(inventory[i]);
+    }
+    
 }
-
-
+console.log(oldCars);
 
 // // ==== Challenge 6 ====
 // // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi =[];
-console.log();
+let BMWAndAudi = [];
+
+for (let i=0; i<inventory.length; i++) {
+    if (inventory[i].car_make === "BMW") {
+        BMWAndAudi.push(inventory[i]);
+    }
+    if (inventory[i].car_make === "Audi") {
+        BMWAndAudi.push(inventory[i]);
+    }
+}
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
