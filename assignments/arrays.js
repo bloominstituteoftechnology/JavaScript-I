f47ca236c8c87943482d633fe3debde74c5aeb70
VonKleistman <VonKleistman@gmail.com>
2018-10-17 07:36:41 -0500

    Completed all challenges in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -112,12 +112,34 @@ function pullCarYears (thisArray) {
     return tempArray;
 }
 
+function lessThanArray (thisArray, targetValue) {
+    let tempArray = [];
+    for (var i in thisArray){
+        if (thisArray[i] < targetValue){
+            tempArray.push(thisArray[i]);
+        }
+    }
+    return tempArray;
+}
+
+function searchSpecificMakes (thisArray, make1, make2){
+    let tempArray = [];
+    for (var i in thisArray){
+        if (thisArray[i].car_make == make1 || thisArray[i].car_make == make2){
+            tempArray.push(thisArray[i]);
+        }
+    }
+
+    return tempArray;
+}
+
 /////////////////////////////////////////////////
 //               No more Lööps...              //
 /////////////////////////////////////////////////
 
 // ==== Challenge 1 ====
-// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model
+// in the console log provided to you below:
 console.log(`Car 33 is a ${loopToID(inventory, 33).car_year} ${loopToID(inventory, 33).car_make} ${loopToID(inventory, 33).car_model}` );
 
 
@@ -141,14 +163,18 @@ carYears = pullCarYears(inventory);
 console.log(carYears);
 
 // ==== Challenge 5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
+// The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year
+// 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-console.log(); 
+oldCars = lessThanArray(carYears, 2000);
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
-// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi
+// array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
+BMWAndAudi = searchSpecificMakes(inventory, "BMW", "Audi");
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
