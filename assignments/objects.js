9f9de3b29163188c866b8b108029f76f98ae4b05
Jason Cook <the.root.user@gmail.com>
2018-10-17 12:32:08 -0400

    Complete objects stretch challenge

--- assignments/objects.js
+++ assignments/objects.js
@@ -82,16 +82,32 @@ console.log(person5.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  'name': 'Susan',
+  'age': 70,
+  'child': {
+    'name': 'George',
+    'age': 50,
+    'grandchild': {
+      'name': 'Sam',
+      'age': 30,
+    }
+  }
+}
 
 // Log the parent object's name
-
+console.log(parent.name)
 // Log the child's age
-
+console.log(parent.child.age)
 // Log the name and age of the grandchild
+console.log(`Name: ${parent.child.grandchild.name}, Age: ${parent.child.grandchild.age}`)
 
 // Have the parent speak
-
+parent.speak = () => console.log(`I am the parent`);
+parent.speak();
 // Have the child speak
-
+parent.child.speak = () => console.log(`I am the child`);
+parent.child.speak();
 // Have the grandchild speak
+parent.child.grandchild.speak = () => console.log(`I am the grandchild`)
+parent.child.grandchild.speak();
