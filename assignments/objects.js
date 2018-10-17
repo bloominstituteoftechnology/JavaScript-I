86fe6514b4dc056f9142141acfeed77942075e1b
ltann <leeroyleeroy18@gmail.com>
2018-10-16 20:44:41 -0700

    completed reading object data

--- assignments/objects.js
+++ assignments/objects.js
@@ -64,14 +64,15 @@ console.log(intern5)
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(intern1.firstName)
 // Kennan's ID
-
+console.log(intern2.id)
 // Keven's email
-
+console.log(intern3.email)
 // Gannie's name
-
+console.log(intern4.firstName)
 // Antonietta's Gender
+console.log(intern5.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
