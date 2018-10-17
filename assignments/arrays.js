2d423948460a051484793ef0d8a380ddf948c6d8
jrizza88 <jetorres88@gmail.com>
2018-10-16 23:51:03 -0400

    completed challenge 1 of array.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -2,7 +2,11 @@
 
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
 
-let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
+let inventory = [
+    {"id":1,
+"car_make":"Lincoln",
+"car_model":"Navigator",
+"car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},
 {"id":3,"car_make":"Land Rover","car_model":"Defender Ice Edition","car_year":2010},
 {"id":4,"car_make":"Honda","car_model":"Accord","car_year":1983},
@@ -63,9 +67,9 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
-
-
+console.log(` Car 33 is a ${inventory[32].car_make} ${inventory[32].car_model}` );
+// console.log(inventory[32].car_make);
+// ` Car 33 is a *car make goes here* *car model goes here* 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
