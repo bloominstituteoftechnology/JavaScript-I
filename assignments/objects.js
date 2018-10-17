7debe8faa521d6c7db6d967253e934f97a222d81
Noah <noah.stoeckel@gmail.com>
2018-10-17 14:32:07 -0400

    completed stretch in objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -89,17 +89,34 @@ console.log(interns[1].speak())
 const parent = {
   name:'Susan',
   age: 70,
-  child:{}
+  speak: function(){
+    return`hello my name is ${this.name}`
+  },
+  child:{
+    name:'George',
+    age: 50,
+    speak: function(){
+      return`hello my name is ${this.name}`
+    },
+    grandchild:{
+      name:'Sam',
+      age: 30,
+      speak: function(){
+        return`hello my name is ${this.name}`
+      }
+    }
+  }
 }
 
 // Log the parent object's name
-
+console.log(parent.name)
 // Log the child's age
-
+console.log(parent.child.name )
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name,parent.child.grandchild.age  )
 // Have the parent speak
-
+console.log(parent.speak())
 // Have the child speak
-
+console.log(parent.child.speak() )
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak() )
