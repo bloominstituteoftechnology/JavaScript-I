7e7c456ede7e20988130895b1b56cc9b51057e84
VonKleistman <VonKleistman@gmail.com>
2018-10-17 05:41:32 -0500

    fixed a bug involving logging undefined. Finished goals of objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -25,7 +25,7 @@ const genericIntern = {
   gender: undefined,
 
   sayHi () {
-    console.log(`Hello, my name is ${name}!`)
+    return(`Hello, my name is ${this.name}!`)
   },
 
   multiply (num1, num2) {
@@ -64,10 +64,10 @@ console.log(internAntonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(internKennan.sayHi());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(internAntonietta.multiply(8, 13));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
