8c24cc20858272fa7176dca81c1ba3755ca00300
Trip1701 <bj2336@email.vccs.edu>
2018-10-17 17:12:03 -0400

    completed project with stretch

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -23,5 +23,5 @@ exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-const triple = exampleArray.map((num => num * 3));
+const triple = exampleArray.map(num => num * 3);
 console.log(triple);
