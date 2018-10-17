c5f50df43730ca4af8ff87a3f42c8246f1f60f93
amadison17 <angelmadison2015@gmail.com>
2018-10-17 18:17:14 -0400

    objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -69,10 +69,16 @@ console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-console.log("Hello, my name is + (Kennan.name)");
+Kennan.speak = function() {
+  return "Hello, my name is" + (Kennan.name);
+}
+console.log(Kennan.speak)
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+Antonietta.multiplyNums = function(a,b) {
+  return a *b;
+}
+console.log(Antonietta.multiplyNums)
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
