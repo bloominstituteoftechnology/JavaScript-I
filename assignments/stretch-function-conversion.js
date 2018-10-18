c061fc7598316db565743ffa1f9939ce046cbefe
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-10-17 19:08:32 -0700

    completed challenges in stertch-function-conversion.js

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,33 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
-
+console.log(`***************STRETCH FUNCTION CONVERSION*********************`)
 // let myFunction = function () {};
+let myFunction = () => console.log(`I think this works.`);
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-
+let anotherFunction = param => param;
+console.log(anotherFunction("hi"));
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
