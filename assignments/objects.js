508f9eafd0cb4e005411aaa129ec08e8f09cad7b
Pwolfe39 <peterwolfe35@gmail.com>
2018-10-17 22:03:13 -0700

    Basic challenges completed

--- assignments/objects.js
+++ assignments/objects.js
@@ -68,10 +68,10 @@ console.log(intern4.firstName);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-console.log("My name is " + firstName.speak());
+console.log("Hello, my name is " + intern3.firstName);
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(intern5.firstName + "has just multiplied 4 * 3 " + " " + (3 * 4));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
