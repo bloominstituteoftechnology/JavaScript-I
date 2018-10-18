1de0145655140aa3787e624ac7aa501193839138
Eiosx <eiosxenzo@gmail.com>
2018-10-17 22:28:14 -0400

    Completed MVP.

--- assignments/objects.js
+++ assignments/objects.js
@@ -69,10 +69,16 @@ console.log(internAntonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+internKennan.speak = function(){
+  console.log(`Hello, my name is ${internKennan.name}!`);
+}
+internKennan.speak();
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+internAntonietta.multiply = function(x,y){
+  return x * y;
+}
+console.log(internAntonietta.multiply(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
