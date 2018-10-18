a3f3ea16a892959f676b69009334ba366a673d9c
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-10-18 09:11:34 -0400

    complete fx stretch

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -11,24 +11,35 @@ myFunction();
 //   return param;
 // };
 
-let anotherFunction = (param + 2) {
-    
-    console.log(param);
-};
+let anotherFunction = param => param 
 
-anotherFunction();
+console.log(anotherFunction('Another function, alright.'));
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let addFunction = (x, y) => x + y
+
+console.log(addFunction(2,3));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-// exampleArray = [1,2,3,4];
+let subtractFunction = (x, y) => x - y
+
+console.log(subtractFunction(2,3));
+
+
+let exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple)
