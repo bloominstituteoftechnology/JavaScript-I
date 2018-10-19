fe115f9da94a04207ca014f6de509cdaa04483ad
seanwu20 <seanwu20@gmail.com>
2018-10-19 16:03:04 -0400

    Finished tasks

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,34 +11,81 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+    "id": 0,
+    "name": "Example",
+    "email": "examples@you.edu",
+    "gender": "F"
 }
 
 // Write your intern objects here:
+const Mitzi = {
+    "id": 0,
+    "name": "Mitzi",
+    "email": "mmelloy0@psu.edu",
+    "gender": "F"
+}
+
+const Kennan = {
+    "id": 1,
+    "name": "Kennan",
+    "email": "kdiben1@tinypic.com",
+    "gender": "M"
+}
+
+const Keven = {
+    "id": 2,
+    "name": "Keven",
+    "email": "kmummery2@wikimedia.org",
+    "gender": "M"
+}
+
+const Gannie = {
+    "id": 3,
+    name: "Gannie",
+    email: "gmartinson3@illinois.edu",
+    gender: "M"
+}
+
+const Antonietta = {
+    id: 4,
+    name: "Antonietta",
+    email: "adaine5@samsung.com",
+    gender: "F"
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Keven.email);
 // Gannie's name
-
+console.log(Gannie.name);
 // Antonietta's Gender
+console.log(Antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+Kennan.speak = function() {
+    console.log("Hello, my name is Kennan");
+};
+
+Kennan.speak();
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+Antonietta.multiplyNums = function (a,b) {
+    console.log(a*b);
+};
+
+Antonietta.multiplyNums(5, 3);
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
