3b3d005c0c490696aa0eb9353adb2066dd68c800
amadison17 <angelmadison2015@gmail.com>
2018-10-19 07:52:02 -0400

    arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,7 +75,12 @@ console.log(lastCar.length-1);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log(carModels.sort);
+console.log();
+for(let i = 0; carModels[i]; i++) {
+    if(carModels[i]) {
+        return car_models.sort[i];
+    }
+}
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
