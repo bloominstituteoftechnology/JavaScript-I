ac0e0fb9aac806d0c71a07771c6c9a3dde80c900
Joseph Eastman <josephryan96@gmail.com>
2018-10-22 12:18:55 -0700

    Reset

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,71 +18,28 @@ const example = {
 }
 
 // Write your intern objects here:
-const Mitzi = {
-  "id": 1,
-  "name": "Mitzi",
-  "email": "mmelloy0@psu.edu",
-  "gender": "F"
-}
-
-const Kennan = {
-  "id": 2,
-  "name": "Kennan",
-  "email": "kdiben1@tinypic.com",
-  "gender": "M"
-}
-
-const Keven = {
-  "id": 3,
-  "name": "Keven",
-  "email": "kmummery2@wikimedia.org",
-  "gender": "M"
-}
-
-const Gannie = {
-  "id": 4,
-  "name": "Gannie",
-  "email": "gmartinson3@illinois.edu",
-  "gender": "M"
-}
-
-const Antonietta = {
-  "id": 5,
-  "name": "Antonietta",
-  "email": "adaine5@samsung.com",
-  "gender": "F"
-}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(Mitzi.name);
 
 // Kennan's ID
-console.log(Kennan.id);
 
 // Keven's email
-console.log(Keven.email);
 
 // Gannie's name
-console.log(Gannie.name);
 
 // Antonietta's Gender
-console.log(Antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-Kennan.speak = `Hello, my name is ${Kennan.name}!`;
-console.log(Kennan.speak);
-
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
-Antonietta.multiplyNums = function(a,b) {return a * b};
-console.log(Antonietta.multiplyNums(3,4));
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -94,33 +51,18 @@ console.log(Antonietta.multiplyNums(3,4));
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
 // Log the parent object's name
-const parent = {
-  'name': 'Susan',
-  'age': 70,
-  'speak': function() {return "Hello, my name is" + " " + this.name + "!";},
-  child: {
-    'name': 'George',
-    'age': 50,
-    'speak': function() {return "Hello, my name is" + " " + this.name + "!";},
-    grandChild: {
-      'name': 'Sam',
-      'age': 30,
-      'speak': function() {return "Hello, my name is" + " " + this.name + "!";}
-    }
-  }
-}
+
 
 // Log the child's age
-console.log(parent.child.age);
 
 // Log the name and age of the grandchild
-console.log(parent.child.grandChild,name.age);
+
 
 // Have the parent speak
-console.log(parent.speak());
+
 
 // Have the child speak
-console.log(parent.child.speak());
+
 
 // Have the grandchild speak
-console.log(parent.child.grandChild.speak());
+
