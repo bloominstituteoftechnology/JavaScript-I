c47fee4f7e754b2442249c753bc4f7883d9610e6
Ej <ejaona@gmail.com>
2018-11-17 12:10:29 -0600

    Worked on objects.js stretch challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -82,14 +82,40 @@ console.log(Antonietta.multiplyNums(4, 5));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
+const parent = {
+  name: "Susan",
+  age: 70,
+  child: {
+    name: "Gorge",
+    age: 50,
+    grandchild: {
+      name: "Sam",
+      age: 30,
+      speak: function() {
+        name = this.name;
+        console.log(`my name is ${name}`);
+      }
+    },
+    speak: function() {
+      name = this.name;
+      console.log(`my name is ${name}`);
+    }
+  },
+  speak: function() {
+    name = this.name;
+    console.log(`my name is ${name}`);
+  }
+};
+
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
-
+console.log(`${parent.child.grandchild.name}  ${parent.child.grandchild.age}`);
 // Have the parent speak
-
+parent.speak();
 // Have the child speak
-
+parent.child.speak();
 // Have the grandchild speak
+parent.child.grandchild.speak();
