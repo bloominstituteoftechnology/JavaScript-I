4d12f0c573115bbdf84dc174ce116c462c52c6b1
Matt Basile <matthewbasile2@gmail.com>
2018-11-26 16:01:31 -0500

    Turned challenge 3 into a function. LINES array.js 81, 84-87.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -78,11 +78,13 @@ console.log(`${make}, ${model}`);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
+function filterCarModel(){
 for(let i = 0; i<inventory.length; i++){
     carModels.push(inventory[i].car_model);
 }
-carModels = carModels.sort();
-console.log(carModels);
+}
+console.log(filterCarModel());
+console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
