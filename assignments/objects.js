b1a646c18c34007011be2f4f93b3b21a514fb2cf
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-11-26 15:47:49 -0600

    logged different information for each intern

--- assignments/objects.js
+++ assignments/objects.js
@@ -47,7 +47,7 @@ const intern4 = {
   "gender": "M"
 }
 
-const intern4 = {
+const intern5 = {
   "id": 4,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
@@ -63,14 +63,16 @@ const intern4 = {
 
 // Mitzi's name
 
+console.log(intern1["name"]);
+
 // Kennan's ID
-
+console.log(intern2["id"]);
 // Keven's email
-
+console.log(intern3["email"]);
 // Gannie's name
-
+console.log(intern4["name"]);
 // Antonietta's Gender
-
+console.log(intern5["gender"]);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
