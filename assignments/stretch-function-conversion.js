93fcb0499d3f9d10277fe8836578db69560bb745
Jacob Bryan <jtb36688@gmail.com>
2018-11-26 17:25:13 -0500

    completed all tasks

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -5,19 +5,23 @@
 // let anotherFunction = function (param) {
 //   return param;
 // };
+let anotherFunction = (param) => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+let triple = exampleArray.map(num => num * 3);
