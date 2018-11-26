e03e62ff9690b90b692d0bd16edbe8bb8b551517
Nathan Thomas <nwthomas@me.com>
2018-11-26 12:14:25 -0800

    Finish third challenge in object.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -82,8 +82,20 @@ console.log(`The employee's gender is ${antonietta.gender}.`);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+kennan.speak = function() {
+  return `Hello, my name is ${kennan.name}!`;
+};
+
+console.log(kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+antonietta.multiply = function(x, y) {
+  return x * y;
+};
+
+console.log(`The answer is ${antonietta.multiply(3, 4)}.`);
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
