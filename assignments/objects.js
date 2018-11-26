a1dacd47bcad58f1c3e7d6a682a874ba8ddd4ce0
David Flack <davidscottflack@gmail.com>
2018-11-26 13:54:56 -0600

    Create intern objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,36 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const Mitzi = {
+  id: 1,
+  email: 'mmelloy0@psu.edu',
+  name: 'Mitzi',
+  gender: 'F'
+}
+const Kennan = {
+  id: 2,
+  email: 'kdiben1@tinypic.com',
+  name: 'Kennan',
+  gender: 'M'
+}
+const Keven = {
+  id: 3,
+  email: 'kmummery2@wikimedia.org',
+  name: 'Keven',
+  gender: 'M'
+}
+const Gannie = {
+  id: 4,
+  email: 'gmartinson3@illinois.edu',
+  name: 'Gannie',
+  gender: 'M'
+}
+const Antonietta = {
+  id: 5,
+  email: 'adaine5@samsung.com',
+  name: 'Antonietta',
+  gender: 'F'
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
