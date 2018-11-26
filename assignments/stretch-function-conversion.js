ed802484e6f39c625eab6d35a1c9b13eeec74cbf
Scott Vojik <scott.vojik@gmail.com>
2018-11-26 12:30:39 -0800

    assignment and stretch complete

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -2,22 +2,35 @@
 
 // let myFunction = function () {};
 
+let myFunction = _ => {};
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = param => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2;
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-// exampleArray = [1,2,3,4];
+let subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
+
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => num * 3);
+console.log(triple);
