39870dec6c51c1d4867ac054a24e2d16c683bf79
ltrii <lelandtr@gmail.com>
2018-11-26 11:13:11 -0800

    Work on Arrays and Objects fixup

--- assignments/objects.js
+++ assignments/objects.js
@@ -83,7 +83,9 @@ console.log(antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-kennan.speak = "Hello, my name is Kennan!";
+kennan.speak = function() {
+  return "Hello, my name is Kennan!";
+}
 
 console.log(kennan.speak());
 
