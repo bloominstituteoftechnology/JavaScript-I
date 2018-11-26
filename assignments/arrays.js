c783b2b5710d418a616674b1a8be8ead182fb5b2
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-11-26 17:34:14 -0500

    Stretch: Converted for loops into higher order functions

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -87,6 +87,15 @@ carModels.sort();
 
 console.log(carModels);
 
+
+// Stretch Solution for Challenge 3
+let stretchCarModels = inventory.map((model) => {
+    return model.car_model;
+}).sort();
+
+console.log(stretchCarModels);
+
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
@@ -96,6 +105,16 @@ for (let i = 0; i < inventory.length; i++) {
 }
 
 console.log(carYears);
+
+
+// Stretch Solution for Challenge 4
+
+let stretchCarYears = inventory.map((year) => {
+    return year.car_year;
+})
+
+console.log(stretchCarYears);
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -109,6 +128,19 @@ for (let i = 0; i < carYears.length; i++) {
 
 console.log(oldCars.length); 
 
+
+// Stretch Solution for Challenge 5
+
+let stretchOldCars = inventory.filter((year) => {
+    if (year.car_year < 2000) return year.car_year;
+}).map((year) => {
+    return year.car_year;
+})
+
+
+console.log(stretchOldCars.length);
+
+
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
@@ -120,5 +152,11 @@ for (let i = 0; i < inventory.length; i++) {
 }
 console.log(JSON.stringify(BMWAndAudi));
 
+// Stretch Solution for Challenge 6
 
+let stretchBMWAudi = inventory.filter((fancy) => {
+    return fancy.car_make === "BMW" || fancy.car_make === "Audi";
+})
+
+console.log(JSON.stringify(stretchBMWAudi));
 
