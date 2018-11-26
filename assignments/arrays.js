1fd5782a02a93b256a4dbd90a00637454dc66e83
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-11-26 16:43:32 -0600

    challenge 5 complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -90,9 +90,11 @@ let carYears = [2009, 2001, 2010, 1983, 1990, 1995, 2009, 1987, 1996, 2000, 2004
 console.log(carYears);
 
 // ==== Challenge 5 ====
-// The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars =[];
-console.log(); 
+// The car lot manager needs to find out how many cars are older than the year 2000. 
+//Using the carYears array you just created, find out 
+//how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
+let oldCars =[1983, 1990, 1995, 1987, 1996, 1997, 1999, 1997, 1999, 1987, 1995, 1994, 1985, 1997, 1992, 1993, 1964, 1999, 1991, 1997, 1992, 1998, 1965, 1996, 1995, 1996, 1999];
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
