3cf7c3193ef8df03c4cea92e1b5180abb0bf8401
MichaelNey <ney.quentin.michael@gmail.com>
2018-11-26 12:25:15 -0800

    Outputting Challenge 2 in console

--- assignments/objects.js
+++ assignments/objects.js
@@ -55,15 +55,15 @@ const Antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Keven.email);
 // Gannie's name
-
+console.log(Gannie.name);
 // Antonietta's Gender
-
+console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
