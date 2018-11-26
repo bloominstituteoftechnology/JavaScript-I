e14daf7aea4daca9b31f06a9860714b7b96f1cba
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-11-26 16:08:15 -0600

    Completed Stretch goal and converted ES5 functions in the stretch-function-conversion.js file to use ES6 arrow functions

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,23 +1,36 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {};
+//let myFunction = function () {};
+
+let myFunction = () => {};
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
+
+let anotherFunction = param => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1+param2;
+// console.log(add(2,4));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1-param2;
+// console.log(subtract(10,5));
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
+// console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(x => x * 3);
 // console.log(triple);
