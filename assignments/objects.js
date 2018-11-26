e67830201836506ac3be67631e88a48624b45c48
Alex Bott <Abott1222@gmail.com>
2018-11-26 12:31:20 -0800

    initial commit

--- assignments/objects.js
+++ assignments/objects.js
@@ -9,6 +9,12 @@
 // 4,gmartinson3@illinois.edu,Gannie,M
 // 5,adaine5@samsung.com,Antonietta,F
 
+
+
+
+
+
+
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
   "id": 0,
@@ -19,19 +25,54 @@ const example = {
 
 // Write your intern objects here:
 
+const intern1 = {
+  id:1,
+  email:'mmelloy0@psu.edu',
+  firstName:'Mitzi',
+  gender:'F'
+}
+
+const intern2 = {
+  id:2,
+  email:'kdiben1@tinypic.com',
+  firstName:'Kennan',
+  gender:'M'
+}
+
+const intern3 = {
+  id:3,
+  email:'kmummery2@wikimedia.org',
+  firstName:'Keven',
+  gender:'M'
+}
+
+const intern4 = {
+  id:4,
+  email:'gmartinson3@illinois.edu',
+  firstName:'Gannie',
+  gender:'M'
+}
+
+const intern5 = {
+  id:5,
+  email:'adaine5@samsung.com',
+  firstName:'Antonietta',
+  gender:'F'
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(intern1.firstName);
 // Kennan's ID
-
+console.log(intern2.id);
 // Keven's email
-
+console.log(intern3.email);
 // Gannie's name
-
+console.log(intern4.gender);
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
