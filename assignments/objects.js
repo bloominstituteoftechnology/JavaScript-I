6b0a81bed2ae4983632bcd3feab6ad5beb8a3a75
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2018-11-26 14:40:49 -0600

    challenge2

--- assignments/objects.js
+++ assignments/objects.js
@@ -61,10 +61,11 @@ console.log(intern1.name);
 // Kennan's ID
 console.log(intern2.id);
 // Keven's email
-
+console.log(intern3.email);
 // Gannie's name
-
+console.log(intern4.name);
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
