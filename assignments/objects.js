11e531a6662fce38cb68431cd07290edd78452f3
Mike Landers <landers.mike@gmail.com>
2018-11-26 12:17:59 -0800

    Starting Objects file

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,11 +19,53 @@ const example = {
 
 // Write your intern objects here:
 
+const intern = [
+  {
+    "id": 0,
+    "name": "Michael",
+    "email": "email@email.com",
+    "gender": "M"
+  },
+  {
+    "id": 1,
+    "name": "Mitzi",
+    "email": "mmelloy0@psu.edu",
+    "gender": "F"
+  },
+  {
+    "id": 2,
+    "name": "Kennan",
+    "email": "kdiben1@tinypic.com",
+    "gender": "M"
+  },
+  {
+    "id": 3,
+    "name": "Keven",
+    "email": "kmummery2@wikimedia.org",
+    "gender": "M"
+  },
+  {
+    "id": 4,
+    "name": "Gannie",
+    "email": "gmartinson3@illinois.edu",
+    "gender": "M"
+  },
+  {
+    "id": 5,
+    "name": "Antonietta",
+    "email": "adaine5@samsung.com",
+    "gender": "F"
+  },
+  
+]
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+
+console.log(intern[1]["name"])
 
 // Kennan's ID
 
