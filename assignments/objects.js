5270362d545143989e5c916a1976b902b88b9195
David Flack <davidscottflack@gmail.com>
2018-11-26 15:37:57 -0600

    Complete objects.js stretch goals

--- assignments/objects.js
+++ assignments/objects.js
@@ -107,13 +107,14 @@ const parent = {
 }// parent
 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name, parent.child.grandchild.age);
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
