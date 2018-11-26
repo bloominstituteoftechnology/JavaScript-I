9b10be9da354d10ca6780deaafa617b1df41f267
Olivia Osborn <olioz95@gmail.com>
2018-11-26 15:50:43 -0500

    wrote out intern objects for challenge 1 in objects.js file

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,40 @@ const example = {
 }
 
 // Write your intern objects here:
+const mitzi = {
+  "id": 1,
+  "email": "mmelloy0@psu.edu",
+  "name": "Mitzi",
+  "gender": "F",
+}
 
+const kennan = {
+  "id": 2,
+  "email": "kdiben1@tinypic.com",
+  "name": "Kennan",
+  "gender": "M",
+}
+
+const keven = {
+  "id": 3,
+  "email": "kmummery2@wikimedia.org",
+  "name": "Keven",
+  "gender": "M",
+}
+
+const gannie = {
+  "id": 4,
+  "email": "gmartinson3@illinois.edu",
+  "name": "Gannie",
+  "gender": "M",
+}
+
+const antonietta = {
+  "id": 5,
+  "email": "adaine5@samsung.com",
+  "name": "Antonietta",
+  "gender": "F",
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
