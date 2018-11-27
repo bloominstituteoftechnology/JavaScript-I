179f0d14ef6de6dd5a840c66cf886356fc4b24dc
umbur <work1415@gmail.com>
2018-11-26 17:11:23 -0700

    added JSON.stringify() method to the last challenge of arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -117,11 +117,15 @@ let BMWAndAudi =[];
    for (let i = 0; i < inventory.length; i++){
        if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi'){
          BMWAndAudi.push(inventory[i]);
-         console.log(BMWAndAudi);
+        
+        
+        
        }
+      
    }
+  
+   console.log(JSON.stringify(BMWAndAudi));
 
-
-
+ 
 
 
