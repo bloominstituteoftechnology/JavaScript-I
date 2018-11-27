76586c4194ad37e2877c98fcbb3192804b64223b
Mike Landers <landers.mike@gmail.com>
2018-11-27 08:47:03 -0800

    * Corrected array challenege 5 which way it was looking at the years.
    * Corrected strech conversion for map

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -2,18 +2,18 @@
 
 // let myFunction = function () {};
 
-// let myFunction = () => ;
+const myFunction = () => {};
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 
-let anotherFunction = (param) => param;
+const anotherFunction = param => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-let add = (param1, param2) => param1 + param2;
+const add = (param1, param2) => param1 + param2;
 
 // add(1,2);
 
@@ -21,15 +21,15 @@ let add = (param1, param2) => param1 + param2;
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-let subtract = (param1, param2) => param1 - param2;
+const subtract = (param1, param2) => param1 - param2;
 
 // subtract(1,2);
 
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-const triple = exampleArray.map = num => num * 3;
-console.log(triple);
+const triple = exampleArray.map(num => num * 3);
+console.log(triple)
 
