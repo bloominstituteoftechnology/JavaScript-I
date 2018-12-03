9ee64093ebe0240ccd33b594e85c7b5c95c51bbe
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-03 15:50:37 -0600

    First Commit

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -1,4 +1,4 @@
-// To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
+// hi. To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
 
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
 
