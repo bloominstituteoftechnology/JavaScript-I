0798940a30ebb4409881e74731fd280090826a0c
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-12-03 16:42:02 -0600

    finally got challenge 3 on the array assignment.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,11 +74,14 @@ for (let lastCar = 0; lastCar < inventory.length; lastCar++) {
     if (inventory[lastCar].id === 50)
         console.log(inventory[lastCar])
 }
-
-
+ 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
+for (let i = 0; i < inventory.length; i++) {
+  carModels.push(inventory[i].car_model);
+}
+console.log(carModels.sort());
 
 
 
