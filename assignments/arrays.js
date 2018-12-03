e13c75252cb28ff308124b395ea46b108e23946b
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-12-03 16:55:56 -0600

    finished challenge 4

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,10 +63,8 @@ let inventory = [{ "id": 1, "car_make": "Lincoln", "car_model": "Navigator", "ca
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-for (let unKnownCar = 0; unKnownCar < inventory.length; unKnownCar++) {
-    if (inventory[unKnownCar].id === 33)
-        console.log(inventory[unKnownCar])
-}
+console.log(`${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}` );
+
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 
@@ -89,7 +87,15 @@ console.log(carModels.sort());
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i].car_year);
+  }
+
+
+console.log(carYears);
+
+
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
