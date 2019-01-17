3696c612bfa740c903a9f95f43391289f6d9b28b
ThatGuyUKnow593 <trenteuclide409@gmail.com>
2019-01-16 21:16:15 -0600

    finished challeng 2 in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,14 +63,32 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+//console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+
+for (let i = 0; i < inventory.length; i++) {
+    let missingCar = 33;
+    
+    if(i === missingCar - 1){
+        console.log('Car ' + inventory[i].id + ' is a ' + inventory[i].car_year + ' ' + inventory[i].car_make + ' ' + inventory[i].car_model);
+    }
+   
+}
 
 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log();
+
+function finalCar(){
+    let index = inventory.length - 1;
+
+    return(inventory[index].car_make + ' ' + inventory[index].car_model)
+}
+
+console.log(finalCar());
+
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
--- assignments/objects.js
+++ assignments/objects.js
@@ -73,11 +73,11 @@ const antonietta = {
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-    kennan.speak = function() {
+    kennan.speak =  function(){
         return('Hello, my name is Kennan!');
-    };
+    }
 
-    console.log(kennan.speak);
+    console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
