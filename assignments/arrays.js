f4fcea37598d4400169d8c573a82fca8baf7c12b
mmorpchicago <mmohamed.rp@gmail.com>
2019-01-17 16:56:37 -0600

    Started working on Objects.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -78,7 +78,7 @@ console.log();
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for (i=0; i<inventory.length; i++){
+for (let i=0; i<inventory.length; i++){
     carModels.push(inventory[i].car_model)
 }
 console.log(`Car Models Sorted: ${carModels.sort()}`);
@@ -86,7 +86,7 @@ console.log(`Car Models Sorted: ${carModels.sort()}`);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (i = 0; i < inventory.length; i++) {
+for (let i = 0; i < inventory.length; i++) {
     carYears.push(inventory[i].car_year);
 }
 console.log(`Car Years: \n ${carYears}`);
@@ -94,7 +94,7 @@ console.log(`Car Years: \n ${carYears}`);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-for (i = 0; i < carYears.length; i++) {
+for (let i = 0; i < carYears.length; i++) {
     if (carYears[i] < 2000) {
         oldCars.push(carYears[i]);
     }
@@ -104,7 +104,7 @@ console.log(`Old Cars: ${oldCars.length}`);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-for (i=0; i < inventory.length; i++) {
+for (let i=0; i < inventory.length; i++) {
     if ((inventory[i].car_make === "Audi") || (inventory[i].car_make === "BMW")) {
         BMWAndAudi.push(inventory[i]);
     }
--- assignments/objects.js
+++ assignments/objects.js
@@ -18,16 +18,26 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const interns = [
+  {id: 1, email: "mmelloy0@psu.edu", firstName: "Mitzi", gender: "F" },
+  {id: 2, email: "kdiben1@tinypic.com", firstName: "Kennan", gender: "M" },
+  {id: 3, email: "kmummery2@wikimedia.org", firstName: "Keven", gender: "M" },
+  {id: 4, email: "gmartinson3@illinois.edu", firstName: "Gannie", gender: "M" },
+  {id: 4, email: "adaine5@samsung.com", firstName: "Antonietta", gender: "F" },
+];
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(interns[0].firstName);
 
 // Kennan's ID
+let kenID = 2
+console.log(interns[kenID].id);
 
 // Keven's email
+console.log(Element(interns))
 
 // Gannie's name
 
