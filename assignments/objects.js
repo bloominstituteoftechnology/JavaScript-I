64ea80dda7f71f4cf96e197fe8f55c6bc161eae0
ThatGuyUKnow593 <trenteuclide409@gmail.com>
2019-01-16 20:35:42 -0600

    objects.js challenge 3 complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -73,9 +73,21 @@ const antonietta = {
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+    kennan.speak = function() {
+        return('Hello, my name is Kennan!');
+    };
+
+    console.log(kennan.speak);
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
+    antonietta.multiplyNums = function(x, y) {
+        return(x*y);
+    }
+
+    console.log(antonietta.multiplyNums(3,4));
+    
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
