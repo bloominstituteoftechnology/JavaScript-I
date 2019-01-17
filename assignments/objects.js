5177542a39bed26b01a95158cf75968d76146f23
rberriz <rberriz@icloud.com>
2019-01-16 23:48:03 -0500

    final with all stretch revised

--- assignments/objects.js
+++ assignments/objects.js
@@ -94,15 +94,15 @@ console.log(antonietta.multiplyNums(3,4));
 const parent = {
   name: 'Susan',
   age: "70",
-  speak: 'Hello my name is {this.name}',
+  speak: `Hello my name is ${this.name}`,
   child: {
     name: 'George',
     age: 50,
-    speak: 'Hello my name is {this.name}',
+    speak: `Hello my name is ${this.name}`,
     grandchild: {
       name: 'Sam',
       age: 30,
-      speak: 'Hello my name is {this.name}'
+      speak: `Hello my name is ${this.name}`
       }
    }
 }
