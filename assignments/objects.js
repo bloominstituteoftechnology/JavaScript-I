426041b5f1e338d532efb8ad364eed8730ffa8f6
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-19 23:40:02 -0800

    Completed the MVP tasks on objects.js moving onto arrays.js before stretch.

--- assignments/objects.js
+++ assignments/objects.js
@@ -49,7 +49,8 @@ const antonietta = {
   id: 5, 
   name: 'Antonietta', 
   email: 'adaine5@samsung.com', 
-  gender: 'F' 
+  gender: 'F',
+  multiplyNums: (a, b) => a * b, 
 };
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
@@ -74,7 +75,7 @@ console.log(antonietta.gender);
 console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
