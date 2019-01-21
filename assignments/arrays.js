099b6b1ed35eb225982179f475a5f3251ea25ceb
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-20 16:17:49 -0800

    Finished off the stretch goals page moving onto stretch task on objects.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,7 +104,7 @@ let oldCars = carYears.filter((x) => x < 2000);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = inventory.filter((x) => x.car_make == "BMW" || x.car_make == "Audi");
- console.log(JSON.stringify(BMWAndAudi));
+//  console.log(JSON.stringify(BMWAndAudi));
 
 
 
