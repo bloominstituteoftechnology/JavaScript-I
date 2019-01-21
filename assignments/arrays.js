7bf04e7056eb699f7fb74119e133a3acbbe85501
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-20 16:05:29 -0800

    MVP is now complete had a total aha moment when figuring out what JSON stands for and digging into its methods. will be biting into stretch shortly.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -99,12 +99,12 @@ let oldCars = carYears.filter((x) => x < 2000);
 //      oldCars.push(carYears[i])
 //      }  
 // }
-// console.log(oldCars.length); 
+//  console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = inventory.filter((x) => x.car_make == "BMW" || "Audi");
- console.log(BMWAndAudi);
+let BMWAndAudi = inventory.filter((x) => x.car_make == "BMW" || x.car_make == "Audi");
+ console.log(JSON.stringify(BMWAndAudi));
 
 
 
