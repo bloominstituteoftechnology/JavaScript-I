3d58f97c90cef2d7bfb936f0fbd9b1080613cfc6
Marc Thiongo <marcthuo@gmail.com>
2019-01-28 17:57:40 -0500

    edit objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -74,11 +74,24 @@ console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-const intern2 = (kennan.speak());
+// console.log(kennan.speak());
+
+const kennan = {
+  speak () {
+    return "Hello, my name is Kennan!";
+  }
+}
 console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+const antonietta = {
+  multiplyNums (a, b) {
+    return a * b;
+  }
+}
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
