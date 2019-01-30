50f8531983ffccec8f27c1f5685fd355704fc60c
myasia barbee <myasiabarbee@icloud.com>
2019-01-30 00:14:50 -0500

    ...

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,45 +18,45 @@ const example = {
 }
 
 // Write your intern objects here:
-const interns = {
+const interns = [
+  {
   "id": 1,
   "name": "Mitzi",
   "email": "mmelloy0@psu.edu",
   "gender": "F"
-}
-
-const interns= {
+},
+{
 "id": 2,
 "name": "Kennan",
 "email": "kdiben1@tinypic.com",
 "gender": "M"
-}
-
-const interns= {
+},
+{
 "id": 3,
 "name": "Keven",
 "email": "kmummery2@wikimedia.org",
 "gender": "M"
-}
-
-const interns= {
+},
+{
 "id": 4,
 "name": "Gannie",
 "email": "gmartinson3@illinois.edu",
 "gender": "M"
-}
-
-const interns= {
+},
+{
 "id": 5,
 "name": "Aintonietta",
 "email": "adaine5@samsung.com",
 "gender": "F"
 }
+]
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+
+console.log(interns[0].name);
 
 // Kennan's ID
 
