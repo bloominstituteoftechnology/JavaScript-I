33ccea1808ebb3b1c954b66582c9226f7f93765a
Orlando Castillo <omaddoc@gmail.com>
2019-01-30 00:02:08 -0500

    finished challenge 2 of object.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,5 +1,7 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
+console.log('Objects')
 
+console.log('Challenge 1')
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
@@ -18,21 +20,56 @@ const example = {
 }
 
 // Write your intern objects here:
+const interns = [
+  {
+    id: 1,
+    name: 'Mitzi',
+    email: 'mmelloy@psu.edu',
+    gender: 'F'
+  },
+  {
+    id: 2,
+    name: 'Kennan',
+    email: 'kdiben1@tinypic.com',
+    gender: 'M'
+  },
+  {
+    id: 3,
+    name: 'Keven',
+    email: 'kmummery2@wikimedia.org',
+    gender: 'M'
+  },
+  {
+    id: 4,
+    name: 'Gannie',
+    email: 'gmartinson3@illinois.edu',
+    gender: 'M'
+  },
+  {
+    id: 5,
+    name: 'Antonietta',
+    email: 'adaine5@samsung.com',
+    gender: 'F'
+  }
+]
 
+console.log(interns);
 
+console.log('Challenge 2')
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(`Name of ID 1: ${interns[0].name}`);
 // Kennan's ID
-
+console.log(`ID of ID 2: ${interns[1].id}`);
 // Keven's email
-
+console.log(`Email of ID 3: ${interns[2].email}`);
 // Gannie's name
-
+console.log(`Name of ID 4: ${interns[3].name}`)
 // Antonietta's Gender
-
+console.log(`Gender of ID 5: ${interns[4].gender}`);
+console.log('Challenge 3');
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
