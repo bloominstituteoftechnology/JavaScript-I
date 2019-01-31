411d1b80f4eb74de9880206d7fca63fc7fbdec4c
myasia barbee <myasiabarbee@icloud.com>
2019-01-30 22:59:44 -0500

    Challenge 4

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -80,7 +80,9 @@ console.log();
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let i=0; i < inventory.length; i++)
+carYears.push(inventory[i].car_year)
+console.log(carYears);
 //map
 
 // ==== Challenge 5 ====
