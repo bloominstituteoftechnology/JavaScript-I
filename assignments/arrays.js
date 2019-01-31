a896d0c4399a065368f4463d2477bd2637f53512
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-01-30 21:04:18 -0600

    worked on challenges 1-4 in array.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -64,29 +64,50 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 
-checkCar(): function {
-    console.log(`Car ${id} is a ${car_year} ${car_make} ${car_model}`)
-}
+console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`)
 
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
-
+// console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log();
+console.log(`${inventory[49].car_make} ${inventory[49].car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-console.log();
+
+function alphabeticalOrder() {
+
+    let carModels = [''];
+
+    for (let i = 0; i < inventory.length; i++) {
+        carModels[i] += inventory[i].car_model;
+    }
+
+    let alphabetModel = carModels.sort()
+    console.log(alphabetModel);
+}
+
+console.log(alphabeticalOrder(inventory));
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 
-for (let i = 0, i < inventory.length; i++) {
+// for (let i = 0, i < inventory.length; i++) {
     
+// }
+
+function years() {
+
+    let carYears = [''];
+
+    for (let i = 0; i < inventory.length; i++) {
+        carYears[i] += inventory[i].car_year;
+    }
+
+    console.log(carYears);
 }
+
 
 let carYears = [];
 console.log();
