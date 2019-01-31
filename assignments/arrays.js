41cc2dce4b08cbf1158d69a7ce9c1c01901ee321
myasia barbee <myasiabarbee@icloud.com>
2019-01-30 23:01:43 -0500

    Challenge 3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -75,8 +75,13 @@ console.log('Car 0 is a 1999 Lincoln Town car');
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
 
+for (let i=0; i < inventory.length; i++)
+
+carModels.sort();
+
+console.log(carModels);
+//sort
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
