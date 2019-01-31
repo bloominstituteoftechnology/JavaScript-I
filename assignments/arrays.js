df6656ea1ae318b3c19b78afb4276a6d590a1246
myasia barbee <myasiabarbee@icloud.com>
2019-01-30 23:20:01 -0500

    ...

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -65,7 +65,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 console.log(`Car 33 is a` (inventory[32].car_year), (inventory[32].car_make), (inventory[32].car_model));
 
-
+// how do I properly form this console.log??//
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
@@ -82,14 +82,14 @@ for (let i=0; i < inventory.length; i++)
 carModels.sort();
 
 console.log(carModels);
-//sort
+//sort?
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 for (let i=0; i < inventory.length; i++)
 carYears.push(inventory[i].car_year)
 console.log(carYears);
-//map
+//map?
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -104,7 +104,7 @@ if (carYears[i] < 2000){
 
     console.log(`There are  ${oldCars.length} cars older than the year 2000.`) 
 
-//filter
+//filter?
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
@@ -119,4 +119,4 @@ for (let i=0; i < inventory.length; i++){
 }
 console.log(JSON.stringify(BMWAndAudi));
 
-//filter
+//filter?
