697693c528770766518370cbb37c77b1081171b4
Greyflanel <richardlovelace811@gmail.com>
2019-03-11 17:54:49 -0500

    My third commit. I just have one more to go on arrays and I can move on to objects.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -79,14 +79,19 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
-carYears = inventory.map( (element) => element.car_year);
-console.log(carYears);
+// let carYears = [];
+// carYears = inventory.map( (element) => element.car_year);
+// console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = [];
-console.log(); 
+// let oldCars = [];
+// for (let i = 0; i < carYears.length; i++) {
+//     if (carYears[i] < 2000) {
+//         oldCars.push(carYears[i]);
+//     }
+// }
+// console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
