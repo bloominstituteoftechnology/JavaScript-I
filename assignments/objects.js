15a84a2aeda6eb2af75544bc1935bb9778c3a6fa
Enoka Jaona <Ejaona@gmail.com>
2019-03-21 09:09:41 -0500

    Refactor complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -67,7 +67,7 @@ console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-Kennan.speak = () => "Hello, my name is Kennan";
+Kennan.speak = _ => "Hello, my name is Kennan";
 console.log(Kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
