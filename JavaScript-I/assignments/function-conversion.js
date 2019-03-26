796d4d21409d5cdf37fbb8f3039d6891e83c4721
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-03-25 21:03:03 -0500

    Add files via upload

---
+++ JavaScript-I/assignments/function-conversion.js
@@ -0,0 +1,44 @@
+// Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+
+// let myFunction = function () {
+// console.log("Function was invoked!");
+// };
+// myFunction();
+const myfunction = () => {
+    console.log("function was invoked!");
+};
+myfunction();
+
+// let anotherFunction = function (param) {
+//   return param;
+// };
+// anotherFunction("Example");
+const anotherfunction = (param) => {
+    return param;
+};
+console.log(anotherfunction());
+
+// let add = function (param1, param2) {
+//   return param1 + param2;
+// };
+// add(1,2);
+const add = (param1, param2) => {
+    return param1 + param2;
+};
+console.log(add());
+
+// let subtract = function (param1, param2) {
+//   return param1 - param2;
+// };
+// subtract(1,2);
+const subtract = (param1, param2) => {
+    return param1 - param2;
+};
+console.log(subtract());
+// Stretch
+
+// exampleArray = [1,2,3,4];
+// const triple = exampleArray.map(function (num) {
+//   return num * 3;
+// });
+// console.log(triple);
