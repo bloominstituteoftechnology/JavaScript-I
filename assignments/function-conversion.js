12e6f75e9ec4856665397b2d4088b450be427cd2
JimmyMcBride <mcbride967@gmail.com>
2019-03-27 00:07:25 -0500

    did the stretch arrow function conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,33 +1,28 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 let myFunction = () => {
-    console.log("Function was invoked!")
-    }
-    myFunction()
-    
-    let anotherFunction = (param) => {
-          console.log(param)
-    }
-    anotherFunction("Example")
-    
-    let add = (param1, param2) => {
-      console.log(param1 + param2)
-    }
-    add(1,2)
-    
-    let subtract = (param1, param2) => {
-      return param1 - param2
-    };
-    console.log(subtract(1,2))
-    
-    
-    // Stretch
-    
-    exampleArray = [1,2,3,4];
-    const triple = exampleArray.map(function (num) {
-        for (let i = 0; i < exampleArray.length; i++){
-            return num * 3;
-        }
-      
-    })
-    console.log(triple)
+console.log("Function was invoked!")
+}
+myFunction()
+
+let anotherFunction = (param) => {
+        console.log(param)
+}
+anotherFunction("Example")
+
+let add = (param1, param2) => {
+    console.log(param1 + param2)
+}
+add(1,2)
+
+let subtract = (param1, param2) => {
+    return param1 - param2
+};
+console.log(subtract(1,2))
+
+
+// Stretch
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => {return num * 3})
+console.log(triple);
