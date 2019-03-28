5e7a2f2d838f90e9eff86d1ede5eece1409901e5
Chris Jones <chrisjonesdesign85@gmail.com>
2019-03-28 06:11:38 -0400

    finalized the project, going for the stretch goal

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,33 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => { console.log("Function was invoked!") };
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => { 
+   console.log(param);
+}
+anotherFunction('Chris')
+
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => { console.log(param1 + param2)}
+add(1,2)
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+let subtract = (param1, param2) => { console.log(param1 - param2)};
+subtract(1,2);
 
 // Stretch
 
