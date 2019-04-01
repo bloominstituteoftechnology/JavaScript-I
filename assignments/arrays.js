8e497437998a276a39fb04de893238c966c0d84d
13JonathanDavid <48808640+13JonathanDavid@users.noreply.github.com>
2019-04-01 19:02:35 -0400

    made a mistake and cloned the master..
    recommitting the arrays and function conversion js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,32 +63,66 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+{
+	console.log("Challenge 1")
+	inventory.sort((a,b) => (a.id > b.id) ? 1 : ((b.id < a.id) ? -1 : 0));  //lets make sure the id is in order.
+	let mycar = 0;
+	console.log("Car " + mycar.id+ " is a " + mycar.car_year + " " + mycar.car_make + " " + mycar.car_model);
 
+    for (let i =0; i < inventory.length;i++){
+      if((inventory[i])["id"] === 33) {
+          mycar=inventory[i];
+		}
+	}
+	console.log("Car " + mycar.id+ " is a " + mycar.car_year + " " + mycar.car_make + " " + mycar.car_model);
+  // Is there a better way to search?
+  //Didn't want to trust that the data would always be equal to the id number (consider if they're are duplicates or skipped numbers).
+	
+}
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
-
+{
+	console.log("Challenge 2")
+	let mycar = (inventory.length)? inventory[0]: 0;
+      for (let i =0; i < inventory.length;i++){
+		if((inventory[i])["id"] > mycar.id) {
+          mycar=inventory[i];
+		}
+	}
+	console.log("Car " + mycar.id+ " is a " + mycar.car_year + " " + mycar.car_make + " " + mycar.car_model);
+	// "Last" is ambiguious..did you mean the car with the highest ID or the last one in the array (those things aren't equiv).
+  
+}
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+{
+	console.log("Challenge 3")
+	inventory.sort((a,b) =>  (a.car_model.localeCompare(b.car_model))); 
+	for (let i =0; i < inventory.length;i++){
+          mycar=inventory[i];
+			console.log("Car " + mycar.id+ " is a " + mycar.car_year + " " + mycar.car_make + " " + mycar.car_model);
+    }
+}
 let carModels = [];
 console.log();
-
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
-console.log();
-
+{
+	console.log("Challenge 4")
+	let carYears = [];
+	inventory.forEach((el, i) => (carYears.push(el.car_year)));
+	console.log(carYears.sort().filter((e,i,a) => i==a.indexOf(e)));
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = [];
-console.log(); 
-
+	console.log("Challenge 5");
+	const oldCarDef = 2000;
+	let oldCars = carYears.filter((e,i,a) => (e < oldCarDef));
+	console.log(oldCars.length);
+}
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
-console.log();
-
-
-
+console.log("Challenge 6")
+{
+	let BMWAndAudi = inventory.filter((e,i,a) => (e.car_make.toUpperCase() === "BMW" || e.car_make.toUpperCase() === "AUDI")?1:0);
+	console.log(JSON.stringify(BMWAndAudi));
+}
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,30 +1,16 @@
-// Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+let myFunction =()=> (console.log("Function was invoked!"));
+myFunction();
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+let anotherFunction = param =>(param);
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
-// anotherFunction("Example");
+anotherFunction("Example");
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
+let add = (param1, param2)=>(param1 + param2);
+add(1,2);
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+let subtract =(param1, param2) => (param1 - param2);
+ subtract(1,2);
 
-
-// Stretch
-
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num)=> num * 3);
+console.log(triple);
