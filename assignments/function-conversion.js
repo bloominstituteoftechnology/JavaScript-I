a32ffb21aec2298287c01d28a2abdf51d9fbce69
EoPlex <you@example.com>
2019-04-01 15:09:42 -0700

    working on arrays.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,15 +5,28 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log("Function was invoked!");
+}
+myFunction();
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => {
+    return param;
+};
+
+console.log(anotherFunction("Example"));
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
