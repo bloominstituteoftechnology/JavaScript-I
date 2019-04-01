043936da6cfaf3f160245e70a0bcd69116f5a4f5
EoPlex <you@example.com>
2019-04-01 16:06:09 -0700

    finished function conversions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -26,13 +26,21 @@ console.log(anotherFunction("Example"));
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => {
+    return param1 + param2;
+}
 
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) {
+    return param1 - param2;
+}
+console.log(subtract(1,2));
 
 // Stretch
 
