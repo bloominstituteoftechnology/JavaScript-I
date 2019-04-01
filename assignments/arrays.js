bdf814cd923f172a9b68c1771fca9ac5862585b2
EoPlex <you@example.com>
2019-04-01 16:24:05 -0700

    fixing challenge 1 on arrays

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,7 +63,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a cars33.car_year cars33.car_make cars33.car_model` );
+console.log(`car33 is a cars33.car_year cars33.car_make cars33.car_model` );
 
 let car33 = {};
 for (let i = 0; i < inventory.length; i++) {
@@ -72,8 +72,8 @@ for (let i = 0; i < inventory.length; i++) {
     }
 }
 
-console.log(car33.id)
 
+console.log(car33 + "is a" cars33.car_year + " " + cars33.car_make + " " + cars33.car_model`);
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
