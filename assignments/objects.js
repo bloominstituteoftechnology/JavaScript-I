7968dd6db1e786343d60cabaa1aa22f125c9b6fd
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-01 16:52:33 -0700

    Update objects.js
    interns
    
    Co-Authored-By: Raine Wallace <rainebeaudubs@users.noreply.github.com>

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,20 +18,67 @@ const example = {
 }
 
 // Write your intern objects here:
-
+var interns = {
+   Mitzi: {
+	id: 1,
+	email: "mmelloy0@psu.edu",
+	name: "Mitzi",
+	gender: "F"
+   },
+   Kennan: {
+   	id: 2,
+  	email: "kdiben1@tinypic.com",
+  	name: "Kennan",
+  	gender: "M",
+  	speak: function () {
+  		return "Hello, my name is " + this.name;
+  	}
+   },
+   Keven: {
+   	id: 3,
+   	email: "kmummery2@wikimedia.org",
+   	name: "Keven",
+   	gender: "M"
+   },
+   Gannie: {
+   	id: 4,
+   	email: "gmartinson3@illinois.edu",
+   	name: "Gannie",
+   	gender: "M"
+   },
+    Antonietta: {
+   	id: 5,
+   	email: "adaine5@samsung.com",
+   	name: "Antonietta",
+   	gender: "F",
+   	multiplyNums: function (x,y) {
+   		return x*y;
+   	}
+   }	
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+interns.Mitzi.name
+
 // Kennan's ID
+
+interns.Kennan.id
 
 // Keven's email
 
+interns.Keven.email
+
 // Gannie's name
 
+interns.Gannie.name
+
 // Antonietta's Gender
+
+interns.Antonietta.gender
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
