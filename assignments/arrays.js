69d7a2128f77c2a26386d9816b20fc9a0d2b93ce
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-01 17:58:23 -0700

    Update arrays.js
    sorting arrays
    
    Co-Authored-By: Raine Wallace <rainebeaudubs@users.noreply.github.com>

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,7 +73,11 @@ console.log("The last car in the inventory is a " + lastCar.car_year + " " + las
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+
+for (var i = 0; i < inventory.length; i++) {
+	carModels.unshift(inventory[i].car_model);
+}
+console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
