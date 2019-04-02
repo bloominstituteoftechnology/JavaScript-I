6eb6351c6fc37ea39a8eb1d88abe41d7a84690f8
Vladimir Canro <vcanro@gmail.com>
2019-04-01 19:28:56 -0600

    finish the function convertion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,23 +4,30 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => console.log("Function was invoked!");
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => param;
+
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2; 
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+let subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
 // Stretch
 
 // exampleArray = [1,2,3,4];
@@ -28,3 +35,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);e
