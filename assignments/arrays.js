c8159b5b1d83195d2eeb51b246ef2d6d07acbb1e
Carlil Jean <jaedagk@live.com>
2019-04-01 19:30:28 -0700

    Final.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,34 +63,90 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
-
-
+for(i = 0; i < carYears.length; i++){
+    var item = inventory[i];
+    var keys = object.keys(item);
+    var values = object.values(item);
+    for(v = 0; v < keys.length; v++){
+        var key = keys[v];
+        var value = values[v];
+        if(key == 'id'){
+            if(value == 33){
+                console.log('Car 33 is a ' item.car_year + ' ' + item.car_make + ' ' + item.car_model);
+            }
+        }
+    }
+}
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory[inventory.length - 1];
+console.log(lastCar.car_make + lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+for(i = 0; i < carModels.length; i++){
+    var item = inventory[i];
+    var keys = object.keys(item);
+    var values = object.values(item);
+    for(b = 0; b < keys.length; b++){
+        var key = keys[b];
+        var value = values[b];
+        if(key == 'car_model'){
+            carModels.push(value.toUpperCase());
+            carModels.sort();
+        }
+    }
+};
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for(i = 0; i < carYears.length; i++){
+    var item = inventory[i];
+    var keys = object.keys(item);
+    var values = object.values(item);
+    for(a = 0; a < keys.length; a++){
+        var key = keys[a];
+        var value = values[a];
+        if(key == 'car_year'){
+            carYears.push(value);
+            carYears.sort();
+        }
+    }
+}
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars =[];
-console.log(); 
-
+let oldCars = [];
+for(i = 0; i < carYears.length; i++){
+    var years = carYears[i];
+    if (years < 2000){
+        oldCars.push(years);
+    }
+}
+console.log('There are' + oldCars.length + 'cars older than 2000');
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-console.log();
+for(i = 0; i < carYears.length; i++){
+    var item = inventory[i];
+    var keys = object.keys(item);
+    var values = object.values(item);
+    for(e = 0; e < keys.length; e++){
+        var key = keys[e];
+        var value = values[e];
+        if(key == 'car_make'){
+            if(value == 'BMW' || value == 'Audi'){
+                BMWAndAudi.push(item);
+            }
+        }
+    }
+}
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -18,28 +18,77 @@ const example = {
 }
 
 // Write your intern objects here:
+const mitzi = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F",
+  "speak": function(){
+    return "Hello, my name is " + this.name + "!";
+  }
+}
 
+const kennan = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+  "speak": function(){
+    return "Hello, my name is " + this.name + "!";
+  }
+}
 
+const keven = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const gannie = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M",
+  "speak": function(){
+    return "Hello, my name is " + this.name + "!";
+  }
+}
+
+const Antonietta = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F",
+  "speak": function(){
+    return "Hello, my name is " + this.name + "!";
+  },
+  "multiplyNums": function(x, y){
+    var x = num1;
+    var y = num2;
+    return num1 * num2;
+  }
+}
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(mitzi.name);
 // Kennan's ID
-
+console.log(kennan.id);
 // Keven's email
-
+console.log(keven.email);
 // Gannie's name
-
+console.log(gannie.name);
 // Antonietta's Gender
-
+console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(kennan.speak("Hello, my name is " + kennan.name + "!"));
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(Antonietta.multiplyNums(3, 4));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
