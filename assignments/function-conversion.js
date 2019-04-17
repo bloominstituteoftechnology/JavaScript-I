ff8a881f1312982fa40cfaed6aeb30fe25af7ad3
Nathan Griffith <nathangriffith@Nathans-MacBook-Pro.local>
2019-04-17 12:26:24 -0400

    finished the project up

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,33 @@
 // };
 // myFunction();
 
+let myFunction = () => {console.log('Function was invoked!!!')}
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = param => param;
+console.log(anotherFunction("paramater returned"))
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (num1, num2) => num1 + num2;
+console.log(add(1,2))
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (num1, num2) => num1 - num2;
+
+console.log(subtract(1,2))
 
 
 // Stretch
