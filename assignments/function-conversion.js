d09f64bb40d193ae2662c4d8adf94f74d8289833
Jordan Lanier <jordan.6195@yahoo.com>
2019-04-17 21:32:16 -0400

    array and funstions done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,40 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const myFunction = () => {
+	console.log('Function was invoked!');
+};
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+const anotherFunction = (param) => {
+	return param;
+};
+anotherFunction('Example');
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+const add = (param1, param2) => {
+	return param1 + param2;
+};
+add(1, 2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (param1, param2) => {
+	return param1 - param2;
+};
+subtract(1, 2);
 
 // Stretch
 
