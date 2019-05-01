8b156a00e6efa28aab44409ea491133b77a92568
Derek Jones <derek@derekjones.io>
2019-05-01 14:23:04 -0400

    Complete Objects.js MVP

--- assignments/objects.js
+++ assignments/objects.js
@@ -30,6 +30,9 @@ const kennan = {
   name: 'Kennan',
   email: 'kdiben1@tinypic.com',
   gender: 'M',
+  speak: function() {
+    return `Hello, my name is ${this.name}!`;
+  },
 };
 
 const keven = {
@@ -51,6 +54,9 @@ const antonietta = {
   name: 'Antonietta',
   email: 'adaine5@samsung.com',
   gender: 'F',
+  multiplyNums: function(num1, num2) {
+    return num1 * num2;
+  },
 };
 
 // ==== Challenge 2: Reading Object Data ====
@@ -73,10 +79,10 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
