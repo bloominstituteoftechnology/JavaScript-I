a6a1acb7d72b73bf27b99b7322954b1291bae586
Jashele T <jashelet@Jashele.domain>
2019-05-06 13:38:45 -0700

    Created object for each new intern on objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,40 @@ const example = {
 
 // Write your intern objects here:
 
+const newInterns = {  
+  id: '1',
+  email: 'mmelloy0@psu.edu',
+  firstName: 'Mitzi',
+  gender: 'F',
+
+  id: '2',
+  email: 'kdiben1@tinypic.com',
+  firstName: 'Kennan',
+  gender: 'M',
+
+  id: '3',
+  email: 'kmummery2@wikimedia.org',
+  firstName: 'Keven',
+  gender: 'M',
+
+  id: '4',
+  email: 'gmartinson3@illinois.edu',
+  firstName: 'Gannie',
+  gender: 'M',
+
+  id: '5',
+  email: 'adaine5@samsung.com',
+  firstName: 'Antonietta',
+  gender: 'F'
+
+  };
+
+
+ 
+
+
+
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
