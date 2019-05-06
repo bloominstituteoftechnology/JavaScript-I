b2ebe590cdd7b6ef76bed309ac27776a5317c3dc
bipolarbearstrikesback <49734722+bipolarbearstrikesback@users.noreply.github.com>
2019-05-06 17:39:09 -0500

    Update objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,7 +10,7 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
+let example = {
   "id": 0,
   "name": "Example",
   "email": "examples@you.edu",
@@ -18,26 +18,58 @@ const example = {
 }
 
 // Write your intern objects here:
-const Mitzi{
-  "id":1,
-    
 
+const interns = [
+  {"id": 1 , "name": "Mitzi", "email":"mmelloy0@psu.edu", "gender": "F"},
+  {"id": 2, "name": "Kennan", "email":"kdiben1@tinypic.com", "gender": "M"},
+  {"id": 3, "name": "Keven", "email":"kmummery2@wikimedia.org", "gender": "M"},
+  {"id": 4, "name": "Gannie", "email":"gmartinson3@illinois.edu", "gender": "M"},
+  {"id": 5, "name": "Antonietta", "email":"adaine5@samsung.com", "gender": "F"}
+]
+
+const mitzi = interns[0];
+const kennan = interns[1];
+const keven = interns[2];
+const gannie = interns[3];
+const antonietta = interns[4];
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+console.log(mitzi.name);
+
 // Kennan's ID
+
+console.log(kennan.id);
 
 // Keven's email
 
+console.log(keven.email);
+
 // Gannie's name
 
+console.log(gannie.name);
+
 // Antonietta's Gender
+ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+kennan.speak = function(){
+  return "Hello, my name is Kennan!";
+}
+console.log(kennan.speak());
+
+// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+//
+
+antonietta.multiplyNums = function(num1,num2){
+    return num1 * num2;
+}
+console.log(antonietta.multiplyNums(3,4));
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
