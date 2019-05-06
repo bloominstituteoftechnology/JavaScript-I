a6a30f43201f1b67c8dd91ecce261e22fcbb2e2d
Andrew Dillon <majik895@gmail.com>
2019-05-06 16:54:23 -0400

    stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -39,3 +39,7 @@ console.log(substract(1,2));
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num*3);
+console.log(triple)
