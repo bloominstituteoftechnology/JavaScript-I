6fd948dca961095fc847f81abd96b9e1b02e754f
Vaeliwen <zaman@imbriaco.com>
2019-05-06 17:17:46 -0400

    Finished object challenges & Array challenges.

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,10 @@ const kennan = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
+  speak: function() {
+    return "Hello, my name is " + this.name + "!"
+  }
 }
 
 const keven = {
@@ -50,7 +53,10 @@ const antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
+  "gender": "F",
+  multiplyNums: function(a, b) {
+    return a * b
+  }
 }
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
@@ -83,8 +89,12 @@ console.log("Antonietta's gender:", fetch(antonietta["gender"]))
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log("Kennan's introduction:", kennan.speak())
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log("Antonietta's multiplication:", antonietta.multiplyNums(3, 4))
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
