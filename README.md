788e77eef7bc263e27203f9880efcceedcdb4793
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-06 13:33:06 +0100

    convert functions from ES5 to ES6

--- README.md
+++ README.md
@@ -15,9 +15,9 @@
 
 **Follow these steps for completing your project.**
 
-* [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
-* [ ] Add your project manager as a reviewer on the pull-request
-* [ ] Your project manager will count the project as complete by merging the branch back into master.
+* [x] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
+* [x] Add your project manager as a reviewer on the pull-request
+* [x] Your project manager will count the project as complete by merging the branch back into master.
 
 ## Assignment Description
 
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,23 +4,28 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+let myFunction = () => console.log('Function was invoked!')
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction =  (param) => param;
+anotherFunction('example')
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) => param1 + param2;
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+const subtract = (param1 , param2) => param1 - param2;
+subtract(1 ,2)
 // Stretch
 
 // exampleArray = [1,2,3,4];
@@ -28,3 +33,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+const exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num*3)
+console.log(triple);
