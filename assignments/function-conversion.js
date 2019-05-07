c1502402fc2827b911efb2b9ae700e9eac38f265
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-07 13:57:22 +0100

    function conversions assignment finshed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,27 +4,28 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+let myFunction = () => console.log("Function was invoked!");
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+let anotherFunction = (param) => param;
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+let add = (a,b) => a + b;
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+let subtract = (a,b) => a-b;
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = exampleArray.map(num => num * 3);
