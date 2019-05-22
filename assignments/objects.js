a372b6a745ed51fe49a9183b96a5301007d8c7ae
Asha <ashalbenjamin@gmail.com>
2019-05-22 07:55:07 -0500

    added-intern-objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,41 @@ const example = {
 
 // Write your intern objects here:
 
+const intern1 = {
+	"id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+
+const intern2 = {
+	"id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+
+const intern3 = {
+	"id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const intern4 = {
+	"id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+const intern5 = {
+	"id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
