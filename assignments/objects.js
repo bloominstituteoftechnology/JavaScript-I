e582c79474e05c7330889fa7059d4f79097f2167
Asha <ashalbenjamin@gmail.com>
2019-05-22 08:09:39 -0500

    kennan-speaks

--- assignments/objects.js
+++ assignments/objects.js
@@ -78,6 +78,13 @@ intern5.gender;
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+let kennan = {};
+	kennan.speak = function(line) {
+		console.log(`Hello, my name is '${line}' `);
+	};
+
+	kennan.speak("Kennan!");
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
