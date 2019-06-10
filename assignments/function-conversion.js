863a99991ecdf47ae1f6005235fa34e910a70b07
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-10 17:13:27 -0600

    MVP

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,45 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log('Function was invoked!')
+}
+myFunction();
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+// console.log(anotherFunction("Example"));
+
+
+
+let anotherFunction = param => {
+    return param;
+}
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+};
+
+console.log(add(1,2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => {
+    return param1 - param2;
+}
+
+console.log(subtract(1,2))
 
 // Stretch
 
