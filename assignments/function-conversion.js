32069f761e50833f704eb9f4b5fd2572bae7cf3d
Brandon Allison <brandonallison@pop-os.localdomain>
2019-06-10 17:27:31 -0400

    finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,47 @@
 // };
 // myFunction();
 
+const myFunction = () => "Function was invoked!" ;
+console.log (myFunction());
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (param) => param;
+console.log (anotherFunction("Example"));
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param2) =>  param1 + param2;
+console.log (add(1,2));
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (param1, param2) => param1 - param2;
+console.log (subtract(1,2));
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
+ //exampleArray = [1,2,3,4];
+//const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+
+var exampleArray = [1,2,3,4];
+
+const triple = exampleArray.map (num => {return num * 3});
+
+console.log (triple);
