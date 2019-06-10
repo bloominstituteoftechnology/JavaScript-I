2a2b48310d6bf0abfa6614603cc05f6e8f227d2a
Armando Roman <armandoroman1016@gmail.com>
2019-06-10 13:43:03 -0700

    mvp

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,26 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+let myFunction = () => console.log('Function was invoked!');
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+let anotherFunction = (param) => param;
+console.log(anotherFunction('Hello!'));
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+let add = (param1, param2) => param1 + param2;
+console.log(add(2084,487));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(10, 2));
 
 // Stretch
 
@@ -28,3 +32,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+numArray=[1,2,3,4,5,6,7]
+const triple = (arr) => arr.map(num => num * 3);
+console.log(triple(numArray));
