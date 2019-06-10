fc127b09b958ddf92ee0e0eee8e717bc63428b54
Eleasah Halsmer <eleasahjh@gmail.com>
2019-06-10 15:32:46 -0600

    function-conversion.js complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -35,9 +35,11 @@ console.log(subtract(1,2));
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
 
+const triple = exampleArray.map((num) => num * 3);
+console.log(triple);
