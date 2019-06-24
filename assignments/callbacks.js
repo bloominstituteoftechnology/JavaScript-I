2b4152eddf73cede214da70a86de578ad3624f41
DasGMA <dasgrigoma@gmail.com>
2019-06-23 19:16:36 -0700

    Few changes

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,54 +1,45 @@
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Gum'];
 
 function firstItem(arr, cb) {
   // firstItem passes the first item of the given array to the callback function.
   cb(arr[0]);
-
-
-
 }
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-
-cb(arr.length);
-
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-
   cb(arr(arr.length - 1));
-
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-
-  cb(x+y);
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-cb(x * y);
-
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 
-  const IsInArray = () => {
-
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === item) {
-        return true;
-      }
-    } return false;
-  };
-  cb(isInArray());
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      cb(true);
+    }
+  } 
+  cb(false);
 };
-  
+
+contains('Gum', items, (inArray) => {
+  console.log(inArray);
+})
 
 
 
@@ -58,10 +49,14 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const seen = {};
 
- const seen = {};
- for (let i = 0; i < array.length; i++){
-   seen[array[i]] = true;
-   cb(Object.keys(seen));
- }
+  for (let i = 0; i < array.length; i++){
+    seen[array[i]] = true;
+    cb(Object.keys(seen));
+  }
 }
+
+removeDuplicates(items, (duplicatesRemoved) => {
+  console.log(duplicatesRemoved);
+})
--- assignments/objects.js
+++ assignments/objects.js
@@ -10,79 +10,76 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-let example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+const example = {
+  id: 0,
+  name: 'Example',
+  email: 'examples@you.edu',
+  gender: 'F'
 }
 
 // Write your intern objects here:
-let mitzi = {
-  "id": 1,
-  "name": "Mitzi",
-  "email": "mmelloy0@psu.edu",
-  "gender": "F"
+const mitzi = {
+  id: 1,
+  name: 'Mitzi',
+  email: 'mmelloy0@psu.edu',
+  gender: 'F'
 }
 
-let kennan = {
-  "id": 2,
-  "name": "Kennan",
-  "email": "kdiben1@tinypic.com",
-  "gender": "M",
-  
+const kennan = {
+  id: 2,
+  name: 'Kennan',
+  emai: 'kdiben1@tinypic.com',
+  gender: 'M',
+  speak: function() {
+    return 'Hello, my name is Kennan!';
+  }
 }
 
-kennan.speak=function(){
-  return "Hello, my name is Kennan!";
+const keven = {
+  id: 3,
+  name: 'Keven',
+  email: 'kmummery2@wikimedia.org',
+  gender: 'M',
+  speak: function() {
+    return 'Hello, my name is Keren!';
+  }
 }
 
-let keven = {
-  "id": 3,
-  "name": "Keven",
-  "email": "kmummery2@wikimedia.org",
-  "gender": "M"
-}
-keven.speak=function(){
-  return "Hello, my name is Keren!";
+const gannie = {
+  id: 4,
+  name: 'Gannie',
+  email: 'gmartinson3@illinois.edu',
+  gender: 'F'
 }
 
-let gannie = {
-  "id": 4,
-  "name": "Gannie",
-  "email": "gmartinson3@illinois.edu",
-  "gender": "F"
-}
-
-let antonietta = {
-  "id": 5,
-  "name": "Antonietta",
-  "email": "adaine5@samsung.com",
-  "gender": "F"
-}
-
-antonietta.multiplyNums = function(a, b){
-  return a * b;
+const antonietta = {
+  id: 5,
+  name: 'Antonietta',
+  email: 'adaine5@samsung.com',
+  gender: 'F',
+  multiplyNums: function(a, b) {
+    return a * b;
+  }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(mitzi["name"]);
+console.log(mitzi.name);
 // Kennan's ID
-console.log(kennan["id"]);
+console.log(kennan.id);
 // Keven's email
-console.log(keven["email"]);
+console.log(keven.email);
 // Gannie's name
-console.log(gannie["name"]);
+console.log(gannie.name);
 // Antonietta's Gender
-console.log(antonietta["gender"]);
+console.log(antonietta.gender);
 
 
 
 // ==== Challenge 3: Object Methods ==== 
-// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+// Give Kennan the ability to say 'Hello, my name is Kennan!' Use the console.log provided as a hint.
  console.log(kennan.speak());
 
 
@@ -104,30 +101,28 @@ console.log(antonietta.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {
-  "name": "Susan",
-  "age": 70,
-  "speak": function(){
-    return "My name " + this.name;
+const parent = {
+  'name': 'Susan',
+  'age': 70,
+  'speak': function() {
+    return 'My name ' + this.name;
   },
   
     child: {
-    "name": "George",
-    "age": 50,
-    "speak": function(){
-      return "My name " + this.name;
+    'name': 'George',
+    'age': 50,
+    'speak': function() {
+      return 'My name ' + this.name;
     },
 
       grandchild: {
-          "name": "Sam",
-          "age": 30,
-          "speak": function(){
-            return "My name " + this.name;
+          'name': 'Sam',
+          'age': 30,
+          'speak': function() {
+            return 'My name ' + this.name;
           },
         }
-
       }
-
 }
 
 
