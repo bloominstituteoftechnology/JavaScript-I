f5aba6dc8015c051c6cecc769c10230809c306d4
adamcglee <adamcglee@gmail.com>
2019-07-02 15:43:51 -0700

    finished object assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -50,34 +50,34 @@ let antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F",
-  "multiplyNums": function(x, y) {
-    return x * y;
-  }
+  "gender": "F"
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(mitzi.name);
+  console.log(mitzi.name);
 // Kennan's ID
-console.log(kennan.id);
+  console.log(kennan.id);
 // Keven's email
-console.log(keven.email);
+  console.log(keven.email);
 // Gannie's name
-console.log(gannie.name);
+  console.log(gannie.name);
 // Antonietta's Gender
-console.log(antonietta.gender);
+  console.log(antonietta.gender);
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-kennan.speak = 'Hello, my name is ' + kennan.name + '!';
+kennan.speak = "Hello, my name is " + kennan.name + "!";
 console.log(kennan.speak);
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-
-console.log(antonietta.multiplyNums(3,4));
+antonietta.multiply = function(x, y) {
+ return x * y;
+}
+console.log(antonietta.multiply(x, y));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -88,7 +88,6 @@ console.log(antonietta.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-let parent = {}
 
 // Log the parent object's name
 
