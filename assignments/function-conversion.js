79259190751f0de584bc4efcd01226bac0b2b46f
Tyler Broadway <tbroadway96@gmail.com>
2019-07-14 09:22:46 -0500

    sprint review; function-conversion.js complete + stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,40 @@
 // };
 // myFunction();
 
+const myFunction = () => console.log('Function was invoked!');
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (param) => console.log(param);
+anotherFunction('Example');
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+ exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
