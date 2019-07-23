985223f6f87b9c1236a81877138c681a2331210c
Bangstry <tylerwesleyquinn@gmail.com>
2019-07-22 22:52:09 -0400

    first commit

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,66 @@ const example = {
 }
 
 // Write your intern objects here:
+const a = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@PushSubscription.edu",
+  gender: "F"
+};
 
+const b = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M"
+};
 
+const c = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+};
+
+const d = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
+}
+
+const e ={
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F"
+};
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(a.name);
 // Kennan's ID
-
+console.log(b.id);
 // Keven's email
-
+console.log(c.email);
 // Gannie's name
-
+console.log(d.name);
 // Antonietta's Gender
-
+console.log(e.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+b.speak = function () {
+  return "Hello, my name is Kennan!"
+};
+console.log(b.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+e.multiplyNums = function(num1,num2) {
+  return num1 * num2;
+};
+console.log(e.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,16 +88,38 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: "Susan",
+  age: 70,
+  speak: function() {
+    return this.name;
+  },
+  child: {
+    name: "George",
+    age: 50,
+    speak: function() {
+      return this.name;
+    },
+    grandchild: {
+      name: "Sam",
+      age: 30,
+      speak: function() {
+        return this.name;
+      }
+    }
+  }
+};
+
 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name,parent.child.grandchild.name);
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
