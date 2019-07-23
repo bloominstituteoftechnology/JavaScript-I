5f5db1d0f805217c1b13a6706fd66d2c647c077b
Crey <crey@creys-mac-mini.lan>
2019-07-23 16:47:19 -0500

    commit

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,11 +3,11 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-1,mmelloy0@psu.edu,Mitzi,F
-2,kdiben1@tinypic.com,Kennan,M
-3,kmummery2@wikimedia.org,Keven,M
-4,gmartinson3@illinois.edu,Gannie,M
-5,adaine5@samsung.com,Antonietta,F
+// 1,mmelloy0@psu.edu,Mitzi,F
+// 2,kdiben1@tinypic.com,Kennan,M
+// 3,kmummery2@wikimedia.org,Keven,M
+// 4,gmartinson3@illinois.edu,Gannie,M
+// 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
