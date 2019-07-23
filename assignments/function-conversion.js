59dcc61abd1b244f12ebbfd198a19631ac304e51
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-07-22 20:03:24 -0700

    completed arrays js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,28 @@
 // };
 // myFunction();
 
+let myFunction = () => {};
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1,param2) => param1 + param2;
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (param1,param2) => param1 - param2;
 
 
 // Stretch
