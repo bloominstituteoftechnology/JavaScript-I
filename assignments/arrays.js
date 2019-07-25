dfc5b71bf0d91c21238dafdd8d7059409babfd23
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-07-24 22:11:12 -0700

    Update arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -105,8 +105,8 @@ let BMWAndAudi =[];
 for (let i=0; i<inventory.length; i++) {
     if ((inventory[i]["car_make"]==="Audi") || (inventory[i]["car_make"]==="BMW")) {
         BMWAndAudi.push(inventory[i]);
- };
-};
+ }
+}
 console.log(BMWAndAudi);
 console.log(JSON.stringify(BMWAndAudi));
 
