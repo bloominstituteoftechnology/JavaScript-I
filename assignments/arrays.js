40bf574beccc247fd1a6aa93e33e09706c84c199
David <davidsebesta@outlook.com>
2019-08-04 20:38:23 -0700

    Finish project and stretch goals

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -2,7 +2,8 @@
 
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
 
-let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
+let inventory = [
+{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},
 {"id":3,"car_make":"Land Rover","car_model":"Defender Ice Edition","car_year":2010},
 {"id":4,"car_make":"Honda","car_model":"Accord","car_year":1983},
@@ -51,7 +52,8 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":47,"car_make":"Volkswagen","car_model":"Jetta","car_year":2007},
 {"id":48,"car_make":"Dodge","car_model":"Magnum","car_year":2008},
 {"id":49,"car_make":"Chrysler","car_model":"Sebring","car_year":1996},
-{"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
+{"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}
+];
 
 
 // Example for loop:
@@ -63,32 +65,42 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+
+console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`);
+
+// refactored
+// const { car_year: car33Yr, car_make: car33Make, car_model: car33Model } = inventory[32];
+// console.log(`Car 33 is a ${car33Yr} ${car33Make} ${car33Model}`);
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory[inventory.length -1];
+
+console.log(`Make: ${lastCar.car_make} Model: ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-console.log();
+let carModels = inventory.map((car) => car.car_model).sort();
+
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
-console.log();
+let carYears = inventory.map((car) => car.car_year);
+
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = [];
-console.log(); 
+let oldCars = carYears.filter((year) => year < 2000);
+
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
-console.log();
+let BMWAndAudi = inventory.filter((car) => car.car_make === 'BMW' || car.car_make === 'Audi');
+
+console.log(JSON.stringify(BMWAndAudi, undefined, 2));
 
 
 
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -20,6 +20,23 @@
 // };
 // subtract(1,2);
 
+const myFunction = () => {
+  console.log("Function was invoked!");
+}
+
+myFunction();
+
+const anotherFunction = (param) => param;
+
+anotherFunction('Example');
+
+const add = (param1, param2) => param1 + param2;
+
+add(1, 2);
+
+const subtract = (param1, param2) => param1 - param2;
+
+subtract(1, 2);
 
 // Stretch
 
@@ -28,3 +45,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+const exampleArray = [1, 2, 3, 4];
+
+const triple = exampleArray.map((num) => num * 3);
+
+console.log(triple);
