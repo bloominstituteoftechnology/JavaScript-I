05bd24002cd0e518bd0041f4b1e53fbc0b8782cb
anatulea <anatulea14@.gmali.com>
2019-08-07 23:05:51 -0500

    finished the project

--- assignments/objects.js
+++ assignments/objects.js
@@ -97,16 +97,44 @@ console.log(person5.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+
+const parent = {
+  "name": "Susan",
+  "age": 70,
+  "speak": function () {
+    console.log(`Hi, my name is ${this.name}`)
+  },
+  "child": {
+    "name": "George",
+    "age": 50,
+    "speak": function () {
+      console.log(`Hello, my name is ${this.name}`)
+    },
+    "grandChild": {
+      "name": "Sam",
+      "age": 30,
+      "speak": function () {
+        console.log(`Hola, my name is ${this.name}`)
+      },
+    }
+  }
+}
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
 
-// Have the parent speak
+console.log(parent.child.grandChild.age);
+
+// Have the parent speak  
+console.log(parent.speak());
 
 // Have the child speak
+console.log(parent.child.speak());
 
 // Have the grandchild speak
+console.log(parent.child.grandChild.speak());
+
