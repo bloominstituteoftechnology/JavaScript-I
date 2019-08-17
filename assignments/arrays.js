208b99b7129d7fa6c104a69980d62f68d987f959
barbarof <barbarofreyes@gmail.com>
2019-08-17 13:48:52 -0500

    add  information

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -68,7 +68,7 @@ console.log(`Car 33 is a 2011 ,Jeep ,Wrangler` );
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-console.log(inventory[49]);
+console.log(inventory.length -1);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
