190002810718e1fc346150eefc6fec70f2b3e714
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-18 11:15:24 +0200

    tweak the object challenges instructions

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,18 +3,18 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1,mmelloy0@psu.edu,Mitzi,F
-// 2,kdiben1@tinypic.com,Kennan,M
-// 3,kmummery2@wikimedia.org,Keven,M
-// 4,gmartinson3@illinois.edu,Gannie,M
-// 5,adaine5@samsung.com,Antonietta,F
+// 1, mmelloy0@psu.edu, Mitzi, F
+// 2, kdiben1@tinypic.com, Kennan, M
+// 3, kmummery2@wikimedia.org, Keven, M
+// 4, gmartinson3@illinois.edu, Gannie, M
+// 5, adaine5@samsung.com, Antonietta, F
 
-// Example format of an intern object: 1,examples@you.edu,Example,F
+// Example format of an intern object: 1, examples@you.edu, Example, F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
+  id: 0,
+  name: "Example",
+  email: "examples@you.edu",
+  gender: "F",
 }
 
 // Write your intern objects here:
@@ -40,7 +40,7 @@ const example = {
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
-// === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
+// === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
