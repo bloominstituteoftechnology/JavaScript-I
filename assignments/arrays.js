65d9c6eccd8c28dab29ea1cf1cd98f2dfc13dea3
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-18 11:36:14 +0200

    remove whitespace

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -98,6 +98,6 @@ let oldCars = [];
 console.log();
 
 // ==== Challenge 6 ====
-// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
 console.log();
