d5d5a4a85b2356da3337d9b13fee6001f46feaa4
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-18 11:39:45 +0200

    provide extra hint

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -84,6 +84,7 @@ console.log();
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+let carModels = [];
 let carModelsSorted = [];
 console.log();
 
