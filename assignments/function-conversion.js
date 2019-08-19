d4e7ac66d57e4a4ebc16306fddc4b91d9123bc90
Daniel <danielkim.s@outlook.com>
2019-08-19 13:11:28 -0700

    Finished function-conversion assignment

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,37 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => {
+    return param;
+};
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => {
+    return param1 + param2;
+};
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+subtract(1,2);
 
 
 // Stretch
@@ -28,3 +44,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => {
+    return num * 3;
+});
+console.log(triple);
+
