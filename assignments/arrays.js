4165f346fdb8747d501e50c75de779d22f899df7
meera-andersen <meera.andersen@gmail.com>
2019-08-19 14:48:24 -0600

    completed first 3 challenges in arrays file

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,19 +74,49 @@ let inventory = [
 // [12, 14]
 
 // ==== Challenge 1 ====
-// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*`);
+// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out 
+//which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+
+console.log("Car 33 is a " +inventory[32].car_year + ", " + inventory[32].car_make + ", " + inventory[32].car_model + ".");
+
 
 // ==== Challenge 2 ====
-// The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+// The dealer needs the information on the last car in their inventory.  What is the make and model of the 
+//last car in the inventory?  Log the make and model into the console.
+
+
+// function lastCar(arr) {
+//   let carId = arr.length-1;
+
+//   let carInfo = arr.carID.car_make;
+
+//   return carInfo.car_make;
+// }
+// console.log(lastCar(inventory));
+
+
+function lastCar(arr) {
+  let car = arr.pop();
+  return car.car_make + ", " + car.car_model;
+}
+console.log(lastCar(inventory));
+
+
 
 // ==== Challenge 3 ====
-// The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+// The marketing team wants the car models listed alphabetically on the website. Sort all the car model names 
+//into alphabetical order and log the results in the console
+
+// take each model, and push it into the new array after sorting alphabetically
 let carModels = [];
 let carModelsSorted = [];
-console.log();
+
+for (let i = 0; i < inventory.length; i++){
+  let model = inventory[i].car_model;
+  carModelsSorted.push(model);
+  carModelsSorted.sort();
+  }
+console.log(carModelsSorted);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
