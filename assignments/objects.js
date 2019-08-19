b3549ef2d67588baf7bd902ee2aa29601b203235
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-08-19 17:07:31 -0400

    did challenges for object.js array.js and function-conversion.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -14,32 +14,68 @@ const example = {
   id: 0,
   name: "Example",
   email: "examples@you.edu",
-  gender: "F",
+  gender: "F"
 }
 
 // Write your intern objects here:
-
+const user1 = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+}
+const user2 = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function(){
+    console.log("Hi, My name is Kennan!");
+  }
+}
+const user3 = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+}
+const user4 = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
+}
+const user5 = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+  multiply: function(num1, num2){
+    return num1*num2;
+  }
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(user1.name);
 // Kennan's ID
-
+console.log(user2.id);
 // Keven's email
-
+console.log(user3.email);
 // Gannie's name
-
+console.log(user4.name);
 // Antonietta's Gender
-
+console.log(user5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(user2.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(user5.multiply(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
