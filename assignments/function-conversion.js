5ef9948e9377bb4e60de3428be362720d81d8db1
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-08-19 11:31:34 +0100

    Completed function conversion exercises

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,28 @@
 // };
 // myFunction();
 
+let myFunction = () => console.log("Function was invoked!");
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2;
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1 - param2;
 
 // Stretch
 
@@ -28,3 +35,5 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+let triple = exampleArray.map(num => num * 3);
