a06888b4a6130811bb5fb43a21d759dfcccd61ce
Henry Souder-Arguedas <henryarguedas@gmail.com>
2019-08-19 18:47:36 -0400

    one array complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -81,8 +81,10 @@ console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_model} ${
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+// let lastCar = 0; 
+
+console.log(inventory[inventory.length -1].car_make);
+console.log(inventory[inventory.length -1].car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
