cf8f47c4c815dc5a593515293d970225a5843c11
karapeoples <52808542+karapeoples@users.noreply.github.com>
2019-08-20 09:38:52 -0500

    2nd Commit

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,21 +1,21 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
+// let myFunction = function () => {
 // console.log("Function was invoked!");
 // };
 // myFunction();
 
-// let anotherFunction = function (param) {
+// let anotherFunction = function (param) => {
 //   return param;
 // };
 // anotherFunction("Example");
 
-// let add = function (param1, param2) {
+// let add = function (param1, param2) => {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-// let subtract = function (param1, param2) {
+// let subtract = function (param1, param2) => {
 //   return param1 - param2;
 // };
 // subtract(1,2);
@@ -24,7 +24,7 @@
 // Stretch
 
 // exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
+// const triple = exampleArray.map(function (num) => {
 //   return num * 3;
 // });
 // console.log(triple);
--- assignments/objects.js
+++ assignments/objects.js
@@ -81,11 +81,19 @@ console.log(internFive.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-
+function speech (name) {
+  return "Hello, my name is "+ name + "!";
+}
+console.log(speech("Keenan"));
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
+function multiplyTwoNumbers(num1, num2){
+  return num1 * num2;
+}
+
+console.log(multiplyTwoNumbers(3,4));
 
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
