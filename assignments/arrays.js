a6ea73443ea040082799f85043d7f27413be3316
Henry Souder-Arguedas <henryarguedas@gmail.com>
2019-08-20 15:52:56 -0400

    initial 2

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -90,12 +90,15 @@ console.log(inventory[inventory.length -1].car_model);
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 let carModelsSorted = [];
-console.log();
+// console.log(inventory.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+  
+}
+// console.log(inventory.map.car_year);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
