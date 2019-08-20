3145acb037c456567bb519f71b9b38aa5a6cabb5
Nicole Adams <NiceAdams@Nicoles-iMac.local>
2019-08-20 15:57:05 -0400

    completed Objects, Arrays, Arrow Function Syntax

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,7 +1,7 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
 // ==== Challenge 1: Writing Objects ==== 
-// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
+// HR needs some information on the new interns to put into a database.  Given an id, email, first name, and gender, create an object for each person in the company list:
 
 // 1, mmelloy0@psu.edu, Mitzi, F
 // 2, kdiben1@tinypic.com, Kennan, M
@@ -16,31 +16,83 @@ const example = {
   email: "examples@you.edu",
   gender: "F",
 }
+console.log(example);
 
 // Write your intern objects here:
 
+const Mitzi = {
+  id: 1,
+  firstName: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F",
+}
+console.log(Mitzi);
+
+const Kennan = {
+  id:2,
+  firstName:"Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function () {
+    return "Hello, my name is " + this.firstName;
+  }
+}
+console.log(Kennan);
+
+const Keven = {
+  id: 3,
+  firstName: "Keven",
+  email: 'kmummery2@wikimedia.org',
+  gender: "M",
+}
+console.log(Keven);
+
+const Gannie = {
+  id: 4,
+  firstName: "Gannie",
+  email: 'gmartinson3@illinois.edu',
+  gender: "F",
+}
+console.log(Gannie);
+
+const Antonietta = {
+  id: 5,
+  firstName: 'Antonietta',
+  email: 'adaine5@samsung.com',
+  gender: "F",
+  multiplies: function(num1,num2){
+    return num1*num2;
+  } 
+}
+console.log(Antonietta);
 
 // ==== Challenge 2: Reading Object Data ==== 
+
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.firstName);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Keven.email);
 // Gannie's name
-
+console.log(Gannie.firstName);
 // Antonietta's Gender
+console.log(Antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(Kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
+console.log(Antonietta.multiplies(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
+
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
