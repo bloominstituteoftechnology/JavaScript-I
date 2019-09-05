f736493f93bbc09160bb25dafa8710e7124f256c
Samuel Kraus <S_read@aol.com>
2019-09-04 23:07:15 -0500

    Update arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -89,11 +89,17 @@ console.log('The last car model is', inventory[lastCar].car_model, 'and the make
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+
+
 let carModels = [];
 let carModelsSorted = [];
 
+for (let i = 0; i<inventory.length; i++){
+  // console.log(inventory[i].car_model);
+  carModels.push(inventory[i].car_model);
+}
 
-console.log();
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
