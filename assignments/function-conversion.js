d503eb084f5ed7812ec668233a5b1bd3272515f3
unknown <franzferdinan51@gmail.com>
2019-09-05 16:03:24 -0400

    finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,23 +4,32 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const myFunction = () => {}
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+const anotherFunction = (param) => {
+    return param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+const add = (param1, param2) => {
+    return param1 + param2;
+  }
+  add(1,2);
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-
+const subtract = (param1, param2) => {
+    return param1 - param2;	  return param1 - param2;
+  };	}
+  subtract(1,2); //?	subtract(1,2);
 // Stretch
 
 // exampleArray = [1,2,3,4];
@@ -28,3 +37,5 @@
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = exampleArray.map(x => x * 3);
+console.log(triple);
