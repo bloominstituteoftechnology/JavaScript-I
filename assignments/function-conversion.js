45bd297b05283cea0a8ac25cae599e8cc2b836c9
chiragthesia <harithesia@gmail.com>
2019-09-04 21:33:10 -0500

    Completed Function and Objects. Having hard time with Array challenges

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,6 +1,6 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
+// let myFunction = () => {
 // console.log("Function was invoked!");
 // };
 // myFunction();
@@ -9,22 +9,23 @@
 //   return param;
 // };
 // anotherFunction("Example");
+// console.log(anotherFunction);
 
-// let add = function (param1, param2) {
+// let add = (param1, param2) => {
 //   return param1 + param2;
 // };
-// add(1,2);
+// console.log(add(1,2));
 
-// let subtract = function (param1, param2) {
+// let subtract =(param1, param2) => {
 //   return param1 - param2;
 // };
-// subtract(1,2);
+// console.log(subtract(1,2));
 
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(function (num) {
+  return num * 3;
+});
+console.log(triple);
--- assignments/objects.js
+++ assignments/objects.js
@@ -10,35 +10,71 @@
 // 5, adaine5@samsung.com, Antonietta, F
 
 // Example format of an intern object: 1, examples@you.edu, Example, F
-const example = {
-  id: 0,
-  name: "Example",
-  email: "examples@you.edu",
-  gender: "F",
-}
+// const example = {
+//   id: 0,
+//   name: "Example",
+//   email: "examples@you.edu",
+//   gender: "F",
+// }
 
+// console.log(example);
 // Write your intern objects here:
+
+const Mitzi = {
+  id: 1,
+  name:"Mitzi",
+  email:"mmelloy0@psu.edu",
+  gender:"F",
+}
+const Kennan = {
+  id: 2,
+  name:"Kennan",
+  email:"kdiben1@tinypic.com",
+  gender:"M",
+  speak:"Hello, my name is Kennan!",
+}
+const Keven = {
+  id: 3,
+  name:"Keven",
+  email:"kmummery2@wikimedia.org",
+  gender:"M",
+}
+const Gannie = {
+  id: 4,
+  name:"Gannie",
+  email:"gmartinson3@illinois.edu",
+  gender:"M",
+}
+const Antonietta = {
+  id: 1,
+  name:"Antonietta",
+  email:"adaine5@samsung.com",
+  gender:"F",
+  multiplyNums:(num1,num2) => num1 + num2,
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Keven.email);
 // Gannie's name
-
+console.log(Gannie.name);
 // Antonietta's Gender
+console.log(Antonietta.gender);
+
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(Kennan.speak);
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(Antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
