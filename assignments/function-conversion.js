9245ed4b2e522253307043e308ad0c54ddc57ddd
Kronykus <MoreheadSteve@gmail.com>
2019-09-16 14:59:00 -0500

    completed function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,46 @@
 // };
 // myFunction();
 
+const myFunction = () => {
+   console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = ( param ) => {
+   return param;
+};
+console.log( anotherFunction( "Example" ) );
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param2) => {
+   return param1 + param2;
+};
+console.log( add(1,2) );
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (param1, param2) => {
+   return param1 - param2;
+};
+console.log( subtract(1,2) );
+
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+const exampleArray = [1,2,3,4];
+const triple = exampleArray.map(function (num) {
+   return num * 3;
+});
+console.log( triple );
