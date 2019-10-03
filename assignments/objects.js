7f849d003e5ca552340df4b1863050c38a39fef5
QDG <nasha@QDGs-MacBook-Air.local>
2019-10-02 20:16:16 -0700

    done with objects and fuctions

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,26 +19,66 @@ const example = {
 
 // Write your intern objects here:
 
+// question- do we need to add the keys with ""? I tried both ways (with and without "") and it returns the same result but just wondering if this will impact anything?
 
+//how can we determine when to use return or console.log in a method?
+
+const mitzi = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F",
+}
+
+const kennan = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+  "speak": function() {
+    return "Hello, my name is Kennan";
+  }
+}
+const keven = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M",
+}
+const gannie = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M",
+}
+const antonietta = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F",
+  "multiplyNums": function(num1, num2) {
+    return num1 * num2;
+  }
+
+}
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(mitzi.name)
 // Kennan's ID
-
+console.log(kennan.id)
 // Keven's email
-
+console.log(keven.email)
 // Gannie's name
-
+console.log(gannie.name)
 // Antonietta's Gender
-
+console.log(antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
-
+console.log(kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,16 +89,37 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  "name": "Susan",
+  "age": 70, 
+  "speak": function () {
+    console.log(`Hello my name is ${this.name}`);
+  },
+    child: {
+    "name": "George",
+    "age": 50, 
+    "speak": function () {
+      console.log(`Hello my name is ${this.name}`);
+    },
+      grandchild: {
+      "name": "Sam",
+      "age": 30,
+      "speak": function () {
+        console.log(`Hello my name is ${this.name}`)
+      },
+    } //this closes the grandchild
+  } //this closes the child
+} //this closes the parent
 
 // Log the parent object's name
-
+console.log(parent.name)
 // Log the child's age
-
+console.log(parent.child.age)
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name, parent.child.grandchild.age)
 // Have the parent speak
-
+parent.speak();
 // Have the child speak
-
+parent.child.speak();
 // Have the grandchild speak
+parent.child.grandchild.speak();
