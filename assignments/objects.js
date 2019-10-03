53b60e5623b502274a3aa674b11a954d017322ec
barbarof <barbarofreyes@gmail.com>
2019-10-03 14:33:19 -0500

    objet done

--- assignments/objects.js
+++ assignments/objects.js
@@ -93,10 +93,10 @@ console.log(people4.multiply(3,4));
  
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
