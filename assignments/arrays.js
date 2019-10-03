25698273ebdaba51610f5f62e85cd74d64e4199d
QDG <nasha@QDGs-MacBook-Air.local>
2019-10-02 20:20:20 -0700

    almost done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -76,6 +76,7 @@ let lastModel = inventory[lastCar].car_model;
 let lastMake = inventory[lastCar].car_make;
 console.log(`${lastModel} & ${lastMake}`);
 
+
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
