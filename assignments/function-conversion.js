12f853aa8b24827a5795e1aa596b07f078f053db
jarrodskahill <jarrodskahill@gmail.com>
2019-10-14 10:37:59 -0700

    did the stretch for conversions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -23,8 +23,8 @@ subtract(1,2);
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num) => {
+  return num * 3;
+});
+console.log(triple);
