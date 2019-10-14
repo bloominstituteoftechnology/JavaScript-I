f4a3dfde15c10e9c454bc708642fb62b9318b0bc
blakelower <blakelower1999@gmail.com>
2019-10-14 20:11:14 +0700

    added all arrow synatxs

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,26 @@ console.log("Function was invoked!");
 };
 myFunction();
 
-// let anotherFunction = function (param) {
-//   return param;
-// };
-// anotherFunction("Example");
 
-// let add = function (param1, param2) {
-//   return param1 + param2;
-// };
-// add(1,2);
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+let anotherFunction = (param) => {
+  return param;
+};
+anotherFunction("Example");
+
+
+
+let add = (param1, param2) => {
+  return param1 + param2;
+};
+add(1,2);
+
+
+
+let subtract = (param1, param2) => {
+  return param1 - param2;
+};
+subtract(1,2);
 
 
 // Stretch
