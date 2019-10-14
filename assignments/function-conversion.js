f0d83a8feb0ecb97f9f58b72a85b2c6e80461b1a
JimmyMcBride <mcbride967@gmail.com>
2019-10-14 11:29:04 -0500

    Cleaned up code, made console logs more descriptive

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,28 +1,29 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-let myFunction = () => {
-console.log("Function was invoked!")
-}
-myFunction()
-
-let anotherFunction = (param) => {
-        console.log(param)
-}
-anotherFunction("Example")
-
-let add = (param1, param2) => {
-    console.log(param1 + param2)
-}
-add(1,2)
-
-let subtract = (param1, param2) => {
-    return param1 - param2
+const myFunction = () => {
+  console.log("Function was invoked!");
 };
-console.log(subtract(1,2))
+myFunction();
 
+const anotherFunction = param => {
+  console.log(param);
+};
+anotherFunction("Example");
+
+const add = (param1, param2) => {
+  console.log("1 + 2 =", param1 + param2);
+};
+add(1, 2);
+
+const subtract = (param1, param2) => {
+  return param1 - param2;
+};
+console.log("1 - 2 =", subtract(1, 2));
 
 // Stretch
 
-exampleArray = [1,2,3,4];
-const triple = exampleArray.map((num) => {return num * 3})
-console.log(triple);
+exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map(num => {
+  return num * 3;
+});
+console.log("Triple each number: [1, 2, 3, 4] =>", triple);
--- assignments/objects.js
+++ assignments/objects.js
@@ -1,6 +1,6 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
+// ==== Challenge 1: Writing Objects ====
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
@@ -11,74 +11,78 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+  id: 0,
+  name: "Example",
+  email: "examples@you.edu",
+  gender: "F"
+};
 
 // Write your intern objects here:
 const mitzi = {
-	"id": 1,
-	"name": "Mitzi",
-	"email": "mmelloy0@psu.edu",
-	"gender": "F"
-}
-const kennan = {
-	"id": 2,
-	"name": "Kennan",
-	"email": "kdiben1@tinypic.com",
-	"gender": "M",
-	speak: () => "Hello, my name is Kennan."
-}
-const keven = {
-	"id": 3,
-	"name": "Keven",
-	"email": "kmummery2@wikimedia.org",
-	"gender": "M"
-}
-const gannie = {
-	"id": 4,
-	"name": "Gannie",
-	"email": "gmartinson3@illinois.edu",
-	"gender": "M"
-}
-const antonietta = {
-	"id": 5,
-	"name": "Antonietta",
-	"email": "adaine5@samsung.com",
-	"gender": "F",
-	multiplyNums: (num1, num2) => num1 * num2
-}
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+};
 
-// ==== Challenge 2: Reading Object Data ==== 
+const kennan = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: () => "Hello, my name is Kennan."
+};
+
+const keven = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+};
+
+const gannie = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
+};
+
+const antonietta = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+  multiplyNums: (num1, num2) => num1 * num2
+};
+
+// ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-console.log(mitzi.name)
+console.log("Mitzi's name:", mitzi.name);
 
 // Kennan's ID
-console.log(kennan.id)
+console.log("Kennan's ID:", kennan.id);
 
 // Keven's email
-console.log(keven.email)
+console.log("Keven's email:", keven.email);
 
 // Gannie's name
-console.log(gannie.name)
+console.log("Gannie's name:", gannie.name);
 
 // Antonietta's Gender
-console.log(antonietta.gender)
+console.log("Antonietta's Gender", antonietta.gender);
 
-// ==== Challenge 3: Object Methods ==== 
+// ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-console.log(kennan.speak())
+console.log("Kenna say's:", kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
-// ==== Stretch Challenge: Nested Objects and the this keyword ==== 
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
@@ -86,41 +90,50 @@ console.log(antonietta.multiplyNums(3,4));
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
 const parent = {
-	name: "Susan",
-	age: 70,
-	sayName: function(){
-		return `Hello, world! My name is ${this.name}.`
-	},
-	child: {
-		"name": "George",
-		"age": 50,
-		sayName: function(){
-			return `Hello, world! My name is ${this.name}.`
-		},
-		grandchild: {
-			"name": "Sam",
-			"age": 30,
-			sayName: function(){
-				return `Hello, world! My name is ${this.name}.`
-			},
-		}
-	}
-}
+  name: "Susan",
+  age: 70,
+  sayName: function() {
+    return `Hello, world! My name is ${this.name}.`;
+  },
+  child: {
+    name: "George",
+    age: 50,
+    sayName: function() {
+      return `Hello, world! My name is ${this.name}.`;
+    },
+    grandchild: {
+      name: "Sam",
+      age: 30,
+      sayName: function() {
+        return `Hello, world! My name is ${this.name}.`;
+      }
+    }
+  }
+};
 
 // Log the parent object's name
-console.log(parent.name)
+console.log("Parent's name:", parent.name);
 
 // Log the child's age
-console.log(parent.child.age)
+console.log("Child's age:", parent.child.age);
 
 // Log the name and age of the grandchild
-console.log(parent.child.grandchild.name, parent.child.grandchild.age)
+console.log(
+  "Grandchild's name:",
+  parent.child.grandchild.name,
+  "|",
+  "Grandchild's age:",
+  parent.child.grandchild.age
+);
 
 // Have the parent speak
-console.log(parent.sayName())
+console.log("The parent says their name:", parent.sayName());
 
 // Have the child speak
-console.log(parent.child.sayName())
+console.log("The child says their name:", parent.child.sayName());
 
 // Have the grandchild speak
-console.log(parent.child.grandchild.sayName())
+console.log(
+  "The grandchild says their name:",
+  parent.child.grandchild.sayName()
+);
