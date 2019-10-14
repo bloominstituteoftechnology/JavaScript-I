2cf9144225461400ea42267fba0eb505926dd74c
YnotDream <yannick.bds@gmail.com>
2019-10-14 15:55:36 +0200

    Solve all stretch challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -72,7 +72,6 @@ console.log(gannie.name);
 // Antonietta's Gender
 console.log(antonietta.gender);
 
-
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
@@ -84,7 +83,7 @@ console.log(kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
-antonietta.multiplyNums = (a,b) => a * b;
+antonietta.multiplyNums = (a, b) => a * b;
 
 console.log(antonietta.multiplyNums(3, 4));
 
@@ -97,16 +96,44 @@ console.log(antonietta.multiplyNums(3, 4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {};
+const parent = {
+  name: "Susan",
+  age: 70,
+  speak: function() {
+    return "My name is " + this.name;
+  },
+  child: {
+    name: "George",
+    age: 50,
+    speak: function() {
+      return "My name is " + this.name;
+    },
+    grandchild: {
+      name: "Sam",
+      age: 30,
+      speak: function() {
+        return "My name is " + this.name;
+      }
+    }
+  }
+};
 
 // Log the parent object's name
+console.log(parent.name);
 
 // Log the child's age
+console.log(parent.child.name);
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild);
 
 // Have the parent speak
+console.log(parent.speak());
 
 // Have the child speak
 
+console.log(parent.child.speak());
+
 // Have the grandchild speak
+
+console.log(parent.child.grandchild.speak());
