65acb6837a7f35ddee68972f6df95339ccda5a00
Niklas Becker <niklasbeckerr@gmail.com>
2019-10-14 13:00:47 +0200

    object challenge 2 done

--- assignments/objects.js
+++ assignments/objects.js
@@ -61,13 +61,23 @@ const antonietta = {
 
 // Mitzi's name
 
+console.log(mitzi.name)
+
 // Kennan's ID
+
+console.log(kennan.id)
 
 // Keven's email
 
+console.log(keven.email);
+
 // Gannie's name
 
+console.log(gannie.name);
+
 // Antonietta's Gender
+
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
