cc4aca950b4d405d7c50d70d6608a8e4ec993f5f
blakelower <blakelower1999@gmail.com>
2019-10-14 20:08:12 +0700

    added arrow syntax

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,9 +1,9 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
-// };
-// myFunction();
+let myFunction = () => {
+console.log("Function was invoked!");
+};
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
