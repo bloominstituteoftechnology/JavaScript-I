b43e5f6498d04ecdba383ea098415e6c49abeadf
toneiojimz <ajimeneza77@gmail.com>
2019-10-14 17:20:55 -0700

    updated

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,25 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const myFunction = ()=>{ return "Function was invoked";}
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+const anotherFunction= (param) =>{return param;}
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) =>{return param1 + param2;}
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+const subatract = (param1, param2) => {return param1 - param2;}
 
 
 // Stretch
--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,76 @@ const example = {
 }
 
 // Write your intern objects here:
+const intern1 = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F",
+}
 
+const intern2 = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function(){
+    return "Hello, my name is Kennan!";
+  }
+
+  
+
+};
+
+const intern3 = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M",
+}
+
+const intern4 = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M",
+}
+
+const intern5 = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+
+  multiplyNums: function (n1, n2){
+    return n1 * n2;
+  }
+
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(intern1.name);
 
 // Kennan's ID
+console.log(intern2.id);
 
 // Keven's email
+console.log(intern3.email);
 
 // Gannie's name
+console.log(intern4.name);
 
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(intern2.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(intern5.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
